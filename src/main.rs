abe9332fd7008a058b1fbddee74af236f123f180
Kenny Kerr <kekerr@microsoft.com>
2019-10-28 07:56:10 -0700

    main

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// #![allow(unused_variables)]
+// #![allow(dead_code)]
+
+use std::io::*;
+
+fn main() {
+    if let Err(e) = run() {
+        println!("{}", e);
+    }
+}
+
+fn run() -> std::io::Result<()> {
+
+
+    Ok(())
+}
