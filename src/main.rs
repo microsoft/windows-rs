7cb899c90b350c6ca2413d58b9da3bf22a1a8ca7
Kenny Kerr <kekerr@microsoft.com>
2019-10-28 13:07:07 -0700

    RoGetActivationFactory

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 // #![allow(dead_code)]
 
 mod runtime;
-use std::io::*;
 use runtime::*;
+use std::io::*;
 
 fn main() {
     if let Err(e) = run() {
@@ -11,23 +11,44 @@ fn main() {
     }
 }
 
+// trait IUnknown{
+//     unsafe fn QueryInterface(&self, interface: &GUID, result: *mut *mut VOID) -> HRESULT;
+//     fn AddRef(&self)->u32;
+//     fn Release(&self)->u32;
+// }
+
+// trait IInspectable : IUnknown{
+//     fn GetIids(&self) -> HRESULT;
+//     fn GetRuntimeClassName(&self) -> HRESULT;
+//     fn GetTrustLevel(&self) -> HRESULT;
+// }
+
+// trait IColorsStatics : IInspectable{
+//     fn get_AliceBlue(&self) -> HRESULT;
+// }
+
 fn run() -> std::io::Result<()> {
     unsafe {
         let hr = RoInitialize(ApartmentType::MultiThreaded);
         println!("{:#x}", hr);
         assert!(hr == 0);
-        let hr = RoInitialize(ApartmentType::MultiThreaded);
-        assert!(hr == 1);
 
-        let mut hstring : PVOID = std::ptr::null_mut();
-        let wchars : Vec<u16> = "hello".encode_utf16().collect();
-        let hr = WindowsCreateString(wchars.as_ptr(), 5, &mut hstring);
+        let mut hstring: HSTRING = std::ptr::null_mut();
+        let wchars: Vec<u16> = "Windows.UI.Colors".encode_utf16().collect();
+        let hr = WindowsCreateString(wchars.as_ptr(), "Windows.UI.Colors".len() as u32, &mut hstring);
         assert!(hr == 0);
 
         let mut length = 0;
         let raw = WindowsGetStringRawBuffer(hstring, &mut length);
         let value = String::from_utf16(std::slice::from_raw_parts(raw, length as usize)).unwrap();
         println!("{} : {} ({})", length, value, value.len());
+
+        let guid_IColorsStatics = GUID::from_values(0xCFF52E04, 0xCCA6, 0x4614, &[0xA1, 0x7E, 0x75, 0x49, 0x10, 0xC8, 0x4A, 0x99]);
+
+        let mut factory: *mut VOID = std::ptr::null_mut();
+        let hr = RoGetActivationFactory(hstring, &guid_IColorsStatics, &mut factory);
+        println!("{:#x}", hr);
+        assert!(hr == 0);
     }
     Ok(())
 }
--- src/runtime.rs
+++ src/runtime.rs
@@ -2,17 +2,34 @@
 
 #[link(name = "onecore")]
 extern "C" {
-    pub fn RoInitialize(apartment: ApartmentType) -> i32;
+    pub fn WindowsCreateString(value: PCWSTR, length: u32, result: &mut HSTRING) -> HRESULT;
+    pub fn WindowsGetStringRawBuffer(value: HSTRING, length: &mut u32) -> PCWSTR;
 
-    pub fn WindowsCreateString(value: *const u16, length: u32, hstring: *mut PVOID) -> i32;
-    pub fn WindowsGetStringRawBuffer(hstring: PVOID, length: *mut u32) -> *const u16;
+    pub fn RoInitialize(apartment: ApartmentType) -> HRESULT;
+    pub fn RoGetActivationFactory(class: HSTRING, interface: &GUID, result: *mut *mut VOID) -> HRESULT;
 }
 
-pub enum VOID{}
-pub type PVOID = *mut VOID;
+pub enum VOID {}
+pub type PCWSTR = *const u16;
+pub type HRESULT = i32;
+pub type HSTRING = *mut VOID;
 
 #[repr(C)]
 pub enum ApartmentType {
     SingleThreaded,
     MultiThreaded,
+}
+
+#[repr(C)]
+pub struct GUID {
+    data1: u32,
+    data2: u16,
+    data3: u16,
+    data4: [u8; 8],
+}
+
+impl GUID {
+    pub fn from_values(data1: u32, data2: u16, data3: u16, data4: &[u8; 8]) -> GUID {
+        GUID { data1, data2, data3, data4: *data4 }
+    }
 }
