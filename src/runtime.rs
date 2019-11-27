761e0ccaa70a037f18ac6df10daab9a2bf8b8f93
Kenny Kerr <kekerr@microsoft.com>
2019-11-27 11:16:43 -0800

    runtime

--- src/runtime.rs
+++ src/runtime.rs
@@ -9,10 +9,13 @@ pub enum VOID {}
 #[link(name = "onecore")]
 extern "C" {
     // TODO: these need stdcall
+    pub fn CoIncrementMTAUsage(cookie: *mut *mut VOID) -> ErrorCode;
+
     pub fn WindowsCreateString(value: *const u16, length: u32, result: *mut *mut VOID) -> ErrorCode;
     pub fn WindowsGetStringRawBuffer(hstring: *const VOID, length: &mut u32) -> *const u16;
     pub fn WindowsGetStringLen(hstring: *const VOID) -> u32;
+    pub fn WindowsPreallocateStringBuffer(len: u32, buffer: *mut *mut u16, handle: *mut *mut VOID) -> ErrorCode;
+    pub fn WindowsPromoteStringBuffer(handle: *const VOID, hstring: *mut *mut VOID) -> ErrorCode;
 
-    pub fn CoIncrementMTAUsage(cookie: *mut *mut VOID) -> ErrorCode;
     pub fn RoGetActivationFactory(hstring: *const VOID, interface: &Guid, result: *mut *mut VOID) -> ErrorCode;
 }
