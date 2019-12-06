1aa1a6c29232a24e1a873e59e541849ad7d6421e
Kenny Kerr <kenny.kerr@microsoft.com>
2019-12-06 14:36:36 -0800

    enums

--- macros/src/writers.rs
+++ macros/src/writers.rs
@@ -12,11 +12,46 @@ pub(crate) fn write_namespace(namespace: &winmd::Namespace, scope: &std::collect
         };
     }
 
-    tokens.into()
+    tokens
 }
 
 fn write_types(namespace: &winmd::Namespace, scope: &std::collections::BTreeSet::<String>) -> proc_macro2::TokenStream {
-    quote! {
+    let mut enums = write_enums(namespace);
 
+    quote! {
+        #enums
     }
+}
+
+fn write_enums(namespace: &winmd::Namespace) -> proc_macro2::TokenStream {
+    let mut tokens = quote! {};
+
+    for t in namespace.enums() {
+        let name = format_ident!("{}", t.name().unwrap());
+        let fields = write_enum_fields(&t);
+        tokens = quote! {
+            #tokens
+            pub enum #name { #fields }
+        };
+    }
+
+    tokens
+}
+
+fn write_enum_fields(t : &winmd::TypeDef) -> proc_macro2::TokenStream {
+    let mut tokens = quote! {};
+
+    for f in t.fields().unwrap() {
+        for c in f.constants().unwrap() {
+            let name = format_ident!("{}", f.name().unwrap());
+            let value = c.value().unwrap();
+
+            tokens = quote! {
+                #tokens
+                #name,
+            };
+        }
+    }
+
+    tokens
 }
