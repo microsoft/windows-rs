4a03e9005000b14c075bc9347e2dccb1fc8d88c1
Kenny Kerr <kenny.kerr@microsoft.com>
2020-01-09 17:30:46 -0800

    generics

--- src/traits.rs
+++ src/traits.rs
@@ -15,3 +15,15 @@ pub trait AsAbi {
     fn as_abi_in(&self) -> Self::In;
     fn as_abi_out(&mut self) -> Self::Out;
 }
+
+impl AsAbi for u32 {
+    type In = u32;
+    type Out = *mut u32;
+
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
+ }
