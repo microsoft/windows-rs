1fa5a09eeaed78cff451cd4301d1d5a5f5cfb295
Kenny Kerr <kenny.kerr@microsoft.com>
2020-01-10 13:55:19 -0800

    functions

--- src/guid.rs
+++ src/guid.rs
@@ -17,8 +17,7 @@ impl Guid {
 
 impl std::fmt::Debug for Guid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:08X?}-{:04X?}-{:04X?}-{:02X?}{:02X?}-{:02X?}{:02X?}{:02X?}{:02X?}{:02X?}{:02X?}",
-         self.data1, self.data2, self.data3, self.data4[0], self.data4[1],  self.data4[2], self.data4[3] ,  self.data4[4], self.data4[5],  self.data4[6], self.data4[7])
+        write!(f, "{:08X?}-{:04X?}-{:04X?}-{:02X?}{:02X?}-{:02X?}{:02X?}{:02X?}{:02X?}{:02X?}{:02X?}", self.data1, self.data2, self.data3, self.data4[0], self.data4[1], self.data4[2], self.data4[3], self.data4[4], self.data4[5], self.data4[6], self.data4[7])
     }
 }
 
