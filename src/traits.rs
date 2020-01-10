d06f21e3f57a8bf0c36930877296f3d4e6d4d128
Kenny Kerr <kenny.kerr@microsoft.com>
2020-01-10 14:15:43 -0800

    snakes

--- src/traits.rs
+++ src/traits.rs
@@ -19,84 +19,132 @@ pub trait AsAbi: Default {
 impl AsAbi for bool {
     type In = bool;
     type Out = *mut bool;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for char {
     // TODO: this needs to be u16
     type In = char;
     type Out = *mut char;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for i8 {
     type In = i8;
     type Out = *mut i8;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for u8 {
     type In = u8;
     type Out = *mut u8;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for i16 {
     type In = i16;
     type Out = *mut i16;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for u16 {
     type In = u16;
     type Out = *mut u16;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for i32 {
     type In = i32;
     type Out = *mut i32;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for u32 {
     type In = u32;
     type Out = *mut u32;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for i64 {
     type In = i64;
     type Out = *mut i64;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for u64 {
     type In = u64;
     type Out = *mut u64;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for f32 {
     type In = f32;
     type Out = *mut f32;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
 
 impl AsAbi for f64 {
     type In = f64;
     type Out = *mut f64;
-    fn as_abi_in(&self) -> Self::In { *self }
-    fn as_abi_out(&mut self) -> Self::Out { self as Self::Out }
+    fn as_abi_in(&self) -> Self::In {
+        *self
+    }
+    fn as_abi_out(&mut self) -> Self::Out {
+        self as Self::Out
+    }
 }
