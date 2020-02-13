468b9378893c4b2064a1decc42b30ff1be52a3fa
Kenny Kerr <kenny.kerr@microsoft.com>
2020-02-13 13:37:29 -0800

    comment

--- src/guid.rs
+++ src/guid.rs
@@ -7,8 +7,6 @@ pub struct Guid {
     data4: [u8; 8],
 }
 
-// TODO: impl Debug trait directly to print Guid properly
-
 impl Guid {
     pub const fn from_values(data1: u32, data2: u16, data3: u16, data4: &[u8; 8]) -> Guid {
         Guid { data1, data2, data3, data4: *data4 }
