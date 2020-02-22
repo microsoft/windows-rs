a50dc5e3c837ab965fecf6d9759d2ac050361914
Kenny Kerr <kenny.kerr@microsoft.com>
2020-02-22 14:21:02 -0800

    tests

--- tests/uri.rs
+++ tests/uri.rs
@@ -11,12 +11,18 @@ use winrt::*;
 fn uri() -> Result<()> {
     use windows::foundation::*;
 
-    let uri = Uri::create_uri("http://kennykerr.ca")?;
+    let uri = &Uri::create_uri("http://kennykerr.ca")?;
     assert!(uri.domain()? == "kennykerr.ca");
     assert!(uri.port()? == 80);
 
     // Calls QueryInterface followed by IStringable::ToString under the hood
     assert!(uri.to_string()? == "http://kennykerr.ca/");
+
+    let default : IUriRuntimeClass = uri.into();
+    assert!(default.domain()? == uri.domain()?);
+
+    let stringable: IStringable = uri.into();
+    assert!(stringable.to_string()? == uri.to_string()?);
 
     Ok(())
 }
