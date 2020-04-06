8e5ef00f7fa9148de6e6bb235db42c654005ef7c
Ryan Levick <rylev@users.noreply.github.com>
2020-04-06 20:23:29 +0200

    Tests for case helpers (#37)

--- crates/winmd/src/case.rs
+++ crates/winmd/src/case.rs
@@ -6,7 +6,7 @@ pub fn to_snake(preamble: &str, camel: &str) -> String {
     // whether we should just hard code the well-known ones or somehow use
     // heuristics to do this generically. It should handle conversions like
     // this:
-    // 
+    //
     // "Windows" -> "windows"
     // "UI" -> "ui"
     // "ApplicationModel" -> "application_model"
@@ -28,4 +28,20 @@ pub fn to_snake(preamble: &str, camel: &str) -> String {
     }
 
     snake
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn to_snake_works() {
+        assert_eq!(to_snake("", "Windows"), "windows".to_owned());
+        assert_eq!(
+            to_snake("", "ApplicationModel"),
+            "application_model".to_owned()
+        );
+        // assert_eq!(to_snake("", "UIProgramming"), "ui_programming".to_owned());
+        // assert_eq!(to_snake("", "CreateU8Array"), "create_u8_array".to_owned());
+        assert_eq!(to_snake("awesome", "Windows"), "awesome_windows".to_owned());
+    }
 }
