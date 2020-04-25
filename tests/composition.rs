b8bb682bc927b90ad95814b584e716572775348f
Kenny Kerr <kenny.kerr@microsoft.com>
2020-04-25 12:13:37 -0700

    sample

--- tests/composition.rs
+++ tests/composition.rs
@@ -5,152 +5,147 @@ winrt::import!(
         "windows.ui.composition"
 );
 
-use windows::foundation::collections::*;
-use windows::ui::composition::*;
-use winrt::*;
+#[link(name = "coremessaging")]
+extern "stdcall" {
+    fn CreateDispatcherQueueController(
+        options: DispatcherQueueOptions,
+        dispatcherQueueController: *mut winrt::RawPtr,
+    ) -> i32;
+}
 
-// impl<T: RuntimeType> Iterator for IIterator<T> {
-//     type Item = T;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         let result = self.current().ok();
-
-//         if result.is_some() {
-//             self.move_next().ok()?;
-//         }
-
-//         result
-//     }
-// }
-
-impl IntoIterator for &VisualCollection {
-    type Item = Visual;
-    type IntoIter = IIterator<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.first().unwrap()
-    }
+#[repr(C)]
+struct DispatcherQueueOptions {
+    size: u32,
+    thread_type: i32,
+    apartment_type: i32,
 }
 
 #[test]
-fn uri() -> winrt::Result<()> {
-    if false {
-        use windows::foundation::numerics::*;
-        use windows::ui::composition::*;
-        use windows::ui::*;
-        use winrt::*;
+fn composition() -> winrt::Result<()> {
+    use windows::foundation::numerics::*;
+    use windows::ui::composition::*;
+    use windows::ui::*;
+    use winrt::*;
 
-        let compositor = Compositor::new()?;
-        let visual = compositor.create_sprite_visual()?;
+    // We need a DispatcherQueue on our thread to properly create a Compositor. Note that since
+    // we aren't pumping messages, the Compositor won't commit. This is fine for the test for now.
+    let options = DispatcherQueueOptions {
+        size: std::mem::size_of::<DispatcherQueueOptions>() as u32,
+        thread_type: 2,    // DQTYPE_THREAD_CURRENT
+        apartment_type: 0, // DQTAT_COM_NONE
+    };
+    let _queue_controller = unsafe {
+        let mut interop_ptr = winrt::IUnknown::default();
+        winrt::ErrorCode(CreateDispatcherQueueController(options, interop_ptr.set())).ok()?;
+        interop_ptr
+    };
 
-        // Hidden QI to IVisual happens before calling set_offset
-        visual.set_offset(Vector3 {
-            x: 1.0,
-            y: 2.0,
-            z: 3.0
-        })?;
+    let compositor = Compositor::new()?;
+    let visual = compositor.create_sprite_visual()?;
+    let red = Colors::red()?;
 
+    assert!(
+        red == Color {
+            a: 255,
+            r: 255,
+            g: 0,
+            b: 0
+        }
+    );
 
+    // Visual.set_brush expects a CompositionBrush but create_color_brush_with_color returns a
+    // CompositionColorBrush that logically derives from CompositionBrush.
+    let brush = compositor.create_color_brush_with_color(&red)?;
+    visual.set_brush(brush)?;
 
-        let red = Colors::red()?;
+    // Visual.brush returns a CompositionBrush but we know that it's actually a CompositionColorBrush
+    // and need to convert it excplicitly since Rust/WinRT doesn't know that.
+    let brush: CompositionColorBrush = visual.brush()?.try_into().unwrap();
+    assert!(brush.color()? == red);
 
-        assert!(
-            red == Color {
-                a: 255,
-                r: 255,
-                g: 0,
-                b: 0
+    visual.set_offset(Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    })?;
+
+    assert!(
+        visual.offset()?
+            == Vector3 {
+                x: 1.0,
+                y: 2.0,
+                z: 3.0
             }
-        );
+    );
 
-        // Visual.set_brush expects a CompositionBrush but create_color_brush_with_color returns a
-        // CompositionColorBrush that logically derives from CompositionBrush.
-        let brush = compositor.create_color_brush_with_color(&red)?;
-        visual.set_brush(brush)?;
+    let children = visual.children()?;
 
-        // Visual.brush returns a CompositionBrush but we know that it's actually a CompositionColorBrush
-        // and need to convert it excplicitly since Rust/WinRT doesn't know that.
-        let brush: CompositionColorBrush = visual.brush()?.try_into().unwrap();
-        assert!(brush.color()? == red);
+    let child = compositor.create_sprite_visual()?;
+    child.set_offset(Vector3 {
+        x: 1.0,
+        y: 0.0,
+        z: 0.0,
+    })?;
+    children.insert_at_bottom(child)?;
 
-        visual.set_offset(Vector3 {
-            x: 1.0,
-            y: 2.0,
-            z: 3.0,
-        })?;
+    let child = compositor.create_sprite_visual()?;
+    child.set_offset(Vector3 {
+        x: 2.0,
+        y: 0.0,
+        z: 0.0,
+    })?;
+    children.insert_at_bottom(child)?;
 
-        assert!(
-            visual.offset()?
-                == Vector3 {
-                    x: 1.0,
-                    y: 2.0,
-                    z: 3.0
-                }
-        );
+    let child = compositor.create_sprite_visual()?;
+    child.set_offset(Vector3 {
+        x: 3.0,
+        y: 0.0,
+        z: 0.0,
+    })?;
+    children.insert_at_bottom(child)?;
 
-        let children = visual.children()?;
+    assert!(children.count()? == 3);
 
-        let child = compositor.create_sprite_visual()?;
-        child.set_offset(Vector3 {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-        })?;
-        children.insert_at_bottom(child)?;
+    // TODO: Collection iteration is still crude but at least the underlying collection interfaces are working.
 
-        let child = compositor.create_sprite_visual()?;
-        child.set_offset(Vector3 {
-            x: 2.0,
-            y: 0.0,
-            z: 0.0,
-        })?;
-        children.insert_at_bottom(child)?;
+    let iterator = children.first()?;
+    assert!(iterator.has_current()?);
 
-        let child = compositor.create_sprite_visual()?;
-        child.set_offset(Vector3 {
-            x: 3.0,
-            y: 0.0,
-            z: 0.0,
-        })?;
-        children.insert_at_bottom(child)?;
+    assert!(
+        iterator.current()?.offset()?
+            == Vector3 {
+                x: 3.0,
+                y: 0.0,
+                z: 0.0,
+            }
+    );
 
-        assert!(children.count()? == 3);
+    assert!(iterator.move_next()?);
+    assert!(iterator.has_current()?);
 
-        // TODO: Collection iteration is still crude but at least the underlying collection interfaces are working.
+    assert!(
+        iterator.current()?.offset()?
+            == Vector3 {
+                x: 2.0,
+                y: 0.0,
+                z: 0.0,
+            }
+    );
 
-        for v in &children {}
+    assert!(iterator.move_next()?);
+    assert!(iterator.has_current()?);
 
-        let mut iterator = children.into_iter();
+    assert!(
+        iterator.current()?.offset()?
+            == Vector3 {
+                x: 1.0,
+                y: 0.0,
+                z: 0.0,
+            }
+    );
 
-        assert!(
-            iterator.next().unwrap().offset()?
-                == Vector3 {
-                    x: 1.0,
-                    y: 0.0,
-                    z: 0.0,
-                }
-        );
-
-        assert!(
-            iterator.next().unwrap().offset()?
-                == Vector3 {
-                    x: 2.0,
-                    y: 0.0,
-                    z: 0.0,
-                }
-        );
-
-        assert!(
-            iterator.next().unwrap().offset()?
-                == Vector3 {
-                    x: 3.0,
-                    y: 0.0,
-                    z: 0.0,
-                }
-        );
-
-        assert!(iterator.next().is_none() == true);
-    }
+    assert!(iterator.move_next()? == false);
+    assert!(iterator.has_current()? == false);
 
     Ok(())
 }
