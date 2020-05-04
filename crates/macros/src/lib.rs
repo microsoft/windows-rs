6460e2578d39f44a20140b58e135738890119c7e
Ryan Levick <ryan.levick@gmail.com>
2020-05-04 10:43:28 +0000

    Rearrange import macro parser

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -28,53 +28,135 @@ pub fn import(stream: TokenStream) -> TokenStream {
 
 #[derive(PartialEq)]
 enum ImportCategory {
-    None,
     Dependency,
     Namespace,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+enum ParseState {
+    Neither,
+    ParsedNamespace,
+    ParsedDependency,
+    Both,
+}
+impl ParseState {
+    fn parsed_namespace(self) -> Self {
+        match self {
+            ParseState::Neither => ParseState::ParsedNamespace,
+            ParseState::ParsedDependency => ParseState::Both,
+            _ => self,
+        }
+    }
+    fn parsed_dependency(self) -> Self {
+        match self {
+            ParseState::Neither => ParseState::ParsedDependency,
+            ParseState::ParsedNamespace => ParseState::Both,
+            _ => self,
+        }
+    }
 }
 
 /// Parse `import!` macro and return a set of paths to dependencies and
 /// a set to all the namespaces referenced
 fn parse_import_stream(stream: TokenStream) -> (BTreeSet<PathBuf>, BTreeSet<String>) {
-    let mut category = ImportCategory::None;
     let mut dependencies = BTreeSet::<PathBuf>::new();
     let mut modules = BTreeSet::<String>::new();
     let mut stream = stream.into_iter().peekable();
+    let mut state = ParseState::Neither;
 
-    while let Some(token) = stream.next() {
-        match token {
-            TokenTree::Ident(value) => {
-                match value.to_string().as_ref() {
-                    "dependencies" => category = ImportCategory::Dependency,
-                    "modules" => category = ImportCategory::Namespace,
-                    value => panic!("winrt::import macro expects either `dependencies` or `modules` but found `{}`", value),
-                }
-                if let Some(TokenTree::Punct(p)) = stream.peek() {
-                    if p.as_char() == ':' {
-                        let _ = stream.next();
-                    }
-                }
+    loop {
+        if state == ParseState::Both {
+            break;
+        }
+        let category = parse_category(&mut stream);
+        match category {
+            ImportCategory::Namespace => {
+                modules.extend(parse_namespace(&mut stream));
+                state = state.parsed_namespace();
             }
-            TokenTree::Literal(value) => match category {
-                ImportCategory::None => panic!(
-                    "winrt::import macro expects either `dependencies` or `modules` but found `{}`",
-                    value
-                ),
-                ImportCategory::Dependency => {
-                    dependencies.append(&mut to_dependencies(value.to_string().trim_matches('"')));
-                }
-                ImportCategory::Namespace => {
-                    modules.insert(namespace_literal_to_rough_namespace(&value.to_string()));
-                }
-            },
-            _ => panic!(
-                "winrt::import macro encountered an unrecognized token: {}",
-                token
-            ),
+            ImportCategory::Dependency => {
+                dependencies.extend(parse_dependencies(&mut stream));
+                state = state.parsed_dependency();
+            }
         }
     }
 
     (dependencies, modules)
+}
+
+fn parse_category(
+    stream: &mut std::iter::Peekable<impl std::iter::Iterator<Item = TokenTree>>,
+) -> ImportCategory {
+    let token = stream.next().expect(
+        "Unexpected end of winrt::import macro. Expected either `dependencies` or `modules`",
+    );
+    match token {
+        TokenTree::Ident(value) => {
+            let category = match value.to_string().as_ref() {
+                "dependencies" => ImportCategory::Dependency,
+                "modules" => ImportCategory::Namespace,
+                value => panic!(
+                    "winrt::import macro expects either `dependencies` or `modules` but found `{}`",
+                    value
+                ),
+            };
+            if let Some(TokenTree::Punct(p)) = stream.peek() {
+                if p.as_char() == ':' {
+                    let _ = stream.next();
+                }
+            }
+            category
+        }
+        _ => {
+            panic!(
+                "winrt::import macro encountered an unrecognized token: {}. Expected `dependencies` or `modules`",
+                token
+            );
+        }
+    }
+}
+
+fn parse_namespace(
+    stream: &mut std::iter::Peekable<impl std::iter::Iterator<Item = TokenTree>>,
+) -> BTreeSet<String> {
+    let mut modules = BTreeSet::<String>::new();
+    loop {
+        let token = stream.peek();
+        match token {
+            Some(TokenTree::Literal(value)) => {
+                modules.insert(namespace_literal_to_rough_namespace(&value.to_string()));
+                let _ = stream.next();
+            }
+            _ => break,
+        }
+    }
+    modules
+}
+
+fn parse_dependencies(
+    stream: &mut std::iter::Peekable<impl std::iter::Iterator<Item = TokenTree>>,
+) -> BTreeSet<PathBuf> {
+    let mut dependencies = BTreeSet::<PathBuf>::new();
+
+    loop {
+        let token = stream.peek();
+        match token {
+            Some(TokenTree::Literal(value)) => {
+                dependencies.append(&mut to_dependencies(value.to_string().trim_matches('"')));
+                let _ = stream.next();
+            }
+            _ => break,
+        }
+    }
+    dependencies
+}
+
+enum Dependency {
+    Paths(BTreeSet<PathBuf>),
+    Nuget {
+        name: String,
+        version: Option<String>,
+    },
 }
 
 /// Returns the paths to resolved dependencies
@@ -82,7 +164,15 @@ fn to_dependencies<P: AsRef<Path>>(dependency: P) -> BTreeSet<PathBuf> {
     let path = dependency.as_ref();
     let mut result = BTreeSet::new();
 
-    if path.is_dir() {
+    if path == Path::new("os") {
+        let mut path = PathBuf::new();
+        let wind_dir_env = std::env::var("windir")
+            .unwrap_or_else(|_| panic!("No `windir` environment variable found"));
+        path.push(wind_dir_env);
+        path.push(SYSTEM32);
+        path.push("winmetadata");
+        result.append(&mut to_dependencies(path));
+    } else if path.is_dir() {
         let paths = std::fs::read_dir(path).unwrap_or_else(|e| {
             panic!(
                 "Could not read dependecy directory at path {:?}: {}",
@@ -99,14 +189,6 @@ fn to_dependencies<P: AsRef<Path>>(dependency: P) -> BTreeSet<PathBuf> {
         }
     } else if path.is_file() {
         result.insert(path.to_path_buf());
-    } else if path.to_str().map(|p| p == "os").unwrap_or(false) {
-        let mut path = PathBuf::new();
-        let wind_dir_env = std::env::var("windir")
-            .unwrap_or_else(|_| panic!("No `windir` environment variable found"));
-        path.push(wind_dir_env);
-        path.push(SYSTEM32);
-        path.push("winmetadata");
-        result.append(&mut to_dependencies(path));
     } else {
         panic!("Dependency {:?} is not a file or directory", path);
     }
