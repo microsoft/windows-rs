7b09ae2ea52bbcc357043a2726cc35121196bd03
Kenny Kerr <kenny.kerr@microsoft.com>
2020-05-13 08:29:02 -0700

    guids

--- tests/signature.rs
+++ tests/signature.rs
@@ -15,17 +15,17 @@ fn signature() -> winrt::Result<()> {
     type A = windows::foundation::collections::IIterable::<windows::foundation::IStringable>;
     type B = windows::foundation::collections::IKeyValuePair::<winrt::HString, windows::foundation::IAsyncOperationWithProgress<A, f32>>;
 
-    assert_eq!(A::signature(),
-    "pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3})");
+    // assert_eq!(A::signature(),
+    // "pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3})");
 
-    assert_eq!(B::signature(),
-    "pinterface({02b51929-c1c4-4a7e-8940-0312b5c18500};string;pinterface({b5d036d7-e297-498f-ba60-0289e76e23dd};pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3});f4))");
+    // assert_eq!(B::signature(),
+    // "pinterface({02b51929-c1c4-4a7e-8940-0312b5c18500};string;pinterface({b5d036d7-e297-498f-ba60-0289e76e23dd};pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3});f4))");
 
-    assert_eq!(windows::foundation::EventHandler::<A>::signature(),
-    "pinterface({9de1c535-6ae1-11e0-84e1-18a905bcc53f};pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3}))");
+    // assert_eq!(windows::foundation::EventHandler::<A>::signature(),
+    // "pinterface({9de1c535-6ae1-11e0-84e1-18a905bcc53f};pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3}))");
 
-    assert_eq!(windows::foundation::EventHandler::<B>::signature(),
-    "pinterface({9de1c535-6ae1-11e0-84e1-18a905bcc53f};pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3}))");
+    // assert_eq!(windows::foundation::EventHandler::<B>::signature(),
+    // "pinterface({9de1c535-6ae1-11e0-84e1-18a905bcc53f};pinterface({faa585ea-6214-4217-afda-7f46de5869b3};{96369f54-8eb6-48f0-abce-c1b211e627c3}))");
 
     // assert_eq!(bool::signature(), "b1");
     // assert_eq!(i8::signature(), "i1");
@@ -105,65 +105,65 @@ fn signature() -> winrt::Result<()> {
 
     // // // Windows.Foundation
 
-    // assert_eq!(
-    //     windows::foundation::IAsyncActionWithProgress::<A>::iid(),
-    //     &winrt::Guid::from("DD725452-2DA3-5103-9C7D-22EE9BB14AD3")
-    // );
+    assert_eq!(
+        windows::foundation::IAsyncActionWithProgress::<A>::iid(),
+        winrt::Guid::from("DD725452-2DA3-5103-9C7D-22EE9BB14AD3")
+    );
 
-    // // // assert_eq!(
-    // // //     windows::foundation::IAsyncOperationWithProgress::<A, B>::iid(),
-    // // //     &winrt::Guid::from("94645425-B9E5-5B91-B509-8DA4DF6A8916")
-    // // // );
+    assert_eq!(
+        windows::foundation::IAsyncOperationWithProgress::<A, B>::iid(),
+        winrt::Guid::from("94645425-B9E5-5B91-B509-8DA4DF6A8916")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::IAsyncOperation::<A>::iid(),
-    // //     &winrt::Guid::from("2BD35EE6-72D9-5C5D-9827-05EBB81487AB")
-    // // );
+    assert_eq!(
+        windows::foundation::IAsyncOperation::<A>::iid(),
+        winrt::Guid::from("2BD35EE6-72D9-5C5D-9827-05EBB81487AB")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::IReferenceArray::<A>::iid(),
-    // //     &winrt::Guid::from("4A33FE03-E8B9-5346-A124-5449913ECA57")
-    // // );
+    assert_eq!(
+        windows::foundation::IReferenceArray::<A>::iid(),
+        winrt::Guid::from("4A33FE03-E8B9-5346-A124-5449913ECA57")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::IReference::<A>::iid(),
-    // //     &winrt::Guid::from("F9E4006C-6E8C-56DF-811C-61F9990EBFB0")
-    // // );
+    assert_eq!(
+        windows::foundation::IReference::<A>::iid(),
+        winrt::Guid::from("F9E4006C-6E8C-56DF-811C-61F9990EBFB0")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::AsyncActionProgressHandler::<A>::iid(),
-    // //     &winrt::Guid::from("C261D8D0-71BA-5F38-A239-872342253A18")
-    // // );
+    assert_eq!(
+        windows::foundation::AsyncActionProgressHandler::<A>::iid(),
+        winrt::Guid::from("C261D8D0-71BA-5F38-A239-872342253A18")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::AsyncActionWithProgressCompletedHandler::<A>::iid(),
-    // //     &winrt::Guid::from("9A0D211C-0374-5D23-9E15-EAA3570FAE63")
-    // // );
+    assert_eq!(
+        windows::foundation::AsyncActionWithProgressCompletedHandler::<A>::iid(),
+        winrt::Guid::from("9A0D211C-0374-5D23-9E15-EAA3570FAE63")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::AsyncOperationCompletedHandler::<A>::iid(),
-    // //     &winrt::Guid::from("9D534225-231F-55E7-A6D0-6C938E2D9160")
-    // // );
+    assert_eq!(
+        windows::foundation::AsyncOperationCompletedHandler::<A>::iid(),
+        winrt::Guid::from("9D534225-231F-55E7-A6D0-6C938E2D9160")
+    );
 
-    // // // assert_eq!(
-    // // //     windows::foundation::AsyncOperationProgressHandler::<A, B>::iid(),
-    // // //     &winrt::Guid::from("264F1E0C-ABE4-590B-9D37-E1CC118ECC75")
-    // // // );
+    assert_eq!(
+        windows::foundation::AsyncOperationProgressHandler::<A, B>::iid(),
+        winrt::Guid::from("264F1E0C-ABE4-590B-9D37-E1CC118ECC75")
+    );
 
-    // // // assert_eq!(
-    // // //     windows::foundation::AsyncOperationWithProgressCompletedHandler::<A, B>::iid(),
-    // // //     &winrt::Guid::from("C2D078D8-AC47-55AB-83E8-123B2BE5BC5A")
-    // // // );
+    assert_eq!(
+        windows::foundation::AsyncOperationWithProgressCompletedHandler::<A, B>::iid(),
+        winrt::Guid::from("C2D078D8-AC47-55AB-83E8-123B2BE5BC5A")
+    );
 
-    // // assert_eq!(
-    // //     windows::foundation::EventHandler::<A>::iid(),
-    // //     &winrt::Guid::from("FA0B7D80-7EFA-52DF-9B69-0574CE57ADA4")
-    // // );
+    assert_eq!(
+        windows::foundation::EventHandler::<A>::iid(),
+        winrt::Guid::from("FA0B7D80-7EFA-52DF-9B69-0574CE57ADA4")
+    );
 
-    // // // assert_eq!(
-    // // //     windows::foundation::TypedEventHandler::<A, B>::iid(),
-    // // //     &winrt::Guid::from("EDB31843-B4CF-56EB-925A-D4D0CE97A08D")
-    // // // );
+    assert_eq!(
+        windows::foundation::TypedEventHandler::<A, B>::iid(),
+        winrt::Guid::from("EDB31843-B4CF-56EB-925A-D4D0CE97A08D")
+    );
 
     Ok(())
 }
