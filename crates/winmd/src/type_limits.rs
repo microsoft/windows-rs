0a6942297a1478c0dc6514d118d02f4c17f38f7d
Kenny Kerr <kekerr@microsoft.com>
2020-05-27 01:36:33 -0700

    parent (#160)
    Co-authored-by: Kenny Kerr <kenny.kerr@microsoft.com>

--- crates/winmd/src/type_limits.rs
+++ crates/winmd/src/type_limits.rs
@@ -15,54 +15,6 @@ impl TypeLimits {
             .find(|name| name.to_lowercase() == namespace)
             .unwrap_or_else(|| panic!("Namespace `{}` not found in winmd files", namespace));
 
-        let mut namespace = found.as_str();
-        self.0.insert(namespace.to_owned());
-
-        while let Some(pos) = namespace.rfind('.') {
-            namespace = &namespace[..pos];
-
-            if reader.types.contains_key(namespace) {
-                self.0.insert(namespace.to_owned());
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parent_inclusion() {
-        let reader = &TypeReader::from_os();
-
-        {
-            // Windows.Foundation's parent is empty so that's not included
-            let mut limits = TypeLimits::default();
-            limits.insert(reader, "windows.foundation");
-            assert!(limits.0.len() == 1);
-            assert!(limits.0.contains("Windows.Foundation"));
-        }
-
-        {
-            // Windows.Foundation.Collections's parent is not empty so it gets included
-            let mut limits = TypeLimits::default();
-            limits.insert(reader, "windows.foundation.collections");
-            assert!(limits.0.len() == 2);
-            assert!(limits.0.contains("Windows.Foundation"));
-            assert!(limits.0.contains("Windows.Foundation.Collections"));
-        }
-
-        {
-            let mut limits = TypeLimits::default();
-            limits.insert(reader, "windows.foundation.collections");
-            limits.insert(reader, "windows.ui.xaml.controls");
-            assert!(limits.0.len() == 5);
-            assert!(limits.0.contains("Windows.Foundation"));
-            assert!(limits.0.contains("Windows.Foundation.Collections"));
-            assert!(limits.0.contains("Windows.UI"));
-            assert!(limits.0.contains("Windows.UI.Xaml"));
-            assert!(limits.0.contains("Windows.UI.Xaml.Controls"));
-        }
+        self.0.insert(found.to_owned());
     }
 }
