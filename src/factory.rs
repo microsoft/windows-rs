6d2dd9c3902d0a69dbad8962109b61b8349eb2a1
Kenny Kerr <kenny.kerr@microsoft.com>
2020-05-28 08:19:52 -0700

    LoadLibrary

--- src/factory.rs
+++ src/factory.rs
@@ -1,5 +1,10 @@
-use crate::runtime;
+use crate::runtime::*;
 use crate::*;
+
+// extern "system" fn(
+//     RawComPtr<IActivationFactory>,
+//     *mut <Object as RuntimeType>::Abi,
+// ) -> ErrorCode;
 
 // TODO: this should return `Result<&I>` e.g. a reference pointing to the factory cache.
 // So this function needs to be implemented as some sort of atomic/singleton where RoGetActivationFactory
@@ -8,19 +13,60 @@ use crate::*;
 // and implement DLL garbage collection for those. Version 0.1 can probably just pin everything.
 // https://github.com/microsoft/cppwinrt/blob/master/strings/base_activation.h
 pub fn factory<C: RuntimeName, I: ComInterface>() -> Result<I> {
-    let mut ptr = std::ptr::null_mut();
+    let mut factory = std::ptr::null_mut();
     unsafe {
         let mut code =
-            runtime::RoGetActivationFactory(HString::from(C::NAME).abi(), &I::iid(), &mut ptr);
+            RoGetActivationFactory(HString::from(C::NAME).abi(), &I::iid(), &mut factory);
 
         if code == ErrorCode::NOT_INITIALIZED {
             let mut _cookie = std::ptr::null_mut();
-            runtime::CoIncrementMTAUsage(&mut _cookie);
-
-            code =
-                runtime::RoGetActivationFactory(HString::from(C::NAME).abi(), &I::iid(), &mut ptr);
+            CoIncrementMTAUsage(&mut _cookie);
+            code = RoGetActivationFactory(HString::from(C::NAME).abi(), &I::iid(), &mut factory);
         }
 
-        code.and_then(|| std::mem::transmute_copy(&ptr))
+        if code.is_ok() {
+            return Ok(std::mem::transmute_copy(&factory));
+        }
+
+        let original: Error = code.into();
+        let mut path = C::NAME;
+
+        while let Some(pos) = path.rfind('.') {
+            path = &path[..pos];
+
+            let path: Vec<u16> = path
+                .encode_utf16()
+                .chain("dll".encode_utf16())
+                .chain(std::iter::once(0))
+                .collect();
+
+            let library: Library = LoadLibraryW(path.as_ptr()).into();
+
+            if library.handle.is_null() {
+                continue;
+            }
+        }
+
+        Err(original)
+    }
+}
+
+struct Library {
+    handle: RawPtr,
+}
+
+impl Drop for Library {
+    fn drop(&mut self) {
+        if !self.handle.is_null() {
+            unsafe {
+                FreeLibrary(self.handle);
+            }
+        }
+    }
+}
+
+impl From<RawPtr> for Library {
+    fn from(handle: RawPtr) -> Library {
+        Library { handle }
     }
 }
