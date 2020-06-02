5915f21e6d3ec1b29f335d3899f9b9e3b5cfc372
Kenny Kerr <kenny.kerr@microsoft.com>
2020-06-02 09:38:34 -0700

    test

--- tests/namespace.rs
+++ tests/namespace.rs
@@ -1,16 +1,19 @@
 // This test ensures that a multi-word namespace "MachineLearning" is properly
 // converted to "machine_learning" and imports correctly.
 
-winrt::import!(
-    dependencies
-        os
-    types
-        windows::ai::machine_learning::*
-);
+mod generated {
+    winrt::import!(
+        dependencies
+            os
+        types
+            windows::foundation::*
+            windows::ai::machine_learning::*
+    );
+}
 
 #[test]
 fn namespace() -> winrt::Result<()> {
-    use windows::ai::machine_learning::{TensorBoolean, TensorKind};
+    use generated::windows::ai::machine_learning::{TensorBoolean, TensorKind};
 
     let tensor = TensorBoolean::create()?;
     assert!(tensor.tensor_kind()? == TensorKind::Boolean);
