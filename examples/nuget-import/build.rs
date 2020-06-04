17b9deb66defbacbb4ed4cbc591ea741acfe71cb
Ryan Levick <ryan.levick@gmail.com>
2020-06-04 13:10:48 +0200

    Run cargo winrt install on build

--- examples/nuget-import/build.rs
+++ examples/nuget-import/build.rs
@@ -1,12 +1,3 @@
-winrt::build!(
-    dependencies
-        nuget: Microsoft.AI.MachineLearning
-        nuget: Microsoft.Windows.SDK.Contracts
-    types
-        microsoft::ai::machine_learning::*
-        windows::foundation::Uri
-);
-
 use std::process::Command;
 
 fn main() {
@@ -15,6 +6,4 @@ fn main() {
         .output()
         .expect("failed to execute process");
     assert!(output.status.success(), "cargo winrt install failed");
-
-    build();
 }
