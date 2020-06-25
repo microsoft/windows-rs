428b5cb1d2c8fe5aa5e018dc573247e2a981746e
Kenny Kerr <kenny.kerr@microsoft.com>
2020-06-24 17:19:09 -0700

    tests

--- tests/gauntlet.rs
+++ tests/gauntlet.rs
@@ -21,72 +21,86 @@ fn tests() -> winrt::Result<()> {
     let tests = TestRunner::make_tests()?;
 
     {
-        let a = true;
+        let a: bool = true;
         let mut b = false;
         let c = tests.param1(a, &mut b)?;
         assert!(b && c);
     }
 
     {
-        let a = 123;
+        let a: u8 = 123;
         let mut b = 0;
         let c = tests.param2(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 123;
+        let a: u16 = 123;
         let mut b = 0;
         let c = tests.param3(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 123;
+        let a: u32 = 123;
         let mut b = 0;
         let c = tests.param4(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 123;
+        let a: u64 = 123;
         let mut b = 0;
         let c = tests.param5(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 123;
+        let a: i16 = 123;
         let mut b = 0;
         let c = tests.param6(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 123;
+        let a: i32 = 123;
         let mut b = 0;
         let c = tests.param7(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 123;
+        let a: i64 = 123;
         let mut b = 0;
         let c = tests.param8(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 12.3;
+        let a: f32 = 12.3;
         let mut b = 0.0;
         let c = tests.param9(a, &mut b)?;
         assert!(a == b && a == c);
     }
 
     {
-        let a = 12.3;
+        let a: f64 = 12.3;
         let mut b = 0.0;
         let c = tests.param10(a, &mut b)?;
+        assert!(a == b && a == c);
+    }
+
+    {
+        let a: u16 = 0x57; // WinRT char e.g. L'W'
+        let mut b = 0;
+        let c = tests.param11(a, &mut b)?;
+        assert!(a == b && a == c);
+    }
+
+    {
+        let a: winrt::HString = "WinRT".into();
+        let mut b = winrt::HString::new();
+        let c = tests.param12(&a, &mut b)?;
         assert!(a == b && a == c);
     }
 
