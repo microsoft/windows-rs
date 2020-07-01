6cb4ce9edf1750b717f0b491f917754901540ade
Kenny Kerr <kenny.kerr@microsoft.com>
2020-07-01 15:06:36 -0700

    test

--- src/array.rs
+++ src/array.rs
@@ -107,3 +107,25 @@ impl<T: RuntimeType> Drop for Array<T> {
         self.clear();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let empty = Array::<bool>::new();
+        assert!(empty.is_empty());
+        assert!(empty.len() == 0);
+    }
+
+    #[test]
+    fn with_len() {
+        let empty = Array::<u32>::with_len(3);
+        assert!(!empty.is_empty());
+        assert!(empty.len() == 3);
+        assert!(empty[0] == 0);
+        assert!(empty[1] == 0);
+        assert!(empty[2] == 0);
+    }
+}
