ee711c9efe2f02de93a9bff1461c3d68542598b0
Kenny Kerr <kenny.kerr@microsoft.com>
2020-07-08 08:48:11 -0700

    composable

---
+++ tests/composable.rs
@@ -0,0 +1,17 @@
+import!(
+    dependencies
+        nuget: Microsoft.Windows.SDK.Contracts
+        nuget: KennyKerr.Windows.TestWinRT
+    types
+        test_component::Composable
+);
+
+use test_component::Composable;
+use winrt::*;
+
+#[test]
+fn composable() -> Result<()> {
+
+
+    Ok(())
+}
