985216fb5ee95be41506f26d52b3a886289bb2ff
Kenny Kerr <kekerr@microsoft.com>
2020-07-20 14:29:36 -0700

    remove Microsoft.AI.MachineLearning (#273)

--- examples/nuget-build/build.rs
+++ examples/nuget-build/build.rs
@@ -1,9 +1,7 @@
 winrt::build!(
     dependencies
-        nuget: Microsoft.AI.MachineLearning
         nuget: Microsoft.Windows.SDK.Contracts
     types
-        microsoft::ai::machine_learning::*
         windows::foundation::Uri
 );
 
--- examples/nuget-build/src/main.rs
+++ examples/nuget-build/src/main.rs
@@ -1,12 +1,8 @@
 include!(concat!(env!("OUT_DIR"), "/winrt.rs"));
 
 fn main() -> winrt::Result<()> {
-    use microsoft::ai::machine_learning::*;
     use windows::foundation::Uri;
 
-    let tensor = TensorBoolean::create()?;
-    assert!(tensor.tensor_kind()? == TensorKind::Boolean);
-    println!("Tensor: {:?}", tensor);
     let uri = Uri::create_uri("http://kennykerr.ca:80")?;
     println!("Port: {:?}", uri.port()?);
 
