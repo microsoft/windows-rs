492a158b6a58e9b8dec2f912e939f3f6b1e32588
Kenny Kerr <kenny.kerr@microsoft.com>
2020-07-21 01:38:30 -0700

    keywords

--- crates/gen/src/lib.rs
+++ crates/gen/src/lib.rs
@@ -11,9 +11,8 @@ mod type_namespaces;
 mod type_reader;
 mod type_stage;
 mod type_tree;
-pub mod types;
+mod types;
 
-pub use types::Type;
 pub mod dependencies;
 pub mod load_winmd;
 pub use file::WinmdFile;
@@ -26,7 +25,17 @@ fn format_ident(name: &str) -> proc_macro2::Ident {
     if name == "Self" {
         quote::format_ident!("{}_", name)
     } else {
-        quote::format_ident!("r#{}", name)
+        // keywords list based on https://doc.rust-lang.org/reference/keywords.html
+        match name {
+            "abstract" | "as" | "become" | "box" | "break" | "const" | "continue" | "crate"
+            | "do" | "else" | "enum" | "extern" | "false" | "final" | "fn" | "for" | "if"
+            | "impl" | "in" | "let" | "loop" | "macro" | "match" | "mod" | "move" | "mut"
+            | "override" | "priv" | "pub" | "ref" | "return" | "Self" | "self" | "static"
+            | "struct" | "super" | "trait" | "true" | "type" | "typeof" | "unsafe" | "unsized"
+            | "use" | "virtual" | "where" | "while" | "yield" | "try" | "async" | "await"
+            | "dyn" => quote::format_ident!("r#{}", name),
+            _ => quote::format_ident!("{}", name),
+        }
     }
 }
 
