b0665521f6ac7eb67d1c6b2fa1eab98c7d608004
Kenny Kerr <kenny.kerr@microsoft.com>
2020-07-23 14:57:27 -0700

    xaml

--- tests/xaml.rs
+++ tests/xaml.rs
@@ -1,36 +1,36 @@
-// winrt::import!(
-//     dependencies
-//         os
-//     types
-//         windows::ui::xaml::*
-// );
+winrt::import!(
+    dependencies
+        os
+    types
+        windows::ui::xaml::*
+);
 
-// use windows::ui::xaml::*;
-// use winrt::ComInterface;
+use windows::ui::xaml::*;
+use winrt::ComInterface;
 
-// #[test]
-// fn xaml() -> winrt::Result<()> {
-//     let app = Application::default();
-//     assert_eq!(app.is_null(), true);
+#[test]
+fn xaml() -> winrt::Result<()> {
+    let app = Application::default();
+    assert_eq!(app.is_null(), true);
 
-//     let app = Application::new()?;
-//     assert_eq!(app.is_null(), false);
+    let app = Application::new()?;
+    assert_eq!(app.is_null(), false);
 
-//     // The DataTemplateKey "constructors" fail because they're not
-//     // satisfying preconditions, but they still validate that the
-//     // composable constructors are being generated correctly.
+    // The DataTemplateKey "constructors" fail because they're not
+    // satisfying preconditions, but they still validate that the
+    // composable constructors are being generated correctly.
 
-//     assert_eq!(
-//         DataTemplateKey::new().unwrap_err().code(),
-//         winrt::ErrorCode(0x8001_010E) // wrong thread
-//     );
+    assert_eq!(
+        DataTemplateKey::new().unwrap_err().code(),
+        winrt::ErrorCode(0x8001_010E) // wrong thread
+    );
 
-//     assert_eq!(
-//         DataTemplateKey::create_instance_with_type(winrt::Object::default())
-//             .unwrap_err()
-//             .code(),
-//         winrt::ErrorCode(0x8007_0057) // invalid argument
-//     );
+    assert_eq!(
+        DataTemplateKey::create_instance_with_type(winrt::Object::default())
+            .unwrap_err()
+            .code(),
+        winrt::ErrorCode(0x8007_0057) // invalid argument
+    );
 
-//     Ok(())
-// }
+    Ok(())
+}
