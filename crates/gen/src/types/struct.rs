18ba98c4faa82554cc197eb2890f388a0a10bd1a
Robert Mikhayelyan <rob.mikh@outlook.com>
2020-07-27 23:56:25 -0700

    start matrix extensions

--- crates/gen/src/types/struct.rs
+++ crates/gen/src/types/struct.rs
@@ -83,16 +83,28 @@ impl Struct {
                 | ("Windows.Foundation.Numerics", "Vector4") 
             => {
                 let ops = [Operation::Add, Operation::Sub, Operation::Div, Operation::Mul];
-                let impls = {
-                    let mut impls = Vec::new();
-                    for op in &ops {
-                        let tokens = op.to_tokens(self);
-                        impls.push(quote!{ #tokens });
+                let impls = ops.iter().map(|op| {
+                    let impl_with_self = op.to_tokens_with_self(self);
+                    let impl_with_scalar = match op {
+                        Operation::Div | Operation::Mul => op.to_tokens_with_scalar(self),
+                        _ => quote!{}
+                    };
+                    quote! { 
+                        #impl_with_self
+                        #impl_with_scalar
                     }
-                    TokenStream::from_iter(impls)
-                };
-                quote! {
-                    #impls
+                });
+                TokenStream::from_iter(impls)
+            }
+            ("Windows.Foundation.Numerics", "Matrix3x2")
+                | ("Windows.Foundation.Numerics", "Matrix4x4")
+            => {
+                let ops = [Operation::Add, Operation::Sub];
+                let impl_with_self = ops.iter().map(|op| {
+                    op.to_tokens_with_self(self)
+                });
+                quote!{
+                    #(#impl_with_self)*
                 }
             }
             _ => { quote!{} }
@@ -109,67 +121,70 @@ enum Operation {
 }
 
 impl Operation {
-    pub fn to_tokens(&self, winrt_struct: &Struct) -> TokenStream {
+    pub fn to_tokens_with_self(&self, winrt_struct: &Struct) -> TokenStream {
         let struct_name = &winrt_struct.name.tokens;
-        let (op_name, op_fn, symbol) = self.get_name_and_symbol();
+        let (op_name, op_fn, symbol) = self.get_name_fn_and_symbol();
         let fields = winrt_struct.fields.iter().map(|field| {
             let name = format_ident(&field.0);
             quote! {
                 #name: self.#name #symbol rhs.#name
             }
         });
+        let permutations = Self::get_self_permutations(struct_name);
+        let impls = permutations.iter().map(|(lhs, rhs)| {
+            let fields = fields.clone();
+            quote! {
+                impl std::ops::#op_name<#rhs> for #lhs {
+                    type Output = #struct_name;
+                    fn #op_fn(self, rhs: #rhs) -> #struct_name {
+                        #struct_name {
+                            #(#fields),*
+                        }
+                    }
+                }
+            }
+        });
+        TokenStream::from_iter(impls)
+    }
+
+    pub fn to_tokens_with_scalar(&self, winrt_struct: &Struct) -> TokenStream {
+        let struct_name = &winrt_struct.name.tokens;
+        let (op_name, op_fn, symbol) = self.get_name_fn_and_symbol();
+        let fields = winrt_struct.fields.iter().map(|field| {
+            let name = format_ident(&field.0);
+            quote! {
+                #name: self.#name #symbol rhs
+            }
+        });
         let permutations = vec![
+            struct_name.clone(), quote!{ &#struct_name },
+        ];
+        let impls = permutations.iter().map(|lhs| {
+            let fields = fields.clone();
+            quote! {
+                impl std::ops::#op_name<f32> for #lhs {
+                    type Output = #struct_name;
+                    fn #op_fn(self, rhs: f32) -> #struct_name {
+                        #struct_name {
+                            #(#fields),*
+                        }
+                    }
+                }
+            }
+        });
+        TokenStream::from_iter(impls)
+    }
+
+    fn get_self_permutations(struct_name: &TokenStream) -> Vec<(TokenStream, TokenStream)> {
+        vec![
             ( struct_name.clone(), struct_name.clone() ),
             ( struct_name.clone(), quote!{ &#struct_name }),
             ( quote!{ &#struct_name }, struct_name.clone() ),
             ( quote!{ &#struct_name }, quote!{ &#struct_name }),
-        ];
-        let versions = {
-            let mut versions = Vec::new();
-            for (lhs, rhs) in permutations {
-                let fields = fields.clone();
-                versions.push(quote! {
-                    impl std::ops::#op_name<#rhs> for #lhs {
-                        type Output = #struct_name;
-                        fn #op_fn(self, rhs: #rhs) -> #struct_name {
-                            #struct_name {
-                                #(#fields),*
-                            }
-                        }
-                    }
-                });
-            }
-            match self {
-                Operation::Div | Operation::Mul => {
-                    let fields = winrt_struct.fields.iter().map(|field| {
-                        let name = format_ident(&field.0);
-                        quote! {
-                            #name: self.#name #symbol rhs
-                        }
-                    });
-                    let permutations = vec![struct_name.clone(), quote!{ &#struct_name }];
-                    for impl_type in permutations {
-                        let fields = fields.clone();
-                        versions.push(quote! {
-                            impl std::ops::#op_name<f32> for #impl_type {
-                                type Output = #struct_name;
-                                fn #op_fn(self, rhs: f32) -> #struct_name {
-                                    #struct_name {
-                                        #(#fields),*
-                                    }
-                                }
-                            }
-                        });
-                    }
-                },
-                _ => (),
-            }
-            TokenStream::from_iter(versions)
-        };
-        versions
+        ]
     }
 
-    fn get_name_and_symbol(&self) -> (TokenStream, TokenStream, TokenStream) {
+    fn get_name_fn_and_symbol(&self) -> (TokenStream, TokenStream, TokenStream) {
         match self {
             Operation::Add => (quote!{Add}, quote!{add}, quote!{+}),
             Operation::Sub => (quote!{Sub}, quote!{sub}, quote!{-}),
