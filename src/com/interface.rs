35565682a38fc01ad5fa0082ef7c741ef9122848
Kenny Kerr <kenny.kerr@microsoft.com>
2020-08-03 08:59:18 -0700

    const

--- src/com/interface.rs
+++ src/com/interface.rs
@@ -45,7 +45,7 @@ pub unsafe trait ComInterface: Sized + crate::AbiTransferable {
         let from = self.as_iunknown();
 
         if let Some(ptr) = from {
-            unsafe { (ptr.vtable().unknown_query_interface)(ptr, &Into::iid(), &mut into).ok()? };
+            unsafe { (ptr.vtable().unknown_query_interface)(ptr, &Into::IID, &mut into).ok()? };
 
             debug_assert!(
                 !into.is_null(),
