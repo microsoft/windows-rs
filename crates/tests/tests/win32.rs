e40fa33a24e60fad9e99006843ae92a8775116de
Kenny Kerr <kenny.kerr@microsoft.com>
2020-12-02 11:47:13 -0800

    enum32 test

---
+++ crates/tests/tests/win32.rs
@@ -0,0 +1,18 @@
+use tests::microsoft::windows::sdk::win32::{ACCESS_MODE, DXGI_ADAPTER_FLAG};
+use winrt::Abi;
+
+#[test]
+fn signed_enum32() {
+    assert!(ACCESS_MODE::default().abi() == 0);
+    assert!(ACCESS_MODE::REVOKE_ACCESS.abi() == 4);
+}
+
+#[test]
+fn unsigned_enum32() {
+    assert!(DXGI_ADAPTER_FLAG::default().abi() == 0);
+    assert!(DXGI_ADAPTER_FLAG::DXGI_ADAPTER_FLAG_SOFTWARE.abi() == 2);
+
+    let both =
+        DXGI_ADAPTER_FLAG::DXGI_ADAPTER_FLAG_SOFTWARE | DXGI_ADAPTER_FLAG::DXGI_ADAPTER_FLAG_REMOTE;
+    assert!(both.abi() == 3);
+}
