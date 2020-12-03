8dff55c01b5d049d92b7c4408a4fb0359eeb9fb8
Kenny Kerr <kekerr@microsoft.com>
2020-12-03 07:53:31 -0800

    Ensure completed handler doesn't capture waiter (#374)

--- src/runtime/waiter.rs
+++ src/runtime/waiter.rs
@@ -1,24 +1,26 @@
 use crate::*;
 
-/// A simple blocking waiter used by the generated bindings and should not generally be used directly.
+/// A simple blocking waiter used by the generated bindings and should not be used directly.
 pub struct Waiter(RawPtr);
+pub struct WaiterSignaler(RawPtr);
 
 impl Waiter {
-    pub fn new() -> Self {
+    pub fn new() -> (Waiter, WaiterSignaler) {
         unsafe {
-            Self(CreateEventW(
-                std::ptr::null_mut(),
-                1,
-                0,
-                std::ptr::null_mut(),
-            ))
+            let handle = CreateEventW(std::ptr::null_mut(), 1, 0, std::ptr::null_mut());
+            (Waiter(handle), WaiterSignaler(handle))
         }
     }
+}
 
-    pub fn signal(&self) {
-        unsafe {
-            SetEvent(self.0);
-        }
+impl WaiterSignaler {
+    /// Signals the `Waiter`. This is unsafe because the lifetime of `WaiterSignaler` is not tied
+    /// to the lifetime of the `Waiter`. This is not possible in this case because the `Waiter`
+    /// is used to signal a WinRT async completion and the compiler doesn't know that the lifetime
+    /// of the delegate is bounded by the calling function.
+    pub unsafe fn signal(&self) {
+        // https://github.com/microsoft/winrt-rs/pull/374#discussion_r535313344
+        SetEvent(self.0);
     }
 }
 
