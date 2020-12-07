4e6c9a5cd927222eb17c0645eda3f81614b2fb45
Kenny Kerr <kenny.kerr@microsoft.com>
2020-12-07 15:32:19 -0800

    test

--- crates/tests/tests/win32.rs
+++ crates/tests/tests/win32.rs
@@ -1,4 +1,7 @@
-use tests::windows::win32::{ACCESS_MODE, DXGI_ADAPTER_FLAG};
+use tests::windows::win32::{
+    ACCESS_MODE, DXGI_ADAPTER_FLAG, DXGI_FORMAT, DXGI_MODE_DESC, DXGI_MODE_SCALING,
+    DXGI_MODE_SCANLINE_ORDER, DXGI_RATIONAL, RECT,
+};
 use winrt::Abi;
 
 #[test]
@@ -18,4 +21,31 @@ fn unsigned_enum32() {
 }
 
 #[test]
-fn rect() {}
+fn rect() {
+    let rect = RECT {
+        left: 1,
+        top: 2,
+        right: 3,
+        bottom: 4,
+    };
+
+    assert!(rect.left == 1);
+    assert!(rect.top == 2);
+    assert!(rect.right == 3);
+    assert!(rect.bottom == 4);
+}
+
+#[test]
+fn dxgi_mode_desc() {
+    let _ = DXGI_MODE_DESC {
+        Width: 1,
+        Height: 2,
+        RefreshRate: DXGI_RATIONAL {
+            Numerator: 3,
+            Denominator: 5,
+        },
+        Format: DXGI_FORMAT::DXGI_FORMAT_R32_TYPELESS,
+        ScanlineOrdering: DXGI_MODE_SCANLINE_ORDER::DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE,
+        Scaling: DXGI_MODE_SCALING::DXGI_MODE_SCALING_CENTERED,
+    };
+}
