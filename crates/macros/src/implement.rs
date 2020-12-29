396aa5b73e1e3bbf0d8a0db5a180407e2f7e1fae
Gyusun Yeom <omniavinco@gmail.com>
2020-12-29 17:31:10 +0900

    Use escaped method name for implement

--- crates/macros/src/implement.rs
+++ crates/macros/src/implement.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use squote::{format_ident, quote, Literal, TokenStream};
+use winrt_gen::format_ident;
 
 // TODO: distinguish between COM and WinRT interfaces
 struct Implements(Vec<winrt_gen::TypeDefinition>);
@@ -160,7 +161,7 @@ pub fn gen(
             let interface_literal = Literal::u32_unsuffixed(interface_count as u32);
 
             for method in &t.default_interface().methods {
-                let method_ident = format_ident!("{}", method.name);
+                let method_ident = format_ident(&method.name);
                 let vcall_ident = format_ident!("abi{}_{}", interface_count, method.vtable_offset);
 
                 vtable_ptrs.combine(&quote! {
