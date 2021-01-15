260f7e9d776ee5c9444d110211776265a249c3dc
Kenny Kerr <kenny.kerr@microsoft.com>
2021-01-14 21:47:11 -0800

    enum

--- crates/tests/tests/enum.rs
+++ crates/tests/tests/enum.rs
@@ -8,6 +8,12 @@ fn signed_enum() {
     assert!(AsyncStatus::Completed == 1.into());
     assert!(AsyncStatus::Error == 3.into());
     assert!(AsyncStatus::Started == 0.into());
+
+    assert!(AsyncStatus::default().0 == 0);
+    assert!(AsyncStatus::Canceled.0 == 2);
+    assert!(AsyncStatus::Completed.0 == 1);
+    assert!(AsyncStatus::Error.0 == 3);
+    assert!(AsyncStatus::Started.0 == 0);
 }
 
 #[test]
@@ -22,7 +28,18 @@ fn unsigned_enum() {
     assert!(AppointmentDaysOfWeek::Friday == 0x20.into());
     assert!(AppointmentDaysOfWeek::Saturday == 0x40.into());
 
+    assert!(AppointmentDaysOfWeek::default().0 == 0);
+    assert!(AppointmentDaysOfWeek::None.0 == 0);
+    assert!(AppointmentDaysOfWeek::Sunday.0 == 0x1);
+    assert!(AppointmentDaysOfWeek::Monday.0 == 0x2);
+    assert!(AppointmentDaysOfWeek::Tuesday.0 == 0x4);
+    assert!(AppointmentDaysOfWeek::Wednesday.0 == 0x8);
+    assert!(AppointmentDaysOfWeek::Thursday.0 == 0x10);
+    assert!(AppointmentDaysOfWeek::Friday.0 == 0x20);
+    assert!(AppointmentDaysOfWeek::Saturday.0 == 0x40);
+
     // Use as bitflags
     let weekend = AppointmentDaysOfWeek::Sunday | AppointmentDaysOfWeek::Saturday;
     assert!(weekend == 0x41.into());
+    assert!(weekend.0 == 0x41);
 }
