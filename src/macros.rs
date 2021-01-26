bbd0f784f23afd649306843e5dff3bf48a7204fd
Ajeet D'Souza <98ajeet@gmail.com>
2021-01-26 19:18:46 +0530

    Fixup

--- src/macros.rs
+++ src/macros.rs
@@ -14,23 +14,22 @@ macro_rules! demand_load {
     } )* ) => {
         $($(
             #[allow(non_snake_case)]
-            unsafe fn $sym( $( $param: $pty ),* ) -> ::std::result::Result<$rt, ErrorCode> {
-                use ::std::{sync::Once, mem::MaybeUninit};
-
-                static ONCE: Once = Once::new();
-                static mut VALUE: MaybeUninit<::std::result::Result<RawPtr, ErrorCode>> = MaybeUninit::uninit();
+            unsafe fn $sym( $( $param: $pty ),* ) -> ::std::result::Result<$rt, $crate::ErrorCode> {
+                static ONCE: ::std::sync::Once = ::std::sync::Once::new();
+                static mut VALUE: ::std::mem::MaybeUninit<::std::result::Result<$crate::RawPtr, $crate::ErrorCode>> =
+                    ::std::mem::MaybeUninit::uninit();
                 const LOAD_LIBRARY_SEARCH_SYSTEM32: u32 = 0x0000_0800;
 
                 ONCE.call_once(|| {
-                    VALUE = MaybeUninit::new(
-                        delay_load($library, stringify!($sym), LOAD_LIBRARY_SEARCH_SYSTEM32)
+                    VALUE = ::std::mem::MaybeUninit::new(
+                        $crate::delay_load($library, ::std::stringify!($sym), LOAD_LIBRARY_SEARCH_SYSTEM32)
                     )
                 });
 
                 // transmute() doesn't work on generic types, as you can't constrain to a
                 // function pointer, so it must be done here outside load_proc().
                 type FnPtr = extern "system" fn ( $( $param: $pty ),* ) -> $rt;
-                let f = ::std::mem::transmute::<RawPtr, FnPtr>(VALUE.assume_init()?);
+                let f = ::std::mem::transmute::<$crate::RawPtr, FnPtr>(VALUE.assume_init()?);
                 Ok( (f)( $( $param ),* ) )
             }
         )*)*
