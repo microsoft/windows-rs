2a5dc48b04c97a0728b6a474f73f2e39a3019f9f
Ryan Levick <rylev@users.noreply.github.com>
2021-02-01 17:57:32 +0100

    Improve BOOL (#483)
    * Improve BOOL
    
    * Fix tests

--- src/result/bool32.rs
+++ src/result/bool32.rs
@@ -1,21 +1,20 @@
-use crate::*;
+use crate::{Abi, ErrorCode};
 
 /// A 32-bit boolean error code value returned by some Win32 functions.
 #[repr(transparent)]
-#[derive(Copy, Clone, Default, Debug, PartialEq)]
+#[derive(Copy, Clone, Default, PartialEq)]
 pub struct BOOL(pub i32);
 
-impl BOOL {
-    /// Returns `true` if `self` is a success code.
-    #[inline]
-    pub fn is_ok(self) -> bool {
-        self.0 != 0
-    }
+/// A BOOL representing true
+pub const TRUE: BOOL = BOOL(1);
+/// A BOOL representing false
+pub const FALSE: BOOL = BOOL(0);
 
-    /// Returns `true` if `self` is a failure code.
+impl BOOL {
+    /// Convert the `BOOL` into a `bool`
     #[inline]
-    pub fn is_err(self) -> bool {
-        self.0 == 0
+    pub fn as_bool(self) -> bool {
+        self.into()
     }
 
     /// Asserts that `self` is a success code.
@@ -24,10 +23,16 @@ impl BOOL {
         self.ok().unwrap();
     }
 
+    /// Expects that `self` is a success code.
+    #[inline]
+    pub fn expects(self, msg: &str) {
+        self.ok().expect(msg);
+    }
+
     /// Converts the `BOOL` to `Result<()>`.
     #[inline]
-    pub fn ok(self) -> Result<()> {
-        if self.is_ok() {
+    pub fn ok(self) -> crate::Result<()> {
+        if self == TRUE {
             Ok(())
         } else {
             Err(ErrorCode::from_thread().into())
@@ -39,18 +44,54 @@ unsafe impl Abi for BOOL {
     type Abi = Self;
 }
 
+impl From<BOOL> for bool {
+    fn from(value: BOOL) -> Self {
+        value == TRUE
+    }
+}
+
 impl From<bool> for BOOL {
-    fn from(value: bool) -> Self {
-        if value {
-            Self(1)
+    fn from(b: bool) -> Self {
+        if b {
+            TRUE
         } else {
-            Self(0)
+            FALSE
         }
     }
 }
 
-impl From<BOOL> for bool {
-    fn from(value: BOOL) -> Self {
-        value.0 != 0
+impl std::fmt::Debug for BOOL {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = if *self == FALSE { "FALSE" } else { "TRUE" };
+        f.write_str(msg)
+    }
+}
+
+impl PartialEq<bool> for BOOL {
+    fn eq(&self, other: &bool) -> bool {
+        let other: BOOL = (*other).into();
+        self == &other
+    }
+}
+
+impl std::ops::Neg for BOOL {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        if self == TRUE {
+            FALSE
+        } else {
+            TRUE
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn bool_interop() {
+        let win_bool = TRUE;
+        let bool = true;
+        assert_eq!(win_bool, bool);
     }
 }
