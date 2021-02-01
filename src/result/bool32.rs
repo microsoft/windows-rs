971b558f46266a900f0997e1e8b23ff4e24a294e
Kenny Kerr <kekerr@microsoft.com>
2021-02-01 12:55:01 -0800

    Fix a few issues with BOOL (#492)

--- src/result/bool32.rs
+++ src/result/bool32.rs
@@ -2,19 +2,24 @@ use crate::{Abi, ErrorCode};
 
 /// A 32-bit boolean error code value returned by some Win32 functions.
 #[repr(transparent)]
-#[derive(Copy, Clone, Default, PartialEq)]
+#[derive(Copy, Clone, Default)]
 pub struct BOOL(pub i32);
 
-/// A BOOL representing true
+/// A BOOL representing true.
 pub const TRUE: BOOL = BOOL(1);
-/// A BOOL representing false
+
+/// A BOOL representing false.
 pub const FALSE: BOOL = BOOL(0);
 
 impl BOOL {
-    /// Convert the `BOOL` into a `bool`
+    /// Convert the `BOOL` into a `bool`.
     #[inline]
     pub fn as_bool(self) -> bool {
-        self.into()
+        if self.0 == 0 {
+            false
+        } else {
+            true
+        }
     }
 
     /// Asserts that `self` is a success code.
@@ -25,14 +30,14 @@ impl BOOL {
 
     /// Expects that `self` is a success code.
     #[inline]
-    pub fn expects(self, msg: &str) {
+    pub fn expect(self, msg: &str) {
         self.ok().expect(msg);
     }
 
     /// Converts the `BOOL` to `Result<()>`.
     #[inline]
     pub fn ok(self) -> crate::Result<()> {
-        if self == TRUE {
+        if self.as_bool() {
             Ok(())
         } else {
             Err(ErrorCode::from_thread().into())
@@ -46,13 +51,19 @@ unsafe impl Abi for BOOL {
 
 impl From<BOOL> for bool {
     fn from(value: BOOL) -> Self {
-        value == TRUE
+        value.as_bool()
+    }
+}
+
+impl From<&BOOL> for bool {
+    fn from(value: &BOOL) -> Self {
+        value.as_bool()
     }
 }
 
 impl From<bool> for BOOL {
-    fn from(b: bool) -> Self {
-        if b {
+    fn from(value: bool) -> Self {
+        if value {
             TRUE
         } else {
             FALSE
@@ -60,24 +71,41 @@ impl From<bool> for BOOL {
     }
 }
 
+impl From<&bool> for BOOL {
+    fn from(value: &bool) -> Self {
+        (*value).into()
+    }
+}
+
 impl std::fmt::Debug for BOOL {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let msg = if *self == FALSE { "FALSE" } else { "TRUE" };
+        let msg = if self.as_bool() { "TRUE" } else { "FALSE" };
         f.write_str(msg)
+    }
+}
+
+impl PartialEq<BOOL> for BOOL {
+    fn eq(&self, other: &BOOL) -> bool {
+        self.as_bool() == other.as_bool()
     }
 }
 
 impl PartialEq<bool> for BOOL {
     fn eq(&self, other: &bool) -> bool {
-        let other: BOOL = (*other).into();
-        self == &other
+        self.as_bool() == *other
     }
 }
 
-impl std::ops::Neg for BOOL {
+impl PartialEq<BOOL> for bool {
+    fn eq(&self, other: &BOOL) -> bool {
+        *self == other.as_bool()
+    }
+}
+
+impl std::ops::Not for BOOL {
     type Output = Self;
-    fn neg(self) -> Self::Output {
-        if self == TRUE {
+    fn not(self) -> Self::Output {
+        if self.as_bool() {
             FALSE
         } else {
             TRUE
@@ -88,10 +116,96 @@ impl std::ops::Neg for BOOL {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn bool_interop() {
-        let win_bool = TRUE;
-        let bool = true;
-        assert_eq!(win_bool, bool);
+    fn comparison() {
+        let win_bool: BOOL = TRUE;
+        let rust_bool: bool = true;
+        assert_eq!(rust_bool, win_bool);
+
+        let win_bool: BOOL = FALSE;
+        let rust_bool: bool = false;
+        assert_eq!(win_bool, rust_bool);
+
+        let win_bool = BOOL(123);
+        let rust_bool: bool = true;
+        assert_eq!(rust_bool, win_bool);
+
+        let win_bool = BOOL(-123);
+        let rust_bool: bool = true;
+        assert_eq!(win_bool, rust_bool);
+
+        let a: BOOL = TRUE;
+        let b: BOOL = BOOL(123);
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    fn win_bool_to_rust_bool() {
+        let win_bool: BOOL = BOOL(123);
+        let rust_bool: bool = win_bool.as_bool();
+        assert!(rust_bool);
+
+        let rust_bool: bool = win_bool.into();
+        assert!(rust_bool);
+
+        let win_bool_ref: &BOOL = &win_bool;
+        let rust_bool: bool = win_bool_ref.as_bool();
+        assert!(rust_bool);
+
+        let rust_bool: bool = win_bool_ref.into();
+        assert!(rust_bool);
+    }
+
+    #[test]
+    fn rust_bool_to_win_bool() {
+        let rust_bool: bool = true;
+        let win_bool: BOOL = rust_bool.into();
+        assert_eq!(win_bool, true);
+
+        let rust_bool: &bool = &rust_bool;
+        let win_bool: BOOL = rust_bool.into();
+        assert_eq!(win_bool, true);
+
+        let rust_bool: bool = false;
+        let win_bool: BOOL = rust_bool.into();
+        assert_eq!(win_bool, false);
+
+        let rust_bool: &bool = &rust_bool;
+        let win_bool: BOOL = rust_bool.into();
+        assert_eq!(win_bool, false);
+    }
+
+    #[test]
+    fn methods() {
+        let win_bool: BOOL = BOOL(123);
+        assert!(win_bool.as_bool());
+        win_bool.unwrap();
+        win_bool.expect("test");
+        assert!(win_bool.ok().is_ok());
+
+        let win_bool: BOOL = FALSE;
+        assert!(!win_bool.as_bool());
+        assert!(win_bool.ok().is_err());
+    }
+
+    #[test]
+    fn format() {
+        assert_eq!(format!("{:?}", TRUE), "TRUE");
+        assert_eq!(format!("{:?}", FALSE), "FALSE");
+        assert_eq!(format!("{:?}", BOOL(123)), "TRUE");
+    }
+
+    #[test]
+    fn not() {
+        let win_bool: BOOL = TRUE;
+        let not: BOOL = !win_bool;
+        assert!(not != win_bool);
+        assert!(not == FALSE);
+
+        let win_bool: BOOL = FALSE;
+        let not: BOOL = !win_bool;
+        assert!(not != win_bool);
+        assert!(not == TRUE);
     }
 }
