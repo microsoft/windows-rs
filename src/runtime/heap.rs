4aedb89556593c8a00f15ee7dede5f748babef9f
Kenny Kerr <kenny.kerr@microsoft.com>
2021-03-09 11:52:24 -0800

    links

--- src/runtime/heap.rs
+++ src/runtime/heap.rs
@@ -3,6 +3,7 @@ use crate::*;
 use bindings::windows::win32::system_services::{GetProcessHeap, HeapAlloc, HeapFree, HeapHandle};
 
 pub fn heap_alloc(bytes: usize) -> RawPtr {
+    // https://github.com/microsoft/win32metadata/issues/322
     unsafe { HeapAlloc(HeapHandle(GetProcessHeap().0), 0, bytes) }
 }
 
