6d5f1caa55c67030f2e8c8886251e7346b8dff25
Kenny Kerr <kenny.kerr@microsoft.com>
2021-04-22 11:52:30 -0700

    weak_ref_count

--- src/runtime/weak_ref_count.rs
+++ src/runtime/weak_ref_count.rs
@@ -1,25 +1,23 @@
+// Implementation of a weak reference count and tearoff
+
+// Somehow try to bake everything in here so we can keep the implement macro simple.
+
 use std::sync::atomic::{fence, AtomicI32, Ordering};
 
-/// A thread-safe reference count for use with COM/HSTRING implementations.
+/// A thread-safe reference count for use with COM weak reference implementations.
 #[repr(transparent)]
 #[derive(Default)]
-pub struct RefCount(AtomicI32);
+pub struct WeakRefCount(AtomicI32);
 
-impl RefCount {
-    /// Creates a new `RefCount` with an initial value of `1`.
+impl WeakRefCount {
     pub fn new() -> Self {
         Self(AtomicI32::new(1))
     }
 
-    /// Increments the reference count, returning the new value.
     pub fn add_ref(&self) -> u32 {
         (self.0.fetch_add(1, Ordering::Relaxed) + 1) as u32
     }
 
-    /// Decrements the reference count, returning the new value.
-    ///
-    /// This operation inserts an `Acquire` fence when the reference count reaches zero.
-    /// This prevents reordering before the object is destroyed.
     pub fn release(&self) -> u32 {
         let remaining = self.0.fetch_sub(1, Ordering::Release) - 1;
 
