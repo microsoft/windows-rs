microsoft/windows-rs

commit fe8955c3577bbece28207f1596f604dc9ee2ccf8

Transform QueryInterface-like functions into generic functions (#735)

crates/gen/src/types/function.rs
@@ -52,13 +52,30 @@ impl Function {
         }
 
         if cfg!(windows) {
-            quote! {
-                pub unsafe fn #name<#constraints>(#params) #return_type {
-                    #[link(name = #link)]
-                    extern "system" {
-                        pub fn #name(#(#abi_params),*) #abi_return_type;
+            if signature.has_query_interface() {
+                let leading_params = &signature.params[..signature.params.len() - 2];
+                let params = signature.gen_win32_params(leading_params, gen);
+                let args = leading_params.iter().map(|p| p.gen_win32_abi_arg());
+
+                quote! {
+                    pub unsafe fn #name<#constraints T: ::windows::Interface>(#params) -> ::windows::Result<T> {
+                        #[link(name = #link)]
+                        extern "system" {
+                            pub fn #name(#(#abi_params),*) #abi_return_type;
+                        }
+                        let mut result__ = ::std::option::Option::None;
+                        #name(#(#args,)* &<T as ::windows::Interface>::IID, ::windows::Abi::set_abi(&mut result__)).and_some(result__)
                     }
-                    #name(#(#args),*)
+                }
+            } else {
+                quote! {
+                    pub unsafe fn #name<#constraints>(#params) #return_type {
+                        #[link(name = #link)]
+                        extern "system" {
+                            pub fn #name(#(#abi_params),*) #abi_return_type;
+                        }
+                        #name(#(#args),*)
+                    }
                 }
             }
         } else {

crates/gen/src/parser/element_type.rs
@@ -38,6 +38,12 @@ pub enum ElementType {
     Callback(types::Callback),
 }
 
+impl Default for ElementType {
+    fn default() -> Self {
+        Self::Void
+    }
+}
+
 impl ElementType {
     pub fn row(&self) -> Row {
         match self {

crates/gen/src/parser/method_signature.rs
@@ -22,6 +22,15 @@ impl MethodSignature {
             .collect()
     }
 
+    pub fn has_query_interface(&self) -> bool {
+        self.return_type.as_ref().map_or(false, |signature| {
+            signature.kind == ElementType::HRESULT
+                && self.params.len() >= 2
+                && self.params[self.params.len() - 2].signature.kind == ElementType::Guid
+                && self.params[self.params.len() - 1].signature.kind == ElementType::Void
+        })
+    }
+
     pub fn gen_winrt_constraint(&self, gen: &Gen) -> TokenStream {
         let params = self.params.iter().map(|p| p.gen_winrt_produce_type(gen));
 
@@ -216,7 +225,7 @@ impl MethodSignature {
 
     pub fn gen_constraints(&self, params: &[MethodParam]) -> TokenStream {
         if params.iter().any(|param| param.is_convertible()) {
-            quote! { 'a }
+            quote! { 'a, }
         } else {
             quote! {}
         }

crates/gen/src/parser/signature.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Default)]
 pub struct Signature {
     pub kind: ElementType,
     pub pointers: usize,

crates/gen/src/types/com_interface.rs
@@ -127,11 +127,25 @@ impl ComInterface {
 
                 let vtable_offset = Literal::usize_unsuffixed(vtable_offset + 3);
 
-                quote! {
-                    pub unsafe fn #name<#constraints>(&self, #params) #return_type {
-                        #udt_return_local
-                        (::windows::Interface::vtable(self).#vtable_offset)(::windows::Abi::abi(self), #(#args,)* #udt_return_type)
-                        #udt_return_expression
+                if signature.has_query_interface() {
+                    let leading_params = &signature.params[..signature.params.len() - 2];
+                    let params = signature.gen_win32_params(leading_params, gen);
+                    let args = leading_params.iter().map(|p| p.gen_win32_abi_arg());
+
+                    quote! {
+                        pub unsafe fn #name<#constraints T: ::windows::Interface>(&self, #params) -> ::windows::Result<T> {
+                            let mut result__ = ::std::option::Option::None;
+                            (::windows::Interface::vtable(self).#vtable_offset)(::windows::Abi::abi(self), #(#args,)* &<T as ::windows::Interface>::IID, ::windows::Abi::set_abi(&mut result__)).and_some(result__)
+                        }
+                    }
+                }
+                else {
+                    quote! {
+                        pub unsafe fn #name<#constraints>(&self, #params) #return_type {
+                            #udt_return_local
+                            (::windows::Interface::vtable(self).#vtable_offset)(::windows::Abi::abi(self), #(#args,)* #udt_return_type)
+                            #udt_return_expression
+                        }
                     }
                 }
             });

examples/clock/src/main.rs
@@ -60,7 +60,7 @@ impl Angles {
 impl Window {
     fn new() -> Result<Self> {
         let factory = create_factory()?;
-        let dxfactory = create_dxfactory()?;
+        let dxfactory: IDXGIFactory2 = unsafe { CreateDXGIFactory1()? };
         let style = create_style(&factory)?;
         let manager: IUIAnimationManager = create_instance(&UIAnimationManager)?;
         let transition = create_transition()?;
@@ -547,13 +547,6 @@ fn create_factory() -> Result<ID2D1Factory1> {
     }
 }
 
-fn create_dxfactory() -> Result<IDXGIFactory2> {
-    unsafe {
-        let mut dxfactory: Option<IDXGIFactory2> = None;
-        CreateDXGIFactory1(&IDXGIFactory2::IID, dxfactory.set_abi()).and_some(dxfactory)
-    }
-}
-
 fn create_style(factory: &ID2D1Factory1) -> Result<ID2D1StrokeStyle> {
     let props = D2D1_STROKE_STYLE_PROPERTIES {
         startCap: D2D1_CAP_STYLE::D2D1_CAP_STYLE_ROUND,
@@ -647,23 +640,15 @@ fn get_dxgi_factory(device: &ID3D11Device) -> Result<IDXGIFactory2> {
     let dxdevice = device.cast::<IDXGIDevice>()?;
     let mut adapter = None;
     unsafe {
-        let adapter = dxdevice.GetAdapter(&mut adapter).and_some(adapter)?;
-        let mut parent = None;
-
-        adapter
-            .GetParent(&IDXGIFactory2::IID, parent.set_abi())
-            .and_some(parent)
+        dxdevice
+            .GetAdapter(&mut adapter)
+            .and_some(adapter)?
+            .GetParent()
     }
 }
 
 fn create_swapchain_bitmap(swapchain: &IDXGISwapChain1, target: &ID2D1DeviceContext) -> Result<()> {
-    let mut surface = None;
-
-    let surface: IDXGISurface = unsafe {
-        swapchain
-            .GetBuffer(0, &IDXGISurface::IID, surface.set_abi())
-            .and_some(surface)?
-    };
+    let surface: IDXGISurface = unsafe { swapchain.GetBuffer(0)? };
 
     let props = D2D1_BITMAP_PROPERTIES1 {
         pixelFormat: D2D1_PIXEL_FORMAT {

src/bindings.rs
@@ -2211,13 +2211,11 @@ pub mod Windows {
                     self.0.bitand_assign(rhs.0)
                 }
             }
-            pub unsafe fn CoCreateInstance<'a>(
+            pub unsafe fn CoCreateInstance<'a, T: ::windows::Interface>(
                 rclsid: *const ::windows::Guid,
                 punkouter: impl ::windows::IntoParam<'a, ::windows::IUnknown>,
                 dwclscontext: CLSCTX,
-                riid: *const ::windows::Guid,
-                ppv: *mut *mut ::std::ffi::c_void,
-            ) -> ::windows::HRESULT {
+            ) -> ::windows::Result<T> {
                 #[link(name = "OLE32")]
                 extern "system" {
                     pub fn CoCreateInstance(
@@ -2228,13 +2226,15 @@ pub mod Windows {
                         ppv: *mut *mut ::std::ffi::c_void,
                     ) -> ::windows::HRESULT;
                 }
+                let mut result__ = ::std::option::Option::None;
                 CoCreateInstance(
                     ::std::mem::transmute(rclsid),
                     punkouter.into_param().abi(),
                     ::std::mem::transmute(dwclscontext),
-                    ::std::mem::transmute(riid),
-                    ::std::mem::transmute(ppv),
+                    &<T as ::windows::Interface>::IID,
+                    ::windows::Abi::set_abi(&mut result__),
                 )
+                .and_some(result__)
             }
             #[repr(transparent)]
             #[derive(

src/runtime/com.rs
@@ -14,10 +14,5 @@ pub fn initialize_sta() -> Result<()> {
 
 /// Creates a COM object with the given CLSID.
 pub fn create_instance<T: Interface>(clsid: &Guid) -> Result<T> {
-    let mut object = None;
-
-    unsafe {
-        CoCreateInstance(clsid, None, CLSCTX::CLSCTX_ALL, &T::IID, object.set_abi())
-            .and_some(object)
-    }
+    unsafe { CoCreateInstance(clsid, None, CLSCTX::CLSCTX_ALL) }
 }

commit 483b3b15461aa56518ad42563ef74a06d7bcc623

gen/function: Emit consistent stub signatures on unsupported platforms
Callers of these functions - whether they're used on unsupported
platforms or not, like `create_instance` with `CoCreateInstance` - fail
to compile when the function signature does not use the new return
pattern for `QueryInterface`-like generic functions.  This change makes
sure the function signature stays the same, only the implementation
changes to `unimplemented!` when the it is not supported on a given
platform.

Fixes: fe8955c ("Transform QueryInterface-like functions into generic functions (#735)")

crates/gen/src/types/function.rs
@@ -51,37 +51,46 @@ impl Function {
             link = "onecoreuap";
         }
 
-        if cfg!(windows) {
+        let body = if cfg!(windows) {
             if signature.has_query_interface() {
                 let leading_params = &signature.params[..signature.params.len() - 2];
-                let params = signature.gen_win32_params(leading_params, gen);
                 let args = leading_params.iter().map(|p| p.gen_win32_abi_arg());
 
                 quote! {
-                    pub unsafe fn #name<#constraints T: ::windows::Interface>(#params) -> ::windows::Result<T> {
-                        #[link(name = #link)]
-                        extern "system" {
-                            pub fn #name(#(#abi_params),*) #abi_return_type;
-                        }
-                        let mut result__ = ::std::option::Option::None;
-                        #name(#(#args,)* &<T as ::windows::Interface>::IID, ::windows::Abi::set_abi(&mut result__)).and_some(result__)
+                    #[link(name = #link)]
+                    extern "system" {
+                        pub fn #name(#(#abi_params),*) #abi_return_type;
                     }
+                    let mut result__ = ::std::option::Option::None;
+                    #name(#(#args,)* &<T as ::windows::Interface>::IID, ::windows::Abi::set_abi(&mut result__)).and_some(result__)
                 }
             } else {
                 quote! {
-                    pub unsafe fn #name<#constraints>(#params) #return_type {
-                        #[link(name = #link)]
-                        extern "system" {
-                            pub fn #name(#(#abi_params),*) #abi_return_type;
-                        }
-                        #name(#(#args),*)
+                    #[link(name = #link)]
+                    extern "system" {
+                        pub fn #name(#(#abi_params),*) #abi_return_type;
                     }
+                    #name(#(#args),*)
+                }
+            }
+        } else {
+            quote! {
+                unimplemented!("Unsupported target OS");
+            }
+        };
+
+        if signature.has_query_interface() {
+            let leading_params = &signature.params[..signature.params.len() - 2];
+            let params = signature.gen_win32_params(leading_params, gen);
+            quote! {
+                pub unsafe fn #name<#constraints T: ::windows::Interface>(#params) -> ::windows::Result<T> {
+                    #body
                 }
             }
         } else {
             quote! {
                 pub unsafe fn #name<#constraints>(#params) #return_type {
-                    panic!("Unsupported target OS");
+                    #body
                 }
             }
         }