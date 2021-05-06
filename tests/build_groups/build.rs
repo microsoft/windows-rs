4eeee52f45a3cd70e1f5444b4ddca4471cc7c560
Kenny Kerr <kenny.kerr@microsoft.com>
2021-05-06 15:34:33 -0700

    test

--- tests/build_groups/build.rs
+++ tests/build_groups/build.rs
@@ -2,5 +2,10 @@ fn main() {
     windows::build!(
         // TODO: IKeyValuePair should not need to be needed here https://github.com/microsoft/windows-rs/issues/772
         Windows::Foundation::{IStringable, Collections::{IVector, IMap, IKeyValuePair}},
+
+        // Test for https://github.com/microsoft/windows-rs/issues/699
+        Windows::Win32::{
+            Debug::GetLastError
+        }
     );
 }
