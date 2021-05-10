ad3d1fba5bd46da1717f59c9254e18bc767eeeba
Kenny Kerr <kenny.kerr@microsoft.com>
2021-05-10 09:31:18 -0700

    overlapped

--- examples/overlapped/bindings/build.rs
+++ examples/overlapped/bindings/build.rs
@@ -1,11 +1,14 @@
 fn main() {
     windows::build!(
-      Windows::Win32::Storage::FileSystem::{CreateFileA, ReadFile},
+      Windows::Win32::Storage::FileSystem::{
+        CreateFileA, ReadFile, FILE_GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING,
+        FILE_FLAG_OVERLAPPED,
+      },
       Windows::Win32::System::SystemServices::GetOverlappedResult,
       Windows::Win32::System::Threading::{
-          CreateEventA, WaitForSingleObject,
+          CreateEventA, WaitForSingleObject, WAIT_OBJECT_0,
       },
-      Windows::Win32::System::Diagnostics::Debug::GetLastError,
+      Windows::Win32::System::Diagnostics::Debug::{GetLastError, ERROR_IO_PENDING},
       Windows::Win32::System::WindowsProgramming::CloseHandle,
     );
 }
