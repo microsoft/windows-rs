81f429accdda8f167e1755e20afd0b96f03dfc30
Damyan Pepper <damyanp@microsoft.com>
2021-05-11 10:09:18 -0700

    Remove unnecessary copyright banner

--- examples/d3d12/src/shaders.hlsl
+++ examples/d3d12/src/shaders.hlsl
@@ -1,14 +1,3 @@
-//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
-
 struct PSInput
 {
     float4 position : SV_POSITION;
