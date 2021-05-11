2889ee8ead34c77647dbb4debf29d196bd2e4314
Damyan Pepper <damyanp@microsoft.com>
2021-05-11 09:25:49 -0700

    fix x86 builds

--- examples/d3d12/src/main.rs
+++ examples/d3d12/src/main.rs
@@ -140,6 +140,30 @@ fn sample_wndproc<S: DXSample>(sample: &mut S, message: u32, wparam: WPARAM) -> 
     }
 }
 
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "32")]
+unsafe fn SetWindowLong(window: HWND, index: WINDOW_LONG_PTR_INDEX, value: isize) -> isize {
+    SetWindowLongA(window, index, value as _) as _
+}
+
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "64")]
+unsafe fn SetWindowLong(window: HWND, index: WINDOW_LONG_PTR_INDEX, value: isize) -> isize {
+    SetWindowLongPtrA(window, index, value)
+}
+
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "32")]
+unsafe fn GetWindowLong(window: HWND, index: WINDOW_LONG_PTR_INDEX) -> isize {
+    GetWindowLongA(window, index) as _
+}
+
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "64")]
+unsafe fn GetWindowLong(window: HWND, index: WINDOW_LONG_PTR_INDEX) -> isize {
+    GetWindowLongPtrA(window, index)
+}
+
 extern "system" fn wndproc<S: DXSample>(
     window: HWND,
     message: u32,
@@ -147,7 +171,7 @@ extern "system" fn wndproc<S: DXSample>(
     lparam: LPARAM,
 ) -> LRESULT {
     unsafe {
-        let user_data = GetWindowLongPtrA(window, GWLP_USERDATA);
+        let user_data = GetWindowLong(window, GWLP_USERDATA);
         let sample: Option<&mut S> = match user_data {
             0 => None,
             _ => Some(transmute(user_data)),
@@ -156,7 +180,7 @@ extern "system" fn wndproc<S: DXSample>(
         match message {
             WM_CREATE => {
                 let create_struct: &CREATESTRUCTA = transmute(lparam);
-                SetWindowLongPtrA(
+                SetWindowLong(
                     window,
                     GWLP_USERDATA,
                     transmute(create_struct.lpCreateParams),
