700d7d67ad105bf5478556309bd1e66f03007745
Kenny Kerr <kenny.kerr@microsoft.com>
2021-06-07 10:28:17 -0700

    fmt

--- crates/fmt/src/main.rs
+++ crates/fmt/src/main.rs
@@ -1,15 +1,15 @@
 use std::io::prelude::*;
 
-fn update_dir(dir: &std::path::Path, set: bool) -> std::io::Result<()> {
+fn update_dir(dir: &std::path::Path) -> std::io::Result<()> {
     if dir.is_dir() {
         for entry in std::fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
 
             if path.is_dir() {
-                update_dir(&path, set)?;
+                update_dir(&path)?;
             } else {
-                update_file(&entry, set)?;
+                update_file(&entry)?;
             }
         }
     }
@@ -17,28 +17,40 @@ fn update_dir(dir: &std::path::Path, set: bool) -> std::io::Result<()> {
     Ok(())
 }
 
-fn update_file(entry: &std::fs::DirEntry, set: bool) -> std::io::Result<()> {
+fn update_file(entry: &std::fs::DirEntry) -> std::io::Result<()> {
     let path = entry.path();
 
     if let Some(extension) = path.extension() {
         if extension == "rs" {
-            println!("{:?}", path);
             let mut file = std::fs::File::open(&path)?;
-            let mut data = String::new();
-            file.read_to_string(&mut data)?;
+            let mut before = String::new();
+            file.read_to_string(&mut before)?;
             drop(file);
 
             let macro_str = "windows::build! \x7b";
-            let use_str = "use \x7b // fmt";
+            let use_str = "use \x7b";
 
-            let data = if set { 
-                data.replace(macro_str, use_str);
-            } else {
-                data.replace(use_str, macro_str)
-            };
+            let after = before.replace(macro_str, use_str);
 
-            let mut file = std::fs::File::create(&path)?;
-            file.write_all(data.as_bytes())?;
+            if before != after {
+                println!("{:?}", path);
+                let mut file = std::fs::File::create(&path)?;
+                file.write_all(after.as_bytes())?;
+                drop(file);
+
+                let mut cmd = ::std::process::Command::new("rustfmt");
+                cmd.arg(&path);
+                cmd.output()?;
+
+                let mut file = std::fs::File::open(&path)?;
+                let mut before = String::new();
+                file.read_to_string(&mut before)?;
+                drop(file);
+
+                let after = before.replace(use_str, macro_str);
+                let mut file = std::fs::File::create(&path)?;
+                file.write_all(after.as_bytes())?;
+            }
         }
     }
 
@@ -46,9 +58,5 @@ fn update_file(entry: &std::fs::DirEntry, set: bool) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let workspace = windows_gen::workspace_dir();
-   // update_dir(&workspace, true)?;
-    update_dir(&workspace, false)?;
-
-    Ok(())
+    update_dir(&windows_gen::workspace_dir())
 }
