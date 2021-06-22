a8a46329384a31877d4d5d6a85d347e9a51ae09e
Kenny Kerr <kenny@kennykerr.ca>
2021-06-22 13:05:23 -0700

    cleanup

--- tests/implement/build.rs
+++ tests/implement/build.rs
@@ -1,8 +1,6 @@
 fn main() {
     windows::build! {
-        // TODO: Dependency tracking is a little too conservative for the implement macro at the moment.
-        Windows::ApplicationModel::Activation::*,
         Windows::Foundation::{IClosable, IStringable},
-        Windows::UI::Xaml::*,
+        Windows::UI::Xaml::Application,
     };
 }
