4e406eec1a335ccebef2a7bc4ae1c2fbe8e478b2
fghzxm <fghzxm@outlook.com>
2021-06-24 21:45:26 +0800

    Run windows_fmt
    Signed-off-by: fghzxm <fghzxm@outlook.com>

--- examples/xaml_island/bindings/build.rs
+++ examples/xaml_island/bindings/build.rs
@@ -1,13 +1,9 @@
 fn main() {
     windows::build! {
-        Windows::UI::Xaml::{
-            Controls::TextBox,
-            Hosting::DesktopWindowXamlSource,
-        },
         Windows::Win32::{
-            Foundation::HWND,
-            System::WinRT::IDesktopWindowXamlSourceNative2,
+            Foundation::HWND, System::WinRT::IDesktopWindowXamlSourceNative2,
             UI::WindowsAndMessaging::SetWindowPos,
         },
+        Windows::UI::Xaml::{Controls::TextBox, Hosting::DesktopWindowXamlSource},
     };
 }
