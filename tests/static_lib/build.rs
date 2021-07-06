53d957d6b639652585fc0170065a4c6fb2471163
Bill Avery <wravery@gmail.com>
2021-07-06 12:14:04 -0700

    Fix macro formatting

--- tests/static_lib/build.rs
+++ tests/static_lib/build.rs
@@ -1,7 +1,9 @@
 fn main() {
     windows::build! {
         Microsoft::Web::WebView2::Win32::*,
-        Windows::Win32::Storage::FileSystem::{GetFileVersionInfoSizeW, GetFileVersionInfoW, VerQueryValueW},
+        Windows::Win32::Storage::FileSystem::{
+            GetFileVersionInfoSizeW, GetFileVersionInfoW, VerQueryValueW,
+        },
         Windows::Win32::UI::Shell::GetCurrentProcessExplicitAppUserModelID,
     };
 }
