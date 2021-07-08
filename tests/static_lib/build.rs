5e27093a84a9e8a3701440426e8c8c40622c40bd
Bill Avery <wravery@gmail.com>
2021-07-07 19:14:14 -0700

    Replace unreferenced symbols with linked libs

--- tests/static_lib/build.rs
+++ tests/static_lib/build.rs
@@ -1,9 +1,9 @@
 fn main() {
     windows::build! {
         Microsoft::Web::WebView2::Win32::CompareBrowserVersions,
-        Windows::Win32::Storage::FileSystem::{
-            GetFileVersionInfoSizeW, GetFileVersionInfoW, VerQueryValueW,
-        },
-        Windows::Win32::UI::Shell::GetCurrentProcessExplicitAppUserModelID,
     };
+
+    // WebView2LoaderStatic.lib has references to these libraries which WebView2Loader.dll would have linked internally.
+    println!("cargo:rustc-link-lib=version");
+    println!("cargo:rustc-link-lib=shell32");
 }
