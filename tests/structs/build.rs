66a40c5f59826fcfaa74a8156e6b346a9e6fc9a8
Rafael Rivera <rafael@withinwindows.com>
2021-07-10 12:06:13 -0700

    Pass params, traits through to struct Debug implementations (#969)
    Closes #439, #968

--- tests/structs/build.rs
+++ tests/structs/build.rs
@@ -1,7 +1,12 @@
 fn main() {
-    // Test for https://github.com/microsoft/windows-rs/issues/738 as DebugPropertyInfo has a
-    // BSTR field and needs to implement Clone.
     windows::build! {
+        // Test for https://github.com/microsoft/windows-rs/issues/738 as DebugPropertyInfo has a
+        // BSTR field and needs to implement Clone.
         Windows::Win32::System::Diagnostics::Debug::DebugPropertyInfo,
+
+        // Test to verify format parameters and traits are passed on to the DebugStruct builder
+        // in generated struct Debug implementations.
+        // https://github.com/microsoft/windows-rs/issues/439
+        Windows::Win32::UI::WindowsAndMessaging::CWPSTRUCT,
     };
 }
