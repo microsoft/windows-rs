ef39f22526d9ad008cb5b0fc8f799a05d81c9f2e
Rafael Rivera <rafael@withinwindows.com>
2021-07-12 21:58:43 -0700

    Tweak PropertyKey::from_attributes per feedback

--- crates/gen/src/propertykey.rs
+++ crates/gen/src/propertykey.rs
@@ -10,16 +10,15 @@ impl PropertyKey {
     pub fn from_attributes<I: IntoIterator<Item = tables::Attribute>>(
         attributes: I,
     ) -> Option<Self> {
-        for attribute in attributes {
-            if attribute.name() == "PropertyKeyAttribute" {
+        attributes
+            .into_iter()
+            .find(|attribute| attribute.name() == "PropertyKeyAttribute")
+            .map(|attribute| {
                 let args = attribute.args();
-                return Some(Self {
+                Self {
                     fmtid: Guid::from_args(&args),
                     pid: args[11].1.unwrap_u32(),
-                });
-            }
-        }
-
-        None
+                }
+            })
     }
 }
