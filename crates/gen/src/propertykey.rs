d29314dc2ff342f0d663aeaedb07633ef7d11735
Rafael Rivera <rafael@withinwindows.com>
2021-07-12 22:27:35 -0700

    Add support for PropertyKey attribute on PROPERTYKEY consts (#975)
    For each `PROPERTYKEY` _constant_ encountered in the metadata, we now tap into the associated `PropertyKeyAttribute` to grab the _fmtid_ and _pid_ values and generate the appropriate `Windows.Win32.System.PropertiesSystem.PROPERTYKEY`.
    
    Fixes #768

---
+++ crates/gen/src/propertykey.rs
@@ -0,0 +1,24 @@
+use super::*;
+
+#[derive(Clone, PartialEq, Default)]
+pub struct PropertyKey {
+    pub fmtid: Guid,
+    pub pid: u32,
+}
+
+impl PropertyKey {
+    pub fn from_attributes<I: IntoIterator<Item = tables::Attribute>>(
+        attributes: I,
+    ) -> Option<Self> {
+        attributes
+            .into_iter()
+            .find(|attribute| attribute.name() == "PropertyKeyAttribute")
+            .map(|attribute| {
+                let args = attribute.args();
+                Self {
+                    fmtid: Guid::from_args(&args),
+                    pid: args[11].1.unwrap_u32(),
+                }
+            })
+    }
+}
