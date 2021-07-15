0e3fb937267fc65067d9e016f1581d7017cbe58c
Kenny Kerr <kenny@kennykerr.ca>
2021-07-15 14:27:45 -0700

    test

--- tests/implement/build.rs
+++ tests/implement/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     windows::build! {
         Windows::Foundation::{IClosable, IStringable},
+        Windows::Foundation::Collections::{IVectorView, IIterable},
         Windows::UI::Xaml::{Application, Controls::Button},
     };
 }
---
+++ tests/implement/tests/generics.rs
@@ -0,0 +1,45 @@
+use test_implement::*;
+use windows::*;
+use Windows::Foundation::Collections::*;
+
+#[implement(
+    Windows::Foundation::Collections::IVectorView<i32>,
+    Windows::Foundation::Collections::IIterable<i32>,
+)]
+struct Thing();
+
+#[allow(non_snake_case)]
+impl Thing {
+    pub fn GetAt(&self, index: u32) -> Result<i32> {
+        Ok(index as _)
+    }
+
+    pub fn Size(&self) -> Result<u32> {
+        Ok(123)
+    }
+
+    pub fn IndexOf(&self, value: i32, index: &mut u32) -> Result<bool> {
+        *index = value as _;
+        Ok(true)
+    }
+
+    pub fn GetMany(&self, _startindex: u32, _items: &mut [i32]) -> Result<u32> {
+        panic!();
+    }
+
+    fn First(&self) -> Result<IIterator<i32>> {
+        panic!();
+    }
+}
+
+#[test]
+fn test_implement() -> Result<()> {
+    let v: IVectorView<i32> = Thing().into();
+    assert_eq!(012, v.GetAt(012)?);
+    assert_eq!(123, v.Size()?);
+    let mut index = 0;
+    assert_eq!(true, v.IndexOf(456, &mut index)?);
+    assert_eq!(456, index);
+
+    Ok(())
+}
