7455af8750a3694a523857417c1e5b4efc9977a8
Kenny Kerr <kekerr@microsoft.com>
2021-07-22 10:17:32 -0700

    Correct ToImpl safety requirement (#993)

--- src/traits/to_impl.rs
+++ src/traits/to_impl.rs
@@ -5,6 +5,6 @@ use super::*;
 /// This trait is automatically implemented when using the [`implement`] macro but
 /// is considered unsafe since different implemenetations of the `from` interface
 // may exist.
-pub unsafe trait ToImpl<T: Interface> {
-    fn to_impl(from: &T) -> &mut Self;
+pub trait ToImpl<T: Interface> {
+    unsafe fn to_impl(from: &T) -> &mut Self;
 }
