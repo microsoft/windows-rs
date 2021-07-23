8512171e28cd7acbbb9a3a7b8759f21c07fc8d5a
Kenny Kerr <kenny@kennykerr.ca>
2021-07-23 13:00:13 -0700

    fmt

--- tests/ntstatus/build.rs
+++ tests/ntstatus/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     windows::build! {
-        Windows::Win32::Foundation::{STATUS_NOT_FOUND, STATUS_INVALID_SIGNATURE, STATUS_SUCCESS},
+        Windows::Win32::Foundation::{STATUS_INVALID_SIGNATURE, STATUS_NOT_FOUND, STATUS_SUCCESS},
         Windows::Win32::Security::Cryptography::Core::{
             BCryptGenRandom, BCryptOpenAlgorithmProvider,
         },
