0b16f38cd9d7ccfad8b6ebfbbc3097d83a6c22a0
Kenny Kerr <kenny@kennykerr.ca>
2021-07-30 14:30:12 -0700

    IUnknown

--- crates/gen/src/object.rs
+++ crates/gen/src/object.rs
@@ -2,6 +2,27 @@ use super::*;
 
 pub fn gen_object(name: &TokenStream, constraints: &TokenStream) -> TokenStream {
     quote! {
+        impl<#constraints> ::std::convert::From<#name> for ::windows::IUnknown {
+            fn from(value: #name) -> Self {
+                unsafe { ::std::mem::transmute(value) }
+            }
+        }
+        impl<#constraints> ::std::convert::From<&#name> for ::windows::IUnknown {
+            fn from(value: &#name) -> Self {
+                ::std::convert::From::from(::std::clone::Clone::clone(value))
+            }
+        }
+        impl<'a, #constraints> ::windows::IntoParam<'a, ::windows::IUnknown> for #name {
+            fn into_param(self) -> ::windows::Param<'a, ::windows::IUnknown> {
+                ::windows::Param::Owned(::std::convert::Into::<::windows::IUnknown>::into(self))
+            }
+        }
+        impl<'a, #constraints> ::windows::IntoParam<'a, ::windows::IUnknown> for &'a #name {
+            fn into_param(self) -> ::windows::Param<'a, ::windows::IUnknown> {
+                ::windows::Param::Owned(::std::convert::Into::<::windows::IUnknown>::into(::std::clone::Clone::clone(self)))
+            }
+        }
+
         impl<#constraints> ::std::convert::From<#name> for ::windows::IInspectable {
             fn from(value: #name) -> Self {
                 value.0
