ac47933cbc17275f4ad9c03b5d4f5c29fba6f11f
Kenny Kerr <kenny@kennykerr.ca>
2021-08-16 14:53:17 -0700

    test

--- tests/interop/build.rs
+++ tests/interop/build.rs
@@ -3,6 +3,5 @@ fn main() {
         Windows::Foundation::Collections::StringMap, Windows::Win32::System::Com::CoInitializeEx,
         Windows::Win32::System::WinRT::RoActivateInstance,
         Windows::Win32::UI::RadialInput::IRadialControllerInterop,
-        Windows::Win32::AI::MachineLearning::WinML::IWinMLEvaluationContext,
     };
 }
