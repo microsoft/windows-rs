microsoft/windows-rs

commit 1291b32b6e9ea856ea8a0646a9816c5f9c886ac6

Remove binary packaging support (#1067)

crates/macros/src/lib.rs
@@ -46,9 +46,8 @@ impl ToTokens for RawString {
 pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let build = parse_macro_input!(stream as BuildMacro);
     let tokens = RawString(build.to_tokens_string());
-    let target_dir = std::env::var("PATH").expect("No `PATH` env variable set");
-    let end = target_dir.find(';').expect("Path not ending in `;`");
-    let target_dir = RawString(target_dir[..end].to_string());
+    let target_dir = RawString(target_dir());
+    let workspace_dir = RawString(workspace_dir());
 
     let tokens = quote! {
         {
@@ -107,7 +106,7 @@ pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 }
             }
 
-            let mut source : ::std::path::PathBuf = ::std::env::var("CARGO_MANIFEST_DIR").expect("No `CARGO_MANIFEST_DIR` env variable set").into();
+            let mut source : ::std::path::PathBuf = #workspace_dir.into();
             source.push(".windows");
 
             if source.exists() {
@@ -132,12 +131,6 @@ pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
                 let profile = ::std::env::var("PROFILE").expect("No `PROFILE` env variable set");
                 copy_to_profile(&source, &destination, &profile);
-
-                destination.push(".windows");
-                destination.push("winmd");
-                source.pop();
-                source.push("winmd");
-                copy(&source, &mut destination);
             }
         }
     };

crates/gen/build.rs
@@ -1,36 +1,0 @@
-#[cfg(windows)]
-fn main() {
-    let mut source: ::std::path::PathBuf = ::std::env::var("CARGO_MANIFEST_DIR")
-        .expect("No `CARGO_MANIFEST_DIR` env var")
-        .into();
-
-    source.push(".windows");
-    source.push("winmd");
-
-    let destination = std::env::var("PATH").expect("No `PATH` env variable set");
-    let end = destination.find(';').expect("Path not ending in `;`");
-    let mut destination: std::path::PathBuf = destination[..end].into();
-    destination.pop();
-    destination.pop();
-    destination.push(".windows");
-    destination.push("winmd");
-
-    if let ::std::result::Result::Ok(files) = ::std::fs::read_dir(source) {
-        for file in files.filter_map(|file| file.ok()) {
-            if let ::std::result::Result::Ok(file_type) = file.file_type() {
-                if file_type.is_file() {
-                    let path = file.path();
-                    if let ::std::option::Option::Some(filename) = path.file_name() {
-                        let _ = std::fs::create_dir_all(&destination);
-                        destination.push(filename);
-                        let _ = ::std::fs::copy(path, &destination);
-                        destination.pop();
-                    }
-                }
-            }
-        }
-    }
-}
-
-#[cfg(not(windows))]
-fn main() {}

commit 4e7a7da1c43294d895a407deb04a47e9d040bef8

macros/lib: Don't copy DLL files outside of the workspace target folder
The previous commit now uses a different folder for `target_dir`, namely
`target/` directly instead of `target/<profile>/deps`.  Popping this
twice would go outside of the directory containing `target/` and copy
DLL files into _every_ folder matching the `<profile>` name (ie. every
`debug` folder inside my `Rust/` projects folder).

Fixes: 1291b32 ("Remove binary packaging support (#1067)")

crates/macros/src/lib.rs
@@ -125,9 +125,7 @@ pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
                     println!("cargo:rustc-link-search=native={}", source.to_str().expect("`CARGO_MANIFEST_DIR` not a valid path"));
                 }
 
-                let mut destination : ::std::path::PathBuf = #target_dir.into();
-                destination.pop();
-                destination.pop();
+                let mut destination: ::std::path::PathBuf = #target_dir.into();
 
                 let profile = ::std::env::var("PROFILE").expect("No `PROFILE` env variable set");
                 copy_to_profile(&source, &destination, &profile);