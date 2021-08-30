4181304dd36be68025df47fdf913191dcf2bd4ae
Kenny Kerr <kenny@kennykerr.ca>
2021-08-30 11:32:33 -0700

    test

---
+++ tests/winrt/simple/tests/test.rs
@@ -0,0 +1,30 @@
+use test_winrt_simple::*;
+use Component::*;
+use windows::*;
+
+#[implement(Component::Simple::IInterface)]
+struct Interface();
+
+#[allow(non_snake_case)]
+impl Interface {
+    fn Method(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[test]
+fn test() -> Result<()> {
+    let class = Simple::Class::new()?;
+    class.Method()?;
+
+    let interface: Simple::IInterface = Interface().into();
+    interface.Method()?;
+
+    let delegate = Simple::Delegate::new(|| Ok(()));
+    delegate.Invoke()?;
+
+    let _struct = Simple::Struct { First: 1, Second: 2};
+    let _enum = Simple::Enum::First;
+
+    Ok(())
+}
