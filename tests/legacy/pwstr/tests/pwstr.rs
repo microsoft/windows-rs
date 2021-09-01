28834c920bc4d211d3fed3234aee2e55cdca0ea8
Kenny Kerr <kekerr@microsoft.com>
2021-09-01 10:42:53 -0700

    Avoid Param abuse (#1109)

--- tests/legacy/pwstr/tests/pwstr.rs
+++ tests/legacy/pwstr/tests/pwstr.rs
@@ -1,27 +1,29 @@
 use std::ffi::{OsStr, OsString};
 use test_pwstr::Windows::Win32::Foundation::PWSTR;
-use windows::IntoParam;
+use windows::*;
 
 #[test]
 fn test() {
-    let expected = "hello\0";
-    let len = expected.chars().count();
+    unsafe {
+        let expected = "hello\0";
+        let len = expected.chars().count();
 
-    let p = get_pwstr_from("hello");
-    assert_eq!(from_parts(p.0, len), expected);
+        let p = get_pwstr_from("hello");
+        assert_eq!(from_parts(p.abi().0, len), expected);
 
-    let p = get_pwstr_from(String::from("hello"));
-    assert_eq!(from_parts(p.0, len), expected);
+        let p = get_pwstr_from(String::from("hello"));
+        assert_eq!(from_parts(p.abi().0, len), expected);
 
-    let p = get_pwstr_from(OsStr::new("hello"));
-    assert_eq!(from_parts(p.0, len), expected);
+        let p = get_pwstr_from(OsStr::new("hello"));
+        assert_eq!(from_parts(p.abi().0, len), expected);
 
-    let p = get_pwstr_from(OsString::from("hello"));
-    assert_eq!(from_parts(p.0, len), expected);
+        let p = get_pwstr_from(OsString::from("hello"));
+        assert_eq!(from_parts(p.abi().0, len), expected);
+    }
 }
 
-fn get_pwstr_from<'a>(t: impl IntoParam<'a, PWSTR>) -> PWSTR {
-    t.into_param().abi()
+fn get_pwstr_from<'a>(t: impl IntoParam<'a, PWSTR>) -> Param<'a, PWSTR> {
+    t.into_param()
 }
 
 fn from_parts(p: *const u16, len: usize) -> String {
