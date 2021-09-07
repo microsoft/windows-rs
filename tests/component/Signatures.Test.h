d12f32cfa52a9a75758a0257f9ae966d3caf5fb0
Kenny Kerr <kenny@kennykerr.ca>
2021-09-07 12:12:56 -0700

    UInt16

--- tests/component/Signatures.Test.h
+++ tests/component/Signatures.Test.h
@@ -78,6 +78,38 @@ namespace winrt::Component::Signatures::implementation
             check(std::equal(a.begin(), a.end(), c.begin(), c.end()));
             check(std::equal(a.begin(), a.end(), d.begin(), d.end()));
         }
+
+        static uint16_t Signature3(uint16_t a, uint16_t& b)
+        {
+            b = a;
+            return a;
+        }
+        static com_array<uint16_t> ArraySignature3(array_view<uint16_t const> a, array_view<uint16_t> b, com_array<uint16_t>& c)
+        {
+            check(a.size() == b.size());
+            check(c.size() == 0);
+            std::copy(a.begin(), a.end(), b.begin());
+            c = com_array<uint16_t>(a.begin(), a.end());
+            return com_array<uint16_t>(a.begin(), a.end());
+        }
+        static void CallSignature3(winrt::Component::Signatures::Signature3 const& handler)
+        {
+            uint16_t a = 123;
+            uint16_t b = 0;
+            auto c = handler(a, b);
+            check(a == b);
+            check(a == c);
+        }
+        static void CallArraySignature3(winrt::Component::Signatures::ArraySignature3 const& handler)
+        {
+            std::array<uint16_t, 3> a{ 1, 2, 3 };
+            std::array<uint16_t, 3> b;
+            com_array<uint16_t> c;
+            com_array d = handler(a, b, c);
+            check(a == b);
+            check(std::equal(a.begin(), a.end(), c.begin(), c.end()));
+            check(std::equal(a.begin(), a.end(), d.begin(), d.end()));
+        }
     };
 }
 namespace winrt::Component::Signatures::factory_implementation
