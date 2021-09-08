ba6d5fc98dedfcc1046774da06afc05152a10327
Kenny Kerr <kenny@kennykerr.ca>
2021-09-08 11:48:13 -0700

    fmt

--- tests/winrt/signatures/build.rs
+++ tests/winrt/signatures/build.rs
@@ -2,7 +2,7 @@ fn main() {
     windows::build! {
         Component::Signatures::*,
         Component::Simple::Class,
-        Windows::Win32::Foundation::{E_NOINTERFACE, E_POINTER, S_FALSE, S_OK},
         Windows::Foundation::PropertyValue,
+        Windows::Win32::Foundation::{E_NOINTERFACE, E_POINTER, S_FALSE, S_OK},
     };
 }
