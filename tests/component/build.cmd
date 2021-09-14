b09d7cca97a22c3e7847b50fe17501801b6226b0
Kenny Kerr <kekerr@microsoft.com>
2021-09-14 14:00:19 -0700

    Only call the win32 winmd generator for x64 builds (#1136)

--- tests/component/build.cmd
+++ tests/component/build.cmd
@@ -1,5 +1,5 @@
 pushd .metadata
-dotnet clean && dotnet build /p:OutputWinmd=..\target\%2\%1\Component.Win32.winmd /p:Platform=%2 /p:Configuration=%1
+if "%3" == "x64" dotnet clean && dotnet build /p:OutputWinmd=..\target\%2\%1\Component.Win32.winmd /p:Platform=%2 /p:Configuration=%1
 popd
 
 copy target\%2\%1\Component.dll ..\..\.windows\%3
