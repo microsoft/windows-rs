d0b57a99f35ab2f5b20d7df1c495b87f9b3b9ce8
Kenny Kerr <kenny@kennykerr.ca>
2021-09-14 21:45:31 -0700

    test

--- tests/win32/handles/build.rs
+++ tests/win32/handles/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     windows::build! {
-        Windows::Win32::Foundation::HWND,
+        Windows::Win32::Foundation::{HANDLE, HWND},
         Windows::Win32::System::Diagnostics::Debug::{SetLastError, WIN32_ERROR},
     };
 }
