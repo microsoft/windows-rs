33440a76629b29deb173a199bb3bba64283b9a94
Rafael Rivera <rafael@withinwindows.com>
2021-09-25 13:58:31 -0700

    upgrade winmdgenerator

--- tests/component/build.cmd
+++ tests/component/build.cmd
@@ -2,9 +2,7 @@ copy "..\target\%2\%1\Component.dll" ..\..\.windows\%3
 copy "..\target\%2\%1\Component.lib" ..\..\.windows\%3
 copy "..\target\%2\%1\Component.winmd" ..\..\.windows\winmd
 
-if "%3" == "x64" (
-	pushd .metadata
-	dotnet clean && dotnet build /p:Platform=%2 /p:Configuration=%1 /p:OutputWinmd=..\..\target\%2\%1\Component.Win32.winmd
-	popd
-	copy "..\target\%2\%1\Component.Win32.winmd" ..\..\.windows\winmd
-)
+pushd .metadata
+dotnet clean && dotnet build /p:Platform=%2 /p:Configuration=%1 /p:OutputWinmd=..\..\target\%2\%1\Component.Win32.winmd
+popd
+copy "..\target\%2\%1\Component.Win32.winmd" ..\..\.windows\winmd
--- tests/static_component/build.cmd
+++ tests/static_component/build.cmd
@@ -1,8 +1,6 @@
 copy "..\target\%2\%1\static_component.lib" ..\..\.windows\%3
 
-if "%3" == "x64" (
-	pushd .metadata
-	dotnet clean && dotnet build /p:Platform=%2 /p:Configuration=%1 /p:OutputWinmd=..\..\target\%2\%1\StaticComponent.Win32.winmd
-	popd
-	copy "..\target\%2\%1\StaticComponent.Win32.winmd" ..\..\.windows\winmd
-)
+pushd .metadata
+dotnet clean && dotnet build /p:Platform=%2 /p:Configuration=%1 /p:OutputWinmd=..\..\target\%2\%1\StaticComponent.Win32.winmd
+popd
+copy "..\target\%2\%1\StaticComponent.Win32.winmd" ..\..\.windows\winmd
