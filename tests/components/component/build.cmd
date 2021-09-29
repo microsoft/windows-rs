dc5b72c475e473cc28c267d91833be3db0a01d81
Kenny Kerr <kenny@kennykerr.ca>
2021-09-29 13:03:23 -0700

    reorg

---
+++ tests/components/component/build.cmd
@@ -0,0 +1,11 @@
+md ..\..\..\.windows\%3
+md ..\..\..\.windows\winmd
+
+copy "..\target\%2\%1\Component.dll" ..\..\..\.windows\%3
+copy "..\target\%2\%1\Component.lib" ..\..\..\.windows\%3
+copy "..\target\%2\%1\Component.winmd" ..\..\..\.windows\winmd
+
+pushd .metadata
+dotnet clean && dotnet build /p:Platform=%2 /p:Configuration=%1 /p:OutputWinmd=..\..\target\%2\%1\Component.Win32.winmd
+popd
+copy "..\target\%2\%1\Component.Win32.winmd" ..\..\..\.windows\winmd
--- tests/static_component/build.cmd
+++
@@ -1,6 +0,0 @@
-copy "..\target\%2\%1\static_component.lib" ..\..\.windows\%3
-
-pushd .metadata
-dotnet clean && dotnet build /p:Platform=%2 /p:Configuration=%1 /p:OutputWinmd=..\..\target\%2\%1\StaticComponent.Win32.winmd
-popd
-copy "..\target\%2\%1\StaticComponent.Win32.winmd" ..\..\.windows\winmd
