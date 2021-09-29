01389132af2098a31e9ff0a80427f8005b8fe250
Kenny Kerr <kenny@kennykerr.ca>
2021-09-29 14:59:53 -0700

    Test method names

---
+++ tests/components/component/.metadata/method_names.cpp
@@ -0,0 +1,1 @@
+#include "method_names.h"
---
+++ tests/win32/method_names/tests/test.rs
@@ -0,0 +1,36 @@
+use test_win32_method_names::*;
+use windows::*;
+use Component::Win32::MethodNames::*;
+
+#[implement(Component::Win32::MethodNames::IMethodNames)]
+struct MethodNames(i32);
+
+#[allow(non_snake_case)]
+impl MethodNames {
+    fn Method(&self) -> Result<()> {
+        Ok(())
+    }
+
+    fn Property(&self) -> Result<i32> {
+        Ok(self.0)
+    }
+
+    fn SetProperty(&mut self, value: i32) -> Result<()> {
+        self.0 = value;
+        Ok(())
+    }
+}
+
+#[test]
+fn test() -> Result<()> {
+    unsafe {
+        let names: IMethodNames = MethodNames(0).into();
+        names.Method()?;
+
+        assert!(names.Property()? == 0);
+        names.SetProperty(123)?;
+        assert!(names.Property()? == 123);
+
+        Ok(())
+    }
+}
