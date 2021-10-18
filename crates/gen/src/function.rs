5912446e2391d7ff76db2441b36d2627d841f4f7
Kenny Kerr <kenny@kennykerr.ca>
2021-10-18 16:29:48 -0700

    test

--- crates/gen/src/function.rs
+++ crates/gen/src/function.rs
@@ -20,23 +20,10 @@ pub fn gen_function(def: &MethodDef, gen: &Gen) -> TokenStream {
         .name()
         .to_lowercase();
 
-    let raw_dylib = !gen.feature.is_empty();
-
-    // TODO: remove this whole block once raw-dylib has stabilized as the workarounds
-    // will no longer be necessary.
-    if !raw_dylib && (link.contains("-ms-win-") || link == "d3dcompiler_47" || link == "sspicli") {
-        link = "onecoreuap".to_string();
-    }
-
     let link_attr = match def.static_lib() {
         Some(link) => quote! { #[link(name = #link, kind = "static")] },
         None => {
-            // TODO: switch to always using raw-dylib once it has stabilized
-            if raw_dylib {
-                quote! { #[link(name = #link, kind="raw-dylib")] }
-            } else {
-                quote! { #[link(name = #link)] }
-            }
+            quote! { #[link(name = #link)] }
         }
     };
 
