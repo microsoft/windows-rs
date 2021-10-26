689182a46bc84bf8e56d6132e4e8182990f54a02
Kenny Kerr <kenny@kennykerr.ca>
2021-10-26 10:29:51 -0700

    gnu

--- crates/tools/gnu/src/main.rs
+++ crates/tools/gnu/src/main.rs
@@ -22,11 +22,11 @@ fn main() {
 
     let lib = output.join("i686_gnu\\lib");
     let _ = std::fs::remove_dir_all(&lib);
-    std::fs::create_dir_all(&output).unwrap();
+    std::fs::create_dir_all(&lib).unwrap();
 
     let lib = output.join("x86_64_gnu\\lib");
     let _ = std::fs::remove_dir_all(&lib);
-    std::fs::create_dir_all(&output).unwrap();
+    std::fs::create_dir_all(&lib).unwrap();
 
     // for (library, functions) in &libraries {
     //     build_library(&output, library, functions);
@@ -78,13 +78,8 @@ fn build_library(
 
     // Note that we don't use set_extension as it confuses PathBuf when the library name includes a period.
 
-    let mut path = std::path::PathBuf::from(output);
-    path.push(format!("{}.c", library));
-    let mut c = std::fs::File::create(&path).unwrap();
-
-    path.pop();
-    path.push(format!("{}.def", library));
-    let mut def = std::fs::File::create(&path).unwrap();
+    let def_path = output.join(format!("{}.def", library));
+    let mut def = std::fs::File::create(&def_path).unwrap();
 
     def.write_all(
         format!(
@@ -99,53 +94,30 @@ EXPORTS
     .unwrap();
 
     for (function, params) in functions {
-        let mut buffer = format!("void __stdcall {}(", function);
-
-        for param in 0..*params {
-            buffer.push_str(&format!("int p{}, ", param));
-        }
-
-        if buffer.ends_with(' ') {
-            buffer.truncate(buffer.len() - 2);
-        }
-
-        buffer.push_str(") {}\n");
-
-        c.write_all(buffer.as_bytes()).unwrap();
-        def.write_all(format!("{}\n", function).as_bytes()).unwrap();
+        def.write_all(format!("{}@{}\n", function, params).as_bytes()).unwrap();
     }
 
-    drop(c);
     drop(def);
 
-    let mut cmd = std::process::Command::new("cl");
-    cmd.current_dir(output);
-    cmd.arg("/nologo");
-    cmd.arg("/c");
-    cmd.arg(format!("{}.c", library));
+    let mut cmd = std::process::Command::new("dlltool");
+    cmd.current_dir(output.join("i686_gnu\\lib"));
+    cmd.arg("-m");
+    cmd.arg("i386");
+    cmd.arg("-d");
+    cmd.arg(format!("{}.def", library));
+    cmd.arg("-l");
+    cmd.arg(format!("{}.a", library));
     cmd.output().unwrap();
 
-    let mut cmd = std::process::Command::new("lib");
-    cmd.current_dir(output);
-    cmd.arg("/nologo");
-    cmd.arg(format!("/out:{}.lib", library));
-    cmd.arg(format!("/def:{}.def", library));
-    cmd.arg(format!("{}.obj", library));
+    let mut cmd = std::process::Command::new("dlltool");
+    cmd.current_dir(output.join("x86_64_gnu\\lib"));
+    cmd.arg("-m");
+    cmd.arg("i386:x86-64");
+    cmd.arg("-d");
+    cmd.arg(format!("{}.def", library));
+    cmd.arg("-l");
+    cmd.arg(format!("{}.a", library));
     cmd.output().unwrap();
 
-    path.pop();
-    path.push(format!("{}.c", library));
-    std::fs::remove_file(&path).unwrap();
-
-    path.pop();
-    path.push(format!("{}.def", library));
-    std::fs::remove_file(&path).unwrap();
-
-    path.pop();
-    path.push(format!("{}.exp", library));
-    std::fs::remove_file(&path).unwrap_or_else(|_| panic!("{:?}", path));
-
-    path.pop();
-    path.push(format!("{}.obj", library));
-    std::fs::remove_file(&path).unwrap();
+    std::fs::remove_file(output.join(format!("x86_64_gnu\\lib\\{}.def", library))).unwrap();
 }
