bf4623a6fc21927059faa8fb1e346f1b9947607a
Rafael Rivera <rafael@withinwindows.com>
2021-11-04 20:27:46 -0700

    Add aarch64-pc-windows-msvc support

--- crates/tests/legacy/win32_arrays/tests/nccalcsize_params.rs
+++ crates/tests/legacy/win32_arrays/tests/nccalcsize_params.rs
@@ -1,6 +1,12 @@
 use test_win32_arrays::Windows::Win32::UI::WindowsAndMessaging::NCCALCSIZE_PARAMS;
 
 #[test]
+#[cfg(target_arch = "aarch64")]
+fn test_arch() {
+    assert_eq!(std::mem::size_of::<NCCALCSIZE_PARAMS>(), 56);
+}
+
+#[test]
 #[cfg(target_arch = "x86_64")]
 fn test_arch() {
     assert_eq!(std::mem::size_of::<NCCALCSIZE_PARAMS>(), 56);
