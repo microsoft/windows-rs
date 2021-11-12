ec7a7290b0c40a3dab1fc35b64e290513e758d88
Kenny Kerr <kenny@kennykerr.ca>
2021-11-12 15:29:19 -0800

    core

--- crates/deps/sys/src/core/mod.rs
+++ crates/deps/sys/src/core/mod.rs
@@ -1,3 +1,17 @@
+#[repr(C)]
+pub struct GUID {
+    pub data1: u32,
+    pub data2: u16,
+    pub data3: u16,
+    pub data4: [u8; 8],
+}
+impl ::core::marker::Copy for GUID {}
+impl ::core::clone::Clone for GUID {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
 #[must_use]
 #[repr(transparent)]
 #[allow(non_camel_case_types)]
@@ -18,15 +32,19 @@ impl ::core::clone::Clone for HSTRING {
     }
 }
 
-#[repr(C)]
-pub struct GUID {
-    pub data1: u32,
-    pub data2: u16,
-    pub data3: u16,
-    pub data4: [u8; 8],
+#[repr(transparent)]
+pub struct IInspectable(*mut core::ffi::c_void);
+impl ::core::marker::Copy for IInspectable {}
+impl ::core::clone::Clone for IInspectable {
+    fn clone(&self) -> Self {
+        *self
+    }
 }
-impl ::core::marker::Copy for GUID {}
-impl ::core::clone::Clone for GUID {
+
+#[repr(transparent)]
+pub struct IUnknown(*mut core::ffi::c_void);
+impl ::core::marker::Copy for IUnknown {}
+impl ::core::clone::Clone for IUnknown {
     fn clone(&self) -> Self {
         *self
     }
