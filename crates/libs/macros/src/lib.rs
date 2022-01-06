7ab3ce8fcdd12321c51a639e42b042b2cb013d0a
Kenny Kerr <kenny@kennykerr.ca>
2022-01-06 14:30:54 -0800

    implement2

--- crates/libs/macros/src/lib.rs
+++ crates/libs/macros/src/lib.rs
@@ -10,29 +10,13 @@ use quote::*;
 use reader::*;
 use syn::parse_macro_input;
 
-/// A macro for generating Windows API bindings to a .rs file at build time.
-///
-/// This macro can be used to import Windows APIs from any Windows metadata (winmd) file.
-/// It is only intended for use from a crate's build.rs script.
-///
-/// The macro generates a single `build` function which can be used in build scripts
-/// to generate the Windows bindings. After using the `build` macro, call the
-/// generated `build` function somewhere in the build.rs script's main function.
-///
-/// # Usage
-/// To use, you must then specify which types you want to use. These
-/// follow the same convention as Rust `use` paths. Types know which other types they depend on so
-/// `build` will generate any other Windows types needed for the specified type to work.
-///
-/// # Example
-/// The following `build!` generates all types inside of the `Microsoft::AI::MachineLearning`
-/// namespace.
-///
-/// ```rust,ignore
-/// build!(
-///     Microsoft::AI::MachineLearning::*
-/// );
-/// ```
+#[proc_macro_attribute]
+pub fn implement2(attribute: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    implement2::gen(attribute, input)
+}
+
+// TODO: get rid of all the remaining macros below
+
 #[proc_macro]
 pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
     parse_macro_input!(stream as BuildMacro);
@@ -46,7 +30,6 @@ pub fn build_legacy(stream: proc_macro::TokenStream) -> proc_macro::TokenStream 
     gen_build_legacy().as_str().parse().unwrap()
 }
 
-/// A macro for generating Windows API bindings ahead of time.
 #[proc_macro]
 pub fn generate(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
     parse_macro_input!(stream as BuildMacro);
@@ -58,25 +41,12 @@ pub fn generate(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
     tokens.parse().unwrap()
 }
 
-/// Rust structs can use the [`macro@implement`] attribute macro to implement entire WinRT or COM
-/// classes or any combination of existing COM and WinRT interfaces.
-///
-/// If the attribute [`proc_macro::TokenStream`] contains the name of a WinRT class then all
-/// of its interfaces are implemented. Otherwise, whatever interfaces are contained within
-/// the attribute TokenStream are implemented.
 #[proc_macro_attribute]
 pub fn implement(attribute: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     implement::gen(attribute, input)
 }
 
-#[proc_macro_attribute]
-pub fn implement2(attribute: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    implement2::gen(attribute, input)
-}
-
-/// Includes the generated bindings into the current context.
 #[proc_macro]
 pub fn include_bindings(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    // TODO: check that input stream is empty
     r#"::std::include!(::std::concat!(::std::env!("OUT_DIR"), "/windows.rs"));"#.parse().unwrap()
 }
