912d8fd1686d3964975d3e9efc44e9a19153c49c
Kenny Kerr <kenny@kennykerr.ca>
2022-01-07 08:14:11 -0800

    min macro

--- crates/libs/macros/src/lib.rs
+++ crates/libs/macros/src/lib.rs
@@ -1,14 +1,14 @@
-mod build_macro;
-mod implement;
+// mod build_macro;
+// mod implement;
 mod implement2;
-mod implement_macro;
+//mod implement_macro;
 
-use build_macro::*;
-use gen::*;
-use implement_macro::*;
-use quote::*;
-use reader::*;
-use syn::parse_macro_input;
+//use build_macro::*;
+//use gen::*;
+//use implement_macro::*;
+//use quote::*;
+//use reader::*;
+//use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn implement2(attribute: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -17,36 +17,36 @@ pub fn implement2(attribute: proc_macro::TokenStream, input: proc_macro::TokenSt
 
 // TODO: get rid of all the remaining macros below
 
-#[proc_macro]
-pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    parse_macro_input!(stream as BuildMacro);
-    gen_build().as_str().parse().unwrap()
-}
+// #[proc_macro]
+// pub fn build(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     parse_macro_input!(stream as BuildMacro);
+//     gen_build().as_str().parse().unwrap()
+// }
 
-#[doc(hidden)]
-#[proc_macro]
-pub fn build_legacy(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    parse_macro_input!(stream as BuildMacro);
-    gen_build_legacy().as_str().parse().unwrap()
-}
+// #[doc(hidden)]
+// #[proc_macro]
+// pub fn build_legacy(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     parse_macro_input!(stream as BuildMacro);
+//     gen_build_legacy().as_str().parse().unwrap()
+// }
 
-#[proc_macro]
-pub fn generate(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    parse_macro_input!(stream as BuildMacro);
+// #[proc_macro]
+// pub fn generate(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     parse_macro_input!(stream as BuildMacro);
 
-    let mut tokens = String::new();
-    tokens.push_str("r#\"");
-    tokens.push_str(&gen_source_tree().into_string());
-    tokens.push_str("\"#");
-    tokens.parse().unwrap()
-}
+//     let mut tokens = String::new();
+//     tokens.push_str("r#\"");
+//     tokens.push_str(&gen_source_tree().into_string());
+//     tokens.push_str("\"#");
+//     tokens.parse().unwrap()
+// }
 
-#[proc_macro_attribute]
-pub fn implement(attribute: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    implement::gen(attribute, input)
-}
+// #[proc_macro_attribute]
+// pub fn implement(attribute: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     implement::gen(attribute, input)
+// }
 
-#[proc_macro]
-pub fn include_bindings(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    r#"::std::include!(::std::concat!(::std::env!("OUT_DIR"), "/windows.rs"));"#.parse().unwrap()
-}
+// #[proc_macro]
+// pub fn include_bindings(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     r#"::std::include!(::std::concat!(::std::env!("OUT_DIR"), "/windows.rs"));"#.parse().unwrap()
+// }
