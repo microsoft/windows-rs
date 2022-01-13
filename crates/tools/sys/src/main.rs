dbcd05e40b97f4c099ed01f5c6606d9d743cdc89
Yoshua Wuyts <yoshuawuyts@gmail.com>
2022-01-13 17:30:52 +0100

    bump versions to `0.30.0`

--- crates/tools/sys/src/main.rs
+++ crates/tools/sys/src/main.rs
@@ -25,7 +25,7 @@ fn main() {
         r#"
 [package]
 name = "windows-sys"
-version = "0.29.0"
+version = "0.30.0"
 authors = ["Microsoft"]
 edition = "2018"
 license = "MIT OR Apache-2.0"
@@ -38,34 +38,34 @@ default-target = "x86_64-pc-windows-msvc"
 all-features = true
 
 [target.i686-pc-windows-msvc.dependencies]
-windows_i686_msvc = { path = "../../targets/i686_msvc", version = "0.29.0" }
+windows_i686_msvc = { path = "../../targets/i686_msvc", version = "0.30.0" }
 
 [target.i686-uwp-windows-msvc.dependencies]
-windows_i686_msvc = { path = "../../targets/i686_msvc", version = "0.29.0" }
+windows_i686_msvc = { path = "../../targets/i686_msvc", version = "0.30.0" }
 
 [target.x86_64-pc-windows-msvc.dependencies]
-windows_x86_64_msvc = { path = "../../targets/x86_64_msvc", version = "0.29.0" }
+windows_x86_64_msvc = { path = "../../targets/x86_64_msvc", version = "0.30.0" }
 
 [target.x86_64-uwp-windows-msvc.dependencies]
-windows_x86_64_msvc = { path = "../../targets/x86_64_msvc", version = "0.29.0" }
+windows_x86_64_msvc = { path = "../../targets/x86_64_msvc", version = "0.30.0" }
 
 [target.aarch64-pc-windows-msvc.dependencies]
-windows_aarch64_msvc = { path = "../../targets/aarch64_msvc", version = "0.29.0" }
+windows_aarch64_msvc = { path = "../../targets/aarch64_msvc", version = "0.30.0" }
 
 [target.aarch64-uwp-windows-msvc.dependencies]
-windows_aarch64_msvc = { path = "../../targets/aarch64_msvc", version = "0.29.0" }
+windows_aarch64_msvc = { path = "../../targets/aarch64_msvc", version = "0.30.0" }
 
 [target.i686-pc-windows-gnu.dependencies]
-windows_i686_gnu = { path = "../../targets/i686_gnu", version = "0.29.0" }
+windows_i686_gnu = { path = "../../targets/i686_gnu", version = "0.30.0" }
 
 [target.i686-uwp-windows-gnu.dependencies]
-windows_i686_gnu = { path = "../../targets/i686_gnu", version = "0.29.0" }
+windows_i686_gnu = { path = "../../targets/i686_gnu", version = "0.30.0" }
 
 [target.x86_64-pc-windows-gnu.dependencies]
-windows_x86_64_gnu = { path = "../../targets/x86_64_gnu", version = "0.29.0" }
+windows_x86_64_gnu = { path = "../../targets/x86_64_gnu", version = "0.30.0" }
 
 [target.x86_64-uwp-windows-gnu.dependencies]
-windows_x86_64_gnu = { path = "../../targets/x86_64_gnu", version = "0.29.0" }
+windows_x86_64_gnu = { path = "../../targets/x86_64_gnu", version = "0.30.0" }
 
 [features]
 default = []
