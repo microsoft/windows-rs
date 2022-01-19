510784eb3b50a97211c962f607161f27cc4aa827
Kenny Kerr <kenny@kennykerr.ca>
2022-01-19 13:34:53 -0800

    test

--- crates/tests/implement_null_result/tests/test.rs
+++ crates/tests/implement_null_result/tests/test.rs
@@ -1,15 +1,43 @@
 use windows::core::*;
 use windows::Win32::Foundation::S_OK;
-use windows::UI::Xaml::Markup::{IXamlType, IXamlType2, IXamlType2_Impl};
+use windows::UI::Xaml::Markup::*;
+use windows::UI::Xaml::Interop::*;
 
+// TODO: we can't necessarily infer the namespace for the trait types for a particular interfaces
+// because associated traits aren't yet supported by Rust. 
+
+// TODO: does this add support for QI to IXamlType (as it does for COM hierarchies)?
 #[implement(IXamlType2)]
 struct Test();
 
 #[allow(non_snake_case)]
 impl IXamlType2_Impl for Test {
-    fn BoxedType(&self) -> Result<IXamlType> {
+    fn BoxedType(&mut self) -> Result<IXamlType> {
         Err(Error::OK)
     }
+}
+
+// Note: IXamlType_Impl is required by IXamlType2_Impl (since IXamlType2 requires IXamlType).
+#[allow(non_snake_case)]
+impl IXamlType_Impl for Test {
+fn BaseType(&mut self) -> Result<IXamlType> { todo!() }
+fn ContentProperty(&mut self) -> Result<IXamlMember> { todo!() }
+fn FullName(&mut self) -> Result<HSTRING> { todo!() }
+fn IsArray(&mut self) -> Result<bool> { todo!() }
+fn IsCollection(&mut self) -> Result<bool> { todo!() }
+fn IsConstructible(&mut self) -> Result<bool> { todo!() }
+fn IsDictionary(&mut self) -> Result<bool> { todo!() }
+fn IsMarkupExtension(&mut self) -> Result<bool> { todo!() }
+fn IsBindable(&mut self) -> Result<bool> { todo!() }
+fn ItemType(&mut self) -> Result<IXamlType> { todo!() }
+fn KeyType(&mut self) -> Result<IXamlType> { todo!() }
+fn UnderlyingType(&mut self) -> Result<TypeName> { todo!() }
+fn ActivateInstance(&mut self) -> Result<IInspectable> { todo!() }
+fn CreateFromString(&mut self, _: &HSTRING) -> Result<IInspectable> { todo!() }
+fn GetMember(&mut self, _: &HSTRING) -> Result<IXamlMember> { todo!() }
+fn AddToVector(&mut self, _: &Option<IInspectable>, _: &Option<IInspectable>) -> Result<()> { todo!() }
+fn AddToMap(&mut self, _: &Option<IInspectable>, _: &Option<IInspectable>, _: &Option<IInspectable>) -> Result<()> { todo!() }
+fn RunInitializer(&mut self) -> Result<()> { todo!() }
 }
 
 #[test]
