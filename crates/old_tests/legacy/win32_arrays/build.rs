42539251751b846d9f33449dcc6146dc7c6f4f72
Kenny Kerr <kenny@kennykerr.ca>
2022-01-19 15:17:53 -0800

    test_win32_arrays

--- crates/old_tests/legacy/win32_arrays/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    windows::core::build_legacy! {Windows::Win32::Graphics::Dxgi::DXGI_ADAPTER_DESC1, Windows::Win32::NetworkManagement::IpHelper::IPV6_ADDRESS_EX, Windows::Win32::System::Diagnostics::Debug::MINIDUMP_MEMORY_LIST, Windows::Win32::UI::WindowsAndMessaging::NCCALCSIZE_PARAMS};
-}
