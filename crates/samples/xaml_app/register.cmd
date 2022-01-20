40d2b400f3ea31f8aa9889c170adcac48a51e53b
Kenny Kerr <kenny@kennykerr.ca>
2022-01-20 11:35:53 -0800

    register

--- crates/samples/xaml_app/register.cmd
+++ crates/samples/xaml_app/register.cmd
@@ -3,4 +3,4 @@ copy appx\* ..\..\..\target\debug
 cd ..\..\..\target\debug
 powershell -command "Get-AppxPackage *942b16b2* | Remove-AppxPackage"
 powershell -command "Add-AppxPackage -Register AppxManifest.xml"
-cd ..\..\..\crates\samples\xaml_app
+cd ..\..\crates\samples\xaml_app
