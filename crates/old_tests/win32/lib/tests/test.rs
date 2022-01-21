285e014f901a455ccb728b12db33758dac9aa925
Kenny Kerr <kenny@kennykerr.ca>
2022-01-21 09:16:19 -0800

    lib

--- crates/old_tests/win32/lib/tests/test.rs
+++
@@ -1,9 +0,0 @@
-use test_win32_lib::*;
-
-#[test]
-fn linker() -> windows::core::Result<()> {
-    unsafe {
-        Windows::Win32::Graphics::Direct3D::Fxc::D3DCreateLinker()?;
-        Ok(())
-    }
-}
