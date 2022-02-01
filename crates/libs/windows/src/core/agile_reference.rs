29e150f6e47e076a0523340652f46af80dffca02
Nerixyz <nero.9@hotmail.de>
2022-02-01 20:43:33 +0100

    docs: add basic documentation for `AgileReference`

--- crates/libs/windows/src/core/agile_reference.rs
+++ crates/libs/windows/src/core/agile_reference.rs
@@ -2,12 +2,17 @@ use super::*;
 use bindings::*;
 use core::marker::PhantomData;
 
+/// A safe wrapper around WinRT interfaces.
+///
+/// Some interfaces are not marked as agile thus then don't implement [`Send`].
+/// These interfaces can be made agile through this `AgileReference`.
 pub struct AgileReference<T> {
     reference: IAgileReference,
     _marker: PhantomData<T>,
 }
 
 impl<T: Interface> AgileReference<T> {
+    /// Creates a new wrapper around the reference
     pub fn new<'a>(from_ref: &'a T) -> Result<Self>
     where
         &'a T: IntoParam<'a, IUnknown>,
@@ -15,6 +20,7 @@ impl<T: Interface> AgileReference<T> {
         unsafe { RoGetAgileReference(AGILEREFERENCE_DEFAULT, &T::IID, from_ref).map(|reference| Self { reference, _marker: Default::default() }) }
     }
 
+    /// Resolves the reference for the current thread.
     pub fn resolve(&self) -> Result<T> {
         unsafe { self.reference.Resolve() }
     }
