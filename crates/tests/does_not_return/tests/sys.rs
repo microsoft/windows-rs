3513f64e10f37c700e6949638f7f573e6b0d58d5
Kenny Kerr <kenny@kennykerr.ca>
2022-02-01 11:27:48 -0800

    test

---
+++ crates/tests/does_not_return/tests/sys.rs
@@ -0,0 +1,8 @@
+use windows_sys::Win32::System::Diagnostics::Debug::FatalExit;
+
+// Compile-only test to check that FatalExit returns `-> !`
+
+#[allow(dead_code)]
+fn test() -> i32 {
+    unsafe { FatalExit(123); }
+}
