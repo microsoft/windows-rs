7e320e39b3cb6ba54b83fcb0400122e0421da301
Kenny Kerr <kenny@kennykerr.ca>
2022-02-18 12:05:14 -0800

    fmt

--- crates/libs/metadata/src/type.rs
+++ crates/libs/metadata/src/type.rs
@@ -143,7 +143,7 @@ impl Type {
     pub fn is_convertible(&self) -> bool {
         match self {
             Self::TypeDef(t) => t.is_convertible(),
-            Self::String | Self::IInspectable | Self::GUID | Self::IUnknown | Self::GenericParam(_) | Self::PCSTR|  Self::PCWSTR => true,
+            Self::String | Self::IInspectable | Self::GUID | Self::IUnknown | Self::GenericParam(_) | Self::PCSTR | Self::PCWSTR => true,
             Self::WinrtConstRef(kind) => kind.is_convertible(),
             _ => false,
         }
