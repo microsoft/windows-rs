feb7d258fa8957945eabe1ce18e57795f211f2ee
Kenny Kerr <kenny@kennykerr.ca>
2022-02-24 16:26:34 -0800

    consent (#1566)

---
+++ crates/samples/consent/src/main.rs
@@ -0,0 +1,17 @@
+use windows::{core::*, Foundation::*, Security::Credentials::UI::*, Win32::Foundation::*, Win32::System::WinRT::*};
+
+fn main() -> Result<()> {
+    unsafe {
+        let interop = factory::<UserConsentVerifier, IUserConsentVerifierInterop>()?;
+
+        let window = HWND(0); // <== replace with your app's window handle
+
+        let operation: IAsyncOperation<UserConsentVerificationResult> = interop.RequestVerificationForWindowAsync(window, "Hello from Rust")?;
+
+        let result: UserConsentVerificationResult = operation.get()?;
+
+        println!("{:?}", result);
+
+        Ok(())
+    }
+}
