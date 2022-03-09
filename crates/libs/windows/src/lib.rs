041039afbb2814a4b201684e09d2c80573a468c3
Kenny Kerr <kenny@kennykerr.ca>
2022-03-09 11:07:54 -0800

    remove nightly features

--- crates/libs/windows/src/lib.rs
+++ crates/libs/windows/src/lib.rs
@@ -2,8 +2,6 @@
 Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs>
 */
 
-#![cfg_attr(feature = "implement", feature(const_fn_fn_ptr_basics))]
-#![cfg_attr(feature = "implement", feature(const_fn_trait_bound))]
 #![doc(html_no_source)]
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, dead_code, unused_variables)]
 
