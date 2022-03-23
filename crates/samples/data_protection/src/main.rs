fbad163e18efae93ff03be04499a4ac626ac392f
Kenny Kerr <kenny@kennykerr.ca>
2022-03-22 17:57:33 -0700

    persist

--- crates/samples/data_protection/src/main.rs
+++ crates/samples/data_protection/src/main.rs
@@ -1,16 +1,24 @@
-use windows::{core::*, Security::Cryptography::DataProtection::*, Security::Cryptography::*};
+use windows::{core::*, Security::Cryptography::DataProtection::*, Security::Cryptography::*, Storage::Streams::*, Win32::System::WinRT::*};
 
-fn main() -> Result<()> {
+fn main() -> std::io::Result<()> {
     let provider = DataProtectionProvider::CreateOverloadExplicit("LOCAL=user")?;
+    let unprotected = CryptographicBuffer::ConvertStringToBinary("Hello world", BinaryStringEncoding::Utf8)?;
 
-    let buffer = CryptographicBuffer::ConvertStringToBinary("Hello world", BinaryStringEncoding::Utf8)?;
+    let protected = provider.ProtectAsync(unprotected)?.get()?;
+    let protected_bytes = unsafe { as_mut_bytes(&protected)? };
+    std::fs::write("secret.bin", protected_bytes)?;
 
-    let protected = provider.ProtectAsync(buffer)?.get()?;
-
+    let protected_bytes = std::fs::read("secret.bin")?;
+    let protected = CryptographicBuffer::CreateFromByteArray(&protected_bytes)?;
     let unprotected = provider.UnprotectAsync(protected)?.get()?;
 
     let message = CryptographicBuffer::ConvertBinaryToString(BinaryStringEncoding::Utf8, unprotected)?;
-
     println!("{}", message);
     Ok(())
+}
+
+unsafe fn as_mut_bytes(buffer: &IBuffer) -> Result<&mut [u8]> {
+    let interop = buffer.cast::<IBufferByteAccess>()?;
+    let data = interop.Buffer()?;
+    Ok(std::slice::from_raw_parts_mut(data, buffer.Length()? as _))
 }
