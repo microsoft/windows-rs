35b57d4dbbd883d867bc591542bad3ea6f080b6e
Kenny Kerr <kenny@kennykerr.ca>
2022-04-04 12:10:21 -0700

    warning

--- crates/libs/windows/src/core/pcstr.rs
+++ crates/libs/windows/src/core/pcstr.rs
@@ -1,5 +1,4 @@
 use super::*;
-use bindings::*;
 
 /// A pointer to a constant null-terminated string of 8-bit Windows (ANSI) characters.
 #[repr(transparent)]
@@ -38,7 +37,7 @@ unsafe impl Abi for PCSTR {
     unsafe fn drop_param(param: &mut Param<'_, Self>) {
         if let Param::Boxed(value) = param {
             if !value.is_null() {
-                BSTR::from_raw(value.0 as _);
+                bindings::BSTR::from_raw(value.0 as _);
             }
         }
     }
@@ -47,7 +46,7 @@ unsafe impl Abi for PCSTR {
 impl<'a> IntoParam<'a, PCSTR> for &str {
     fn into_param(self) -> Param<'a, PCSTR> {
         // TODO: workaround for https://github.com/microsoft/win32metadata/issues/868
-        unsafe { Param::Boxed(PCSTR(SysAllocStringByteLen(PCSTR(self.as_ptr()), self.len() as _).into_raw() as _)) }
+        unsafe { Param::Boxed(PCSTR(bindings::SysAllocStringByteLen(PCSTR(self.as_ptr()), self.len() as _).into_raw() as _)) }
     }
 }
 #[cfg(feature = "alloc")]
