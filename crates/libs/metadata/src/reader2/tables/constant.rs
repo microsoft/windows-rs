2a7e1f90b02cc3d743aee576352d2a3413da3971
Kenny Kerr <kenny@kennykerr.ca>
2022-04-07 07:24:47 -0700

    constant

---
+++ crates/libs/metadata/src/reader2/tables/constant.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+#[derive(Clone, PartialEq, PartialOrd, Eq, Ord)]
+pub struct Constant<'a>(pub Row<'a>);
+
+impl<'a> Constant<'a> {
+    pub fn definition(&self) -> Type {
+        todo!()
+    }
+    pub fn value(&self) -> Blob {
+        self.0.blob(2)
+    }
+}
