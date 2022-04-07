19d2d946de3eac99c305342bf9b19fc5dfb20cf6
Kenny Kerr <kenny@kennykerr.ca>
2022-04-07 11:41:29 -0700

    flags

--- crates/libs/metadata/src/reader2/flags.rs
+++ crates/libs/metadata/src/reader2/flags.rs
@@ -5,28 +5,50 @@ pub struct ParamAttributes(pub usize);
 pub struct PInvokeAttributes(pub usize);
 pub struct TypeAttributes(pub usize);
 
-impl FieldAttributes {}
+impl FieldAttributes {
+    pub fn literal(&self) -> bool {
+        self.0 & 0x40 != 0
+    }
+}
 
-impl MethodAttributes {}
+impl MethodAttributes {
+    pub fn special(&self) -> bool {
+        self.0 & 0x800 != 0
+    }
+}
 
-impl MethodImplAttributes {}
+impl MethodImplAttributes {
+    pub fn preserve_sig(&self) -> bool {
+        self.0 & 0x80 != 0
+    }
+}
 
-impl ParamAttributes {}
+impl ParamAttributes {
+    pub fn input(&self) -> bool {
+        self.0 & 0x1 != 0
+    }
+    pub fn output(&self) -> bool {
+        self.0 & 0x2 != 0
+    }
+    pub fn optional(&self) -> bool {
+        self.0 & 0x10 != 0
+    }
+}
 
 impl PInvokeAttributes {
     pub fn last_error(&self) -> bool {
-        self.0 & 0x0040 != 0
+        self.0 & 0x40 != 0
     }
 }
 
 impl TypeAttributes {
-    pub fn input(&self) -> bool {
-        self.0 & 0x0001 != 0
+    pub fn union(&self) -> bool {
+        self.0 & 0x10 != 0
     }
-    pub fn output(&self) -> bool {
-        self.0 & 0x0002 != 0
+    pub fn winrt(&self) -> bool {
+        self.0 & 0x800 != 0
     }
-    pub fn optional(&self) -> bool {
-        self.0 & 0x0010 != 0
+    pub fn interface(&self) -> bool {
+        self.0 & 0x20 != 0
     }
 }
