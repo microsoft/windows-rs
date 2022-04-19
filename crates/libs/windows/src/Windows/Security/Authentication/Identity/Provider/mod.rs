5f76358dd5ca393ad2bcc971a755c33ead8ecb73
Kenny Kerr <kenny@kennykerr.ca>
2022-04-19 09:36:18 -0700

    Add missing features to enum bitwise operators (#1701)

--- crates/libs/windows/src/Windows/Security/Authentication/Identity/Provider/mod.rs
+++ crates/libs/windows/src/Windows/Security/Authentication/Identity/Provider/mod.rs
@@ -1088,28 +1088,33 @@ impl ::core::fmt::Debug for SecondaryAuthenticationFactorDeviceCapabilities {
         f.debug_tuple("SecondaryAuthenticationFactorDeviceCapabilities").field(&self.0).finish()
     }
 }
+#[cfg(feature = "deprecated")]
 impl ::core::ops::BitOr for SecondaryAuthenticationFactorDeviceCapabilities {
     type Output = Self;
     fn bitor(self, other: Self) -> Self {
         Self(self.0 | other.0)
     }
 }
+#[cfg(feature = "deprecated")]
 impl ::core::ops::BitAnd for SecondaryAuthenticationFactorDeviceCapabilities {
     type Output = Self;
     fn bitand(self, other: Self) -> Self {
         Self(self.0 & other.0)
     }
 }
+#[cfg(feature = "deprecated")]
 impl ::core::ops::BitOrAssign for SecondaryAuthenticationFactorDeviceCapabilities {
     fn bitor_assign(&mut self, other: Self) {
         self.0.bitor_assign(other.0)
     }
 }
+#[cfg(feature = "deprecated")]
 impl ::core::ops::BitAndAssign for SecondaryAuthenticationFactorDeviceCapabilities {
     fn bitand_assign(&mut self, other: Self) {
         self.0.bitand_assign(other.0)
     }
 }
+#[cfg(feature = "deprecated")]
 impl ::core::ops::Not for SecondaryAuthenticationFactorDeviceCapabilities {
     type Output = Self;
     fn not(self) -> Self {
