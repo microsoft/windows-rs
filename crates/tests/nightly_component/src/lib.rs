6a7d411b16e65cb38aa4666c3f2fd5f769cd70b7
Kenny Kerr <kenny@kennykerr.ca>
2022-04-21 10:25:08 -0700

    clippy

--- crates/tests/nightly_component/src/lib.rs
+++ crates/tests/nightly_component/src/lib.rs
@@ -1,4 +1,4 @@
-#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, clashing_extern_declarations, unused_variables, dead_code)]
+#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, clashing_extern_declarations, unused_variables, dead_code, clippy::all)]
 
 mod bindings;
 use std::mem::*;
