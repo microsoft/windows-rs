9dc5d60aa067056cfef269ab3ea17c908c003977
Ryan Levick <me@ryanlevick.com>
2022-05-09 16:58:55 +0200

    Change to_impl to not be broken

--- crates/libs/windows/src/core/to_impl.rs
+++ crates/libs/windows/src/core/to_impl.rs
@@ -1,12 +1,6 @@
-use super::*;
-
 /// A trait for retrieving the implementation behind a COM or WinRT interface.
 ///
-/// This trait is automatically implemented when using the `implement` macro but
-/// is considered unsafe since different implementations of the `from` interface
-/// may exist.
-pub trait ToImpl<T: Interface> {
-    /// # Safety
-    #[allow(clippy::mut_from_ref)]
-    unsafe fn to_impl(from: &T) -> &mut Self;
+/// This trait is automatically implemented when using the `implement` macro.
+pub trait ToImpl<T> {
+    fn as_impl(&self) -> &T;
 }
