17511354b0b4d23179afc3c590fadbb784c0a24c
Ryan Phelps <rphelps@apexfintechsolutions.com>
2022-05-14 08:04:54 -0700

    Remove From<&T> impls for ADDR types

--- crates/libs/bindgen/src/extensions/sockaddr_in.rs
+++ crates/libs/bindgen/src/extensions/sockaddr_in.rs
@@ -9,19 +9,7 @@ pub fn gen() -> TokenStream {
                 SOCKADDR_IN {
                     sin_family: AF_INET.0 as u16,
                     sin_port: addr.port().to_be(),
-                    sin_addr: addr.ip().into(),
-                    ..Default::default()
-                }
-            }
-        }
-        impl ::core::convert::From<&::std::net::SocketAddrV4> for SOCKADDR_IN {
-            fn from(addr: &::std::net::SocketAddrV4) -> Self {
-                // addr.port() is in host byte order
-                // sin_port must be big-endian, network byte order
-                SOCKADDR_IN {
-                    sin_family: AF_INET.0 as u16,
-                    sin_port: addr.port().to_be(),
-                    sin_addr: addr.ip().into(),
+                    sin_addr: (*addr.ip()).into(),
                     ..Default::default()
                 }
             }
--- crates/libs/windows/src/Windows/Win32/Networking/WinSock/mod.rs
+++ crates/libs/windows/src/Windows/Win32/Networking/WinSock/mod.rs
@@ -1838,18 +1838,8 @@ impl ::core::convert::From<::std::net::Ipv6Addr> for IN6_ADDR {
         Self { u: IN6_ADDR_0 { Byte: addr.octets() } }
     }
 }
-impl ::core::convert::From<&::std::net::Ipv6Addr> for IN6_ADDR {
-    fn from(addr: &::std::net::Ipv6Addr) -> Self {
-        Self { u: IN6_ADDR_0 { Byte: addr.octets() } }
-    }
-}
 impl ::core::convert::From<IN6_ADDR> for ::std::net::Ipv6Addr {
     fn from(in6_addr: IN6_ADDR) -> Self {
-        Self::from(unsafe { in6_addr.u.Byte })
-    }
-}
-impl ::core::convert::From<&IN6_ADDR> for ::std::net::Ipv6Addr {
-    fn from(in6_addr: &IN6_ADDR) -> Self {
         Self::from(unsafe { in6_addr.u.Byte })
     }
 }
@@ -2147,18 +2137,8 @@ impl ::core::convert::From<::std::net::Ipv4Addr> for IN_ADDR {
         Self { S_un: IN_ADDR_0 { S_addr: u32::from(addr).to_be() } }
     }
 }
-impl ::core::convert::From<&::std::net::Ipv4Addr> for IN_ADDR {
-    fn from(addr: &::std::net::Ipv4Addr) -> Self {
-        Self { S_un: IN_ADDR_0 { S_addr: u32::from(*addr).to_be() } }
-    }
-}
 impl ::core::convert::From<IN_ADDR> for ::std::net::Ipv4Addr {
     fn from(in_addr: IN_ADDR) -> Self {
-        Self::from(u32::from_be(unsafe { in_addr.S_un.S_addr }))
-    }
-}
-impl ::core::convert::From<&IN_ADDR> for ::std::net::Ipv4Addr {
-    fn from(in_addr: &IN_ADDR) -> Self {
         Self::from(u32::from_be(unsafe { in_addr.S_un.S_addr }))
     }
 }
@@ -7812,12 +7792,7 @@ impl ::core::default::Default for SOCKADDR_IN {
 }
 impl ::core::convert::From<::std::net::SocketAddrV4> for SOCKADDR_IN {
     fn from(addr: ::std::net::SocketAddrV4) -> Self {
-        SOCKADDR_IN { sin_family: AF_INET.0 as u16, sin_port: addr.port().to_be(), sin_addr: addr.ip().into(), ..Default::default() }
-    }
-}
-impl ::core::convert::From<&::std::net::SocketAddrV4> for SOCKADDR_IN {
-    fn from(addr: &::std::net::SocketAddrV4) -> Self {
-        SOCKADDR_IN { sin_family: AF_INET.0 as u16, sin_port: addr.port().to_be(), sin_addr: addr.ip().into(), ..Default::default() }
+        SOCKADDR_IN { sin_family: AF_INET.0 as u16, sin_port: addr.port().to_be(), sin_addr: (*addr.ip()).into(), ..Default::default() }
     }
 }
 #[repr(C)]
@@ -7855,19 +7830,7 @@ impl ::core::convert::From<::std::net::SocketAddrV6> for SOCKADDR_IN6 {
             sin6_family: AF_INET6.0 as u16,
             sin6_port: addr.port().to_be(),
             sin6_flowinfo: addr.flowinfo().to_be(),
-            sin6_addr: addr.ip().into(),
-            Anonymous: SOCKADDR_IN6_0 { sin6_scope_id: addr.scope_id() },
-            ..Default::default()
-        }
-    }
-}
-impl ::core::convert::From<&::std::net::SocketAddrV6> for SOCKADDR_IN6 {
-    fn from(addr: &::std::net::SocketAddrV6) -> Self {
-        SOCKADDR_IN6 {
-            sin6_family: AF_INET6.0 as u16,
-            sin6_port: addr.port().to_be(),
-            sin6_flowinfo: addr.flowinfo().to_be(),
-            sin6_addr: addr.ip().into(),
+            sin6_addr: (*addr.ip()).into(),
             Anonymous: SOCKADDR_IN6_0 { sin6_scope_id: addr.scope_id() },
             ..Default::default()
         }
@@ -7998,31 +7961,13 @@ impl ::core::convert::From<::std::net::SocketAddrV4> for SOCKADDR_INET {
         SOCKADDR_INET { Ipv4: addr.into() }
     }
 }
-impl ::core::convert::From<&::std::net::SocketAddrV4> for SOCKADDR_INET {
-    fn from(addr: &::std::net::SocketAddrV4) -> Self {
-        SOCKADDR_INET { Ipv4: addr.into() }
-    }
-}
 impl ::core::convert::From<::std::net::SocketAddrV6> for SOCKADDR_INET {
     fn from(addr: ::std::net::SocketAddrV6) -> Self {
         SOCKADDR_INET { Ipv6: addr.into() }
     }
 }
-impl ::core::convert::From<&::std::net::SocketAddrV6> for SOCKADDR_INET {
-    fn from(addr: &::std::net::SocketAddrV6) -> Self {
-        SOCKADDR_INET { Ipv6: addr.into() }
-    }
-}
 impl ::core::convert::From<::std::net::SocketAddr> for SOCKADDR_INET {
     fn from(addr: ::std::net::SocketAddr) -> Self {
-        match addr {
-            ::std::net::SocketAddr::V4(socket_addr_v4) => socket_addr_v4.into(),
-            ::std::net::SocketAddr::V6(socket_addr_v6) => socket_addr_v6.into(),
-        }
-    }
-}
-impl ::core::convert::From<&::std::net::SocketAddr> for SOCKADDR_INET {
-    fn from(addr: &::std::net::SocketAddr) -> Self {
         match addr {
             ::std::net::SocketAddr::V4(socket_addr_v4) => socket_addr_v4.into(),
             ::std::net::SocketAddr::V6(socket_addr_v6) => socket_addr_v6.into(),
