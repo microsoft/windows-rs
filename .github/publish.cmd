b31e99660a7485c7b7432501fe85b48f9305aa6c
Kenny Kerr <kenny@kennykerr.ca>
2022-05-18 19:16:45 -0700

    Version 0.37.0

---
+++ .github/publish.cmd
@@ -0,0 +1,11 @@
+cargo publish windows_aarch64_msvc
+cargo publish windows_i686_gnu
+cargo publish windows_i686_msvc
+cargo publish windows_x86_64_gnu
+cargo publish windows_x86_64_msvc
+cargo publish windows-tokens
+cargo publish windows-metadata
+cargo publish windows-interface
+cargo publish windows-implement
+cargo publish windows-bindgen
+cargo publish windows
