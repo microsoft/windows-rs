65b99b7c39b513ffe7134d316d6b1b7a28b84183
Aron Parker <hl3mukkel@gmail.com>
2022-06-04 22:26:38 +0200

    Revert "Reduce allocations in Tree::flatten()"
    This reverts commit 51852797a0bc387d9e33cff2c3f8784b7e91856d.

--- crates/libs/metadata/src/reader/tree.rs
+++ crates/libs/metadata/src/reader/tree.rs
@@ -19,17 +19,9 @@ impl<'a> Tree<'a> {
         }
     }
     pub fn flatten(&self) -> Vec<&Self> {
-        let mut res = Vec::new();
-        let mut stack = vec![self];
-
-        while let Some(tree) = stack.pop() {
-            res.push(tree);
-            stack.extend(tree.nested.values().rev());
-        }
-
-        res
+        // TODO: surely there's a way to do this without a ton of intermediate Vec's...
+        std::iter::once(self).chain(self.nested.values().flat_map(|tree| tree.flatten())).collect()
     }
-
     pub fn seek(mut self, namespace: &'a str) -> Option<Self> {
         if let Some(next) = namespace.find('.') {
             self.nested.remove(&namespace[..next]).and_then(|tree| tree.seek(&namespace[next + 1..]))
