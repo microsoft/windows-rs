acba9c23f0a66d87cc7d492620b01938b7ed74d9
Kenny Kerr <kenny@kennykerr.ca>
2022-06-07 09:09:23 -0700

    Test error propagation (#1803)

---
+++ crates/tests/error/tests/propagate.rs
@@ -0,0 +1,19 @@
+use windows::{core::*, Foundation::*, Win32::Foundation::*};
+
+#[implement(IStringable)]
+struct Test;
+
+impl IStringable_Impl for Test {
+    fn ToString(&self) -> Result<HSTRING> {
+        Err(Error::new(E_INVALIDARG, "Test message".into()))
+    }
+}
+
+#[test]
+fn test() {
+    let test: IStringable = Test.into();
+    let result = test.ToString();
+    let error = result.unwrap_err();
+    assert_eq!(error.code(), E_INVALIDARG);
+    assert_eq!(error.message(), "Test message");
+}
