ff5a9069dba1ed9b78a21f16c888c49bc850afd8
Kenny Kerr <kenny@kennykerr.ca>
2022-06-07 09:05:55 -0700

    trim_end

--- crates/tests/win32/tests/hresult.rs
+++ crates/tests/win32/tests/hresult.rs
@@ -8,9 +8,9 @@ fn test_message() {
 
     let code: HRESULT = ERROR_SUCCESS.into();
     let message: String = code.message().try_into().unwrap();
-    assert_eq!(message.trim_end(), "The operation completed successfully.");
+    assert_eq!(message, "The operation completed successfully.");
 
     let code: HRESULT = ERROR_IO_PENDING.into();
     let message: String = code.message().try_into().unwrap();
-    assert_eq!(message.trim_end(), "Overlapped I/O operation is in progress.");
+    assert_eq!(message, "Overlapped I/O operation is in progress.");
 }
