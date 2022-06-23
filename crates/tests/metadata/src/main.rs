7e05a5a4d96ac8a647befa5947ec1c757a4364e9
Kenny Kerr <kenny@kennykerr.ca>
2022-06-23 09:55:27 -0700

    test

--- crates/tests/metadata/src/main.rs
+++ crates/tests/metadata/src/main.rs
@@ -1,17 +1,27 @@
 fn main() {
     use metadata::reader::*;
 
+    println!("-- Windows --");
+
     let files = vec![File::new(r#"d:\git\windows-rs\crates\libs\metadata\default\Windows.winmd"#).unwrap()];
     let reader = &Reader::new(&files);
     let def = reader.get(TypeName::new("Windows.Foundation", "AsyncStatus")).next().unwrap();
-    let field = reader.type_def_fields(def).next().unwrap();
-    let blob = reader.row_blob(field.0, 2);
-    dbg!(blob.slice);
+
+    for field in reader.type_def_fields(def) {
+        println!("{}", reader.field_name(field));
+        let blob = reader.row_blob(field.0, 2);
+        dbg!(blob.slice);
+    }
+
+    println!("-- TestWindows --");
 
     let files = vec![File::new(r#"C:\Users\kekerr\AppData\Local\Temp\test_metadata.winmd"#).unwrap()];
     let reader = &Reader::new(&files);
     let def = reader.get(TypeName::new("TestWindows.Foundation", "AsyncStatus")).next().unwrap();
-    let field = reader.type_def_fields(def).next().unwrap();
-    let blob = reader.row_blob(field.0, 2);
-    dbg!(blob.slice);
+
+    for field in reader.type_def_fields(def) {
+        println!("{}", reader.field_name(field));
+        let blob = reader.row_blob(field.0, 2);
+        dbg!(blob.slice);
+    }
 }
