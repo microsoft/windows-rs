1d325f4f8242450dd2a7ee48a8f577ad857a5cc4
Kenny Kerr <kenny@kennykerr.ca>
2022-07-11 16:00:58 -0700

    sample

--- crates/samples/message_box/src/main.rs
+++ crates/samples/message_box/src/main.rs
@@ -1,8 +1,14 @@
-use windows::core::PCSTR;
-use windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+use windows::core::*;
+use windows::Win32::UI::WindowsAndMessaging::*;
 
 fn main() {
     unsafe {
-        MessageBoxA(None, PCSTR("Hello".as_bytes().as_ptr()), PCSTR("World".as_bytes().as_ptr()), MB_OK);
+        // TODO: these can now be replaced with the s! and w! macros (coming up next).
+
+        MessageBoxA(None, PCSTR("Ansi\0".as_bytes().as_ptr()), PCSTR("World\0".as_bytes().as_ptr()), MB_OK);
+
+        let hello = HSTRING::from("Wide");
+        let world = HSTRING::from("World");
+        MessageBoxW(None, &hello, &world, MB_OK);
     }
 }
