975ffd17fa3129efcd0e74bb925e48cd1d6bdabc
Arpad Borsos <swatinem@swatinem.de>
2022-07-21 09:52:58 +0200

    ref: Replace unsafe strlen usage by safe Rust
    Removes the `unsafe` usage of C strlen and unchecked utf8 decoding
    from `File::str`, replacing it with safe (but panicing) Rust code.
    This also documents the method at the same time.

--- crates/libs/metadata/src/reader/file.rs
+++ crates/libs/metadata/src/reader/file.rs
@@ -333,13 +333,21 @@ impl File {
         }
     }
 
+    /// Returns the string from the `#Strings` stream as referenced by
+    /// the (table, row, column) triple.
+    ///
+    /// # Panics
+    ///
+    /// * When any element of the (table, row, column) triple is invalid.
+    /// * When the offset in the string table is out of bounds.
+    /// * When no null terminator can be found in the string table.
+    /// * When the null-terminated string is not valid utf-8.
     pub fn str(&self, row: usize, table: usize, column: usize) -> &str {
         let offset = self.strings + self.usize(row, table, column);
 
-        unsafe {
-            let len = strlen(self.bytes.as_ptr().add(offset));
-            std::str::from_utf8_unchecked(&self.bytes[offset..offset + len])
-        }
+        let bytes = &self.bytes[offset..];
+        let nul_pos = bytes.iter().position(|&c| c == 0).expect("expected null-terminated C-string");
+        std::str::from_utf8(&bytes[..nul_pos]).expect("expected valid utf-8 C-string")
     }
 
     pub fn blob(&self, row: usize, table: usize, column: usize) -> &[u8] {
