af096a3be54cea73e0ed46a71b58b4e9165f4c6b
Kenny Kerr <kenny@kennykerr.ca>
2022-09-20 09:06:29 -0500

    test

---
+++ crates/tests/reserved/tests/tests.rs
@@ -0,0 +1,19 @@
+use windows::{core::*, Win32::System::Registry::*, Win32::System::Threading::*, Win32::UI::WindowsAndMessaging::*};
+
+/// Tests a few APIs that have reserved parameters to ensure they can be called with `None`. 
+#[test]
+fn test() -> Result<()> {
+    unsafe {
+        assert_eq!(InSendMessageEx(None), ISMEX_NOSEND);
+        assert!(CreateThreadpool(None).0 != 0);
+
+        let mut key = HKEY::default();
+        RegOpenKeyExA(HKEY_CLASSES_ROOT, s!(r".txt"), 0, KEY_QUERY_VALUE, &mut key).ok()?;
+        let mut len = 0;
+        RegQueryValueExA(key, s!("Content Type"), None, None, None, Some(&mut len)).ok()?;
+        let mut buffer = vec![0u8; (len) as usize];
+        RegQueryValueExA(key, s!("Content Type"), None, None, Some(buffer.as_mut_ptr() as _), Some(&mut len)).ok()?;
+        assert_eq!(String::from_utf8_lossy(&buffer), "text/plain\0");
+        Ok(())
+    }
+}
