14f69a9fa1a78b761c556b68b0ec940f8214225d
ridwanabdillahi <91507758+ridwanabdillahi@users.noreply.github.com>
2022-09-20 14:34:04 -0700

    Respond to PR comments. Move windows.natvis to crates/libs/windows and readme to docs/.

--- crates/libs/windows/src/lib.rs
+++ crates/libs/windows/src/lib.rs
@@ -4,7 +4,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 
 #![doc(html_no_source)]
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, clashing_extern_declarations, unused_variables, dead_code, clippy::all)]
-#![cfg_attr(feature = "debugger_visualizer", feature(debugger_visualizer), debugger_visualizer(natvis_file = "../../../../debug_metadata/windows.natvis"))]
+#![cfg_attr(feature = "debugger_visualizer", feature(debugger_visualizer), debugger_visualizer(natvis_file = "../windows.natvis"))]
 
 extern crate self as windows;
 mod Windows;
