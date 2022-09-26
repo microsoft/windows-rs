4d584ea0b5dc9ee2274dd5ef5ee805773e046fa3
Kenny Kerr <kenny@kennykerr.ca>
2022-09-26 16:12:43 -0500

    ldap

--- crates/tests/calling_convention/tests/sys.rs
+++ crates/tests/calling_convention/tests/sys.rs
@@ -4,6 +4,6 @@ use windows_sys::{Win32::Foundation::*, Win32::Networking::Ldap::*};
 fn test() {
     // TODO: workaround for https://github.com/microsoft/win32metadata/issues/1211
     unsafe {
-        assert_eq!(ERROR_BUSY, LdapMapErrorToWin32(LDAP_BUSY as _));
+        assert_eq!(ERROR_BUSY, LdapMapErrorToWin32(LDAP_BUSY));
     }
 }
