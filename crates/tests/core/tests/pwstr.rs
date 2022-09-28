abd087cd49bad4b9607a3015184af1ed1b0102d9
Kenny Kerr <kenny@kennykerr.ca>
2022-09-28 10:36:21 -0500

    tests

--- crates/tests/core/tests/pwstr.rs
+++ crates/tests/core/tests/pwstr.rs
@@ -1,4 +1,4 @@
-use windows::core::*;
+use windows::{core::*, Win32::Foundation::*};
 
 #[test]
 fn test() -> Result<()> {
@@ -6,5 +6,12 @@ fn test() -> Result<()> {
     let s: String = unsafe { p.to_string()? };
     assert_eq!("hello", s);
     assert_eq!("hello", format!("{}", unsafe { p.display() }));
+
+    let invalid = &mut [0xD834, 0xDD1E, 0x006d, 0x0075, 0xD800, 0x0069, 0x0063];
+    let p = PWSTR::from_raw(invalid.as_mut_ptr());
+    let e: Error = unsafe { p.to_string().unwrap_err().into() };
+    assert_eq!(e.code(), ERROR_NO_UNICODE_TRANSLATION.into());
+    assert_eq!(e.message(), "No mapping for the Unicode character exists in the target multi-byte code page.");
+
     Ok(())
 }
