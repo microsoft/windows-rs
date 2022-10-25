fd0929d7f7f4e5c683793e93f7bb9cbfe00993e5
Kenny Kerr <kenny@kennykerr.ca>
2022-10-25 11:44:01 -0500

    counter

---
+++ crates/samples/counter/src/main.rs
@@ -0,0 +1,21 @@
+use windows::{core::*, Win32::System::Performance::*};
+
+fn main() {
+    unsafe {
+        let mut query = 0;
+        PdhOpenQueryW(None, 0, &mut query);
+
+        let mut counter = 0;
+        PdhAddCounterW(query, w!("\\Processor(0)\\% Processor Time"), 0, &mut counter);
+
+        loop {
+            std::thread::sleep(std::time::Duration::new(1, 0));
+            PdhCollectQueryData(query);
+
+            let mut value = Default::default();
+            if 0 == PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, None, &mut value) {
+                println!("{:.2}", value.Anonymous.doubleValue);
+            }
+        }
+    }
+}
---
+++ crates/samples/counter_sys/src/main.rs
@@ -0,0 +1,21 @@
+use windows_sys::{core::*, Win32::System::Performance::*};
+
+fn main() {
+    unsafe {
+        let mut query = 0;
+        PdhOpenQueryW(std::ptr::null(), 0, &mut query);
+
+        let mut counter = 0;
+        PdhAddCounterW(query, w!("\\Processor(0)\\% Processor Time"), 0, &mut counter);
+
+        loop {
+            std::thread::sleep(std::time::Duration::new(1, 0));
+            PdhCollectQueryData(query);
+
+            let mut value = std::mem::zeroed();
+            if 0 == PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, std::ptr::null_mut(), &mut value) {
+                println!("{:.2}", value.Anonymous.doubleValue);
+            }
+        }
+    }
+}
