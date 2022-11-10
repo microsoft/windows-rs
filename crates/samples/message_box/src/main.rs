b7d22b26ba2912cb168f042da9a4865016b23d7a
Kenny Kerr <kenny@kennykerr.ca>
2022-11-10 12:21:17 -0600

    samples

--- crates/samples/message_box/src/main.rs
+++ crates/samples/message_box/src/main.rs
@@ -1,10 +1,8 @@
-use windows::core::*;
-use windows::Win32::UI::WindowsAndMessaging::*;
+use windows::{core::*, Win32::UI::Shell::*, Win32::UI::WindowsAndMessaging::*};
 
 fn main() {
     unsafe {
         MessageBoxA(None, s!("Ansi"), s!("World"), MB_OK);
-        MessageBoxW(None, w!("Wide"), w!("World"), MB_OK);
-        MessageBoxW(None, h!("WinRT"), h!("World"), MB_OK);
+        ShellMessageBoxW(None, None, w!("Wide"), w!("World"), MB_ICONERROR.0);
     }
 }
