5f8c17a4f8cdb31f7f18adbe83cec5830bfed260
Kenny Kerr <kenny@kennykerr.ca>
2022-11-11 16:48:12 -0600

    test

--- crates/tests/calling_convention/tests/sys.rs
+++ crates/tests/calling_convention/tests/sys.rs
@@ -1,7 +1,7 @@
 use windows_sys::{Win32::Foundation::*, Win32::Networking::Ldap::*, Win32::System::SystemInformation::*};
 
 #[test]
-fn test() {
+fn calling_convention() {
     unsafe {
         // This function requires cdecl on x86.
         assert_eq!(ERROR_BUSY, LdapMapErrorToWin32(LDAP_BUSY));
@@ -10,3 +10,15 @@ fn test() {
         GetTickCount();
     }
 }
+
+#[test]
+#[cfg(windows_delay_load)]
+fn delay_load() {}
+
+#[test]
+#[cfg(windows_raw_dylib)]
+fn raw_dylib() {}
+
+#[test]
+#[cfg(all(windows_delay_load, windows_raw_dylib))]
+fn delay_load_and_raw_dylib() {}
