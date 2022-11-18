198c15b31061b9fd87c71082197dc3781415b3b1
Kenny Kerr <kenny@kennykerr.ca>
2022-11-18 13:09:08 -0600

    extern

--- crates/libs/sys/src/lib.rs
+++ crates/libs/sys/src/lib.rs
@@ -4,7 +4,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 
 #![no_std]
 #![doc(html_no_source)]
-#![allow(non_snake_case)]
+#![allow(non_snake_case, clashing_extern_declarations)]
 #![cfg_attr(windows_raw_dylib, feature(raw_dylib, native_link_modifiers_verbatim))]
 
 extern crate self as windows_sys;
