7edf99261ea1472f96b2a7e890794340db7a5600
Kenny Kerr <kenny@kennykerr.ca>
2022-11-29 08:57:02 -0600

    verbatim is now stable

--- crates/libs/windows/src/lib.rs
+++ crates/libs/windows/src/lib.rs
@@ -5,7 +5,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 #![doc(html_no_source)]
 #![allow(non_snake_case, clashing_extern_declarations)]
 #![cfg_attr(windows_debugger_visualizer, feature(debugger_visualizer), debugger_visualizer(natvis_file = "../windows.natvis"))]
-#![cfg_attr(windows_raw_dylib, feature(raw_dylib, native_link_modifiers_verbatim))]
+#![cfg_attr(windows_raw_dylib, feature(raw_dylib))]
 
 extern crate self as windows;
 mod Windows;
