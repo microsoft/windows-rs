c9081f871293b9aaa0aee30da03bc5efa68f4841
Kyle Bloom <kylejbloom@gmail.com>
2022-11-30 13:54:24 +0000

    Use constants in test

--- crates/tests/extensions/tests/variant_bool.rs
+++ crates/tests/extensions/tests/variant_bool.rs
@@ -3,9 +3,9 @@ use windows::Win32::Foundation::*;
 #[test]
 fn test() {
     let status = VARIANT_BOOL::default();
-    assert_eq!(status.0, 0);
+    assert_eq!(status, VARIANT_FALSE);
 
-    let status = VARIANT_BOOL(-1);
+    let status = VARIANT_TRUE;
     assert_eq!(status.0, -1);
     assert_eq!(status.as_bool(), true);
     assert_eq!(status.ok().is_ok(), true);
@@ -13,7 +13,7 @@ fn test() {
     unsafe {
         SetLastError(ERROR_ACCESS_DENIED);
     }
-    let status = VARIANT_BOOL(0);
+    let status = VARIANT_FALSE;
     assert_eq!(status.0, 0);
     assert_eq!(status.as_bool(), false);
     let result = status.ok();
