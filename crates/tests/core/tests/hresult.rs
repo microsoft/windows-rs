fc170b14ad1bcc4e20d196789f81129fb5c9012a
Kenny Kerr <kenny@kennykerr.ca>
2022-12-05 16:13:09 -0600

    Propagate error information through `HRESULT`'s `ok` method (#2228)

---
+++ crates/tests/core/tests/hresult.rs
@@ -0,0 +1,12 @@
+use windows::{core::*, Win32::Foundation::*};
+
+#[test]
+fn ok() {
+    let error = Error::new(E_FAIL, "test info".into());
+    let code: HRESULT = error.into(); // SetErrorInfo is called before dropping the Error object.
+    let result: Result<()> = code.ok(); // GetErrorInfo is called to retrieve the error info.
+
+    let error = result.unwrap_err();
+    assert_eq!(error.code(), E_FAIL);
+    assert_eq!(error.message(), "test info");
+}
