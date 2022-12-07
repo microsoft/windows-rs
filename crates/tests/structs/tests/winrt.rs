03b23624e30d58e28668dad37e47891decdeaf19
Kenny Kerr <kenny.kerr@microsoft.com>
2022-12-07 15:36:12 -0600

    comment

--- crates/tests/structs/tests/winrt.rs
+++ crates/tests/structs/tests/winrt.rs
@@ -2,7 +2,8 @@ use windows::Storage::Search::*;
 
 #[test]
 fn test() {
-    // SortyEntry is a rare WinRT struct with non-blittable fields.
+    // SortyEntry is a WinRT struct with non-blittable fields. This just validates that it can
+    // be used directly and isn't wrapped inside ManuallyDrop.
 
     let mut se = SortEntry::default();
     assert!(se.PropertyName.is_empty());
