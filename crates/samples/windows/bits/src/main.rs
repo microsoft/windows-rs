faf5f560660f0d9618a17a67d18e096e61b883dc
Kenny Kerr <kenny.kerr@microsoft.com>
2023-01-11 16:09:09 -0600

    bits sample

---
+++ crates/samples/windows/bits/src/main.rs
@@ -0,0 +1,64 @@
+use windows::{
+    core::*, Win32::Networking::BackgroundIntelligentTransferService::*, Win32::System::Com::*,
+};
+
+fn main() -> windows::core::Result<()> {
+    unsafe {
+        CoInitializeEx(None, COINIT_MULTITHREADED)?;
+
+        let manager: IBackgroundCopyManager =
+            CoCreateInstance(&BackgroundCopyManager, None, CLSCTX_LOCAL_SERVER)?;
+
+        let mut job_id = Default::default();
+        let mut job = None;
+        manager.CreateJob(w!("sample"), BG_JOB_TYPE_DOWNLOAD, &mut job_id, &mut job)?;
+        let job = job.unwrap();
+        job.AddFile(w!("https://kennykerr.ca/favicon.svg"), w!("D:\\rust.svg"))?;
+
+        let callback: IBackgroundCopyCallback = Callback.into();
+        job.SetNotifyInterface(&callback)?;
+        job.SetNotifyFlags(BG_NOTIFY_JOB_TRANSFERRED | BG_NOTIFY_JOB_ERROR)?;
+
+        job.Resume()?;
+        println!("downloading...");
+
+        getchar();
+        job.Cancel()?;
+        println!("canceled");
+        Ok(())
+    }
+}
+
+#[implement(IBackgroundCopyCallback)]
+struct Callback;
+
+impl IBackgroundCopyCallback_Impl for Callback {
+    fn JobTransferred(&self, job: &Option<IBackgroundCopyJob>) -> Result<()> {
+        let job = job.as_ref().unwrap();
+        unsafe { job.Complete()? };
+        println!("done");
+        std::process::exit(0);
+    }
+
+    fn JobError(
+        &self,
+        job: &Option<IBackgroundCopyJob>,
+        error: &Option<IBackgroundCopyError>,
+    ) -> Result<()> {
+        let job = job.as_ref().unwrap();
+        let error = error.as_ref().unwrap();
+        unsafe {
+            job.Cancel()?;
+            println!("{}", error.GetErrorDescription(0)?.display());
+        }
+        std::process::exit(0);
+    }
+
+    fn JobModification(&self, _: &Option<IBackgroundCopyJob>, _: u32) -> Result<()> {
+        Ok(())
+    }
+}
+
+extern "C" {
+    pub fn getchar() -> i32;
+}
