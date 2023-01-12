87ef21682fa31e3595ccda8096de7f97a0f0f11a
Kenny Kerr <kenny.kerr@microsoft.com>
2023-01-12 12:06:35 -0600

    Update publish script

--- docs/publish.cmd
+++ docs/publish.cmd
@@ -1,14 +1,17 @@
-cargo publish -p windows_aarch64_gnullvm
-cargo publish -p windows_aarch64_msvc
-cargo publish -p windows_i686_gnu
-cargo publish -p windows_i686_msvc
-cargo publish -p windows_x86_64_gnu
-cargo publish -p windows_x86_64_gnullvm
-cargo publish -p windows_x86_64_msvc
-cargo publish -p windows-tokens
-cargo publish -p windows-metadata
-cargo publish -p windows-interface
-cargo publish -p windows-implement
-cargo publish -p windows-bindgen
-cargo publish -p windows
-cargo publish -p windows-sys
+:: cargo publish -p windows_aarch64_gnullvm
+:: cargo publish -p windows_aarch64_msvc
+:: cargo publish -p windows_i686_gnu
+:: cargo publish -p windows_i686_msvc
+:: cargo publish -p windows_x86_64_gnu
+:: cargo publish -p windows_x86_64_gnullvm
+:: cargo publish -p windows_x86_64_msvc
+
+:: cargo publish -p windows-targets
+:: cargo publish -p windows-tokens
+:: cargo publish -p windows-metadata
+:: cargo publish -p windows-interface
+:: cargo publish -p windows-implement
+:: cargo publish -p windows-bindgen
+:: cargo publish -p windows
+
+:: cargo publish -p windows-sys
