f283e806283b6c29dfbe90da4dd70bd1c299abd8
Kenny Kerr <kenny@kennykerr.ca>
2023-02-24 09:51:10 -0600

    test

--- crates/tests/arch_feature/tests/win.rs
+++ crates/tests/arch_feature/tests/win.rs
@@ -1,9 +1,19 @@
-use windows::{Win32::System::Diagnostics::Debug::CONTEXT, Win32::System::Environment::VBS_BASIC_ENCLAVE_BASIC_CALL_CREATE_THREAD};
+use windows::{Win32::Graphics::Gdi::*, Win32::System::Diagnostics::Debug::*, Win32::System::Environment::*, Win32::UI::Controls::Dialogs::*};
 
 #[test]
 #[cfg(target_arch = "x86_64")]
 fn test() {
     assert_eq!(1232, core::mem::size_of::<CONTEXT>());
+    assert_eq!(16, core::mem::align_of::<CONTEXT>());
+
+    assert_eq!(1280, core::mem::size_of::<MINIDUMP_THREAD_CALLBACK>());
+    assert_eq!(16, core::mem::align_of::<MINIDUMP_THREAD_CALLBACK>());
+
+    assert_eq!(72, core::mem::size_of::<CHOOSECOLORA>());
+    assert_eq!(8, core::mem::align_of::<CHOOSECOLORA>());
+
+    assert_eq!(14, core::mem::size_of::<BITMAPFILEHEADER>());
+    assert_eq!(2, core::mem::align_of::<BITMAPFILEHEADER>());
 
     use windows::Win32::System::Environment::VBS_BASIC_ENCLAVE_THREAD_DESCRIPTOR64;
 
@@ -19,6 +29,16 @@ fn test() {
 #[cfg(target_arch = "x86")]
 fn test() {
     assert_eq!(716, core::mem::size_of::<CONTEXT>());
+    assert_eq!(4, core::mem::align_of::<CONTEXT>());
+
+    assert_eq!(744, core::mem::size_of::<MINIDUMP_THREAD_CALLBACK>());
+    assert_eq!(4, core::mem::align_of::<MINIDUMP_THREAD_CALLBACK>());
+
+    assert_eq!(36, core::mem::size_of::<CHOOSECOLORA>());
+    assert_eq!(1, core::mem::align_of::<CHOOSECOLORA>());
+
+    assert_eq!(14, core::mem::size_of::<BITMAPFILEHEADER>());
+    assert_eq!(2, core::mem::align_of::<BITMAPFILEHEADER>());
 
     use windows::Win32::System::Environment::VBS_BASIC_ENCLAVE_THREAD_DESCRIPTOR32;
 
