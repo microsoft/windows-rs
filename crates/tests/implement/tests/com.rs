4d0126d398adc34390aad22381b2e6c4d9daef7a
Kenny Kerr <kenny@kennykerr.ca>
2023-03-15 14:23:09 -0500

    https://github.com/microsoft/win32metadata/issues/1498

--- crates/tests/implement/tests/com.rs
+++ crates/tests/implement/tests/com.rs
@@ -3,20 +3,14 @@
 use windows::core::*;
 use windows::Foundation::*;
 use windows::Win32::Foundation::HANDLE;
-use windows::Win32::System::WinRT::Composition::*;
 use windows::Win32::System::WinRT::Display::*;
 
-#[implement(windows::Foundation::IStringable, windows::Win32::System::WinRT::Composition::ISwapChainInterop, windows::Win32::System::WinRT::Display::IDisplayPathInterop)]
+#[implement(windows::Foundation::IStringable, windows::Win32::System::WinRT::Display::IDisplayPathInterop)]
 struct Mix;
 
 impl IStringable_Impl for Mix {
     fn ToString(&self) -> Result<HSTRING> {
         Ok("Mix".into())
-    }
-}
-impl ISwapChainInterop_Impl for Mix {
-    fn SetSwapChain(&self, _: Option<&IUnknown>) -> Result<()> {
-        Ok(())
     }
 }
 
@@ -32,8 +26,7 @@ impl IDisplayPathInterop_Impl for Mix {
 
 #[test]
 fn mix() -> Result<()> {
-    let a: ISwapChainInterop = Mix.into();
-    unsafe { a.SetSwapChain(None)? };
+    let a: IUnknown = Mix.into();
 
     let b: IStringable = a.cast()?;
     assert!(b.ToString()? == "Mix");
@@ -41,8 +34,7 @@ fn mix() -> Result<()> {
     let c: IStringable = Mix.into();
     assert!(c.ToString()? == "Mix");
 
-    let d: ISwapChainInterop = c.cast()?;
-    unsafe { d.SetSwapChain(None)? };
+    let d: IUnknown = c.cast()?;
 
     let e: IDisplayPathInterop = d.cast()?;
     unsafe { assert!(e.GetSourceId()? == 123) };
