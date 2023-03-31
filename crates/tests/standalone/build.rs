a7e4ebdd72883601e8b70874e2bd4100471b08fb
Kenny Kerr <kenny@kennykerr.ca>
2023-03-31 10:48:20 -0500

    cfg

--- crates/tests/standalone/build.rs
+++ crates/tests/standalone/build.rs
@@ -1,11 +1,6 @@
 fn main() {
     let apis = [
-        "Windows.Win32.Foundation.BOOL",
         "Windows.Win32.Foundation.CloseHandle",
-        "Windows.Win32.Foundation.HANDLE",
-        "Windows.Win32.Foundation.WIN32_ERROR",
-        "Windows.Win32.Security.SECURITY_ATTRIBUTES",
-        "Windows.Win32.System.Com.CLSCTX",
         "Windows.Win32.System.Com.CoCreateInstance",
         "Windows.Win32.System.Com.STGTY_REPEAT",
         "Windows.Win32.System.Threading.CreateEventW",
