7c67d96c3ef640f9913cb8adea2ffe7c9d09c62a
Kenny Kerr <kenny@kennykerr.ca>
2023-04-03 16:51:22 -0500

    core types

--- crates/tests/standalone/src/lib.rs
+++ crates/tests/standalone/src/lib.rs
@@ -1,10 +1,100 @@
 #![cfg(test)]
 
-mod bindings;
-use bindings::*;
+mod b_bstr;
+mod b_guid;
+mod b_hresult;
+mod b_hstring;
+mod b_inspectable;
+mod b_none;
+mod b_pcstr;
+mod b_pcwstr;
+mod b_pstr;
+mod b_pwstr;
+mod b_test;
+mod b_unknown;
+
+#[test]
+fn bstr() {
+    unsafe {
+        b_bstr::SysAllocString(std::ptr::null());
+    }
+}
+
+#[test]
+fn guid() {
+    unsafe {
+        b_guid::CoCreateGuid(std::ptr::null_mut());
+    }
+}
+
+#[test]
+fn hresult() {
+    unsafe {
+        b_hresult::CoInitialize(std::ptr::null());
+    }
+}
+
+#[test]
+fn hstring() {
+    unsafe {
+        b_hstring::WindowsGetStringLen(std::ptr::null_mut());
+    }
+}
+
+#[test]
+fn inspectable() {
+    unsafe {
+        b_inspectable::RoActivateInstance(std::ptr::null_mut(), &mut std::ptr::null_mut());
+    }
+}
+
+#[test]
+fn none() {
+    unsafe {
+        b_none::GetTickCount();
+    }
+}
+
+#[test]
+fn pcstr() {
+    unsafe {
+        b_pcstr::lstrlenA(std::ptr::null());
+    }
+}
+
+#[test]
+fn pcwstr() {
+    unsafe {
+        b_pcwstr::lstrlenW(std::ptr::null());
+    }
+}
+
+#[test]
+fn pstr() {
+    unsafe {
+        let mut buffer = [0; 10];
+        b_pstr::VarI1FromDate(0.0, buffer.as_mut_ptr());
+    }
+}
+
+#[test]
+fn pwstr() {
+    let _ = b_pwstr::CALPOLESTR {
+        cElems: 0,
+        pElems: std::ptr::null_mut(),
+    };
+}
+
+#[test]
+fn unknown() {
+    unsafe {
+        b_unknown::CoIsHandlerConnected(std::ptr::null_mut());
+    }
+}
 
 #[test]
 fn test() {
+    use b_test::*;
     unsafe {
         let event = CreateEventW(std::ptr::null(), 1, 0, std::ptr::null());
         SetEvent(event);
