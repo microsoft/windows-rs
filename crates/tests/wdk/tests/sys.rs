d841117347f73dabd397d69d4163a3e7490035f0
Kenny Kerr <kenny@kennykerr.ca>
2023-04-14 09:36:59 -0500

    wdk test

--- crates/tests/wdk/tests/sys.rs
+++ crates/tests/wdk/tests/sys.rs
@@ -1,10 +1,18 @@
-use windows_sys::Wdk::System::OfflineRegistry::*;
+use windows_sys::{Wdk::System::OfflineRegistry::*, Wdk::System::SystemServices::*};
 
 #[test]
-fn test() {
+fn offline_registry() {
     unsafe {
         let mut hive = 0;
         ORCreateHive(&mut hive);
         ORCloseHive(hive);
+    }
+}
+
+// TODO: need the corresponding windows crate test once https://github.com/microsoft/wdkmetadata/issues/31 is fixed.
+#[test]
+fn ntoskrnl() {
+    unsafe {
+        assert_eq!(ExIsSoftBoot(), 0);
     }
 }
