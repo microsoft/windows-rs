1631579ae6223a8d005bc458ace94bc9d7e9b45c
Kenny Kerr <kenny@kennykerr.ca>
2023-06-26 16:12:04 -0500

    test

--- crates/tests/riddle/src/params.rs
+++ crates/tests/riddle/src/params.rs
@@ -32,6 +32,162 @@ impl IParams {
             .from_abi(result__)
         }
     }
+    pub fn I8(&self, a: &mut i8, b: &mut i8) -> ::windows_core::Result<i8> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).I8)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn U8(&self, a: &mut u8, b: &mut u8) -> ::windows_core::Result<u8> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).U8)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn I16(&self, a: &mut i16, b: &mut i16) -> ::windows_core::Result<i16> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).I16)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn U16(&self, a: &mut u16, b: &mut u16) -> ::windows_core::Result<u16> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).U16)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn I32(&self, a: &mut i32, b: &mut i32) -> ::windows_core::Result<i32> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).I32)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn U32(&self, a: &mut u32, b: &mut u32) -> ::windows_core::Result<u32> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).U32)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn I64(&self, a: &mut i64, b: &mut i64) -> ::windows_core::Result<i64> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).I64)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn U64(&self, a: &mut u64, b: &mut u64) -> ::windows_core::Result<u64> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).U64)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn F32(&self, a: &mut f32, b: &mut f32) -> ::windows_core::Result<f32> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).F32)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn F64(&self, a: &mut f64, b: &mut f64) -> ::windows_core::Result<f64> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).F64)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn ISize(&self, a: &mut isize, b: &mut isize) -> ::windows_core::Result<isize> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).ISize)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
+    pub fn USize(&self, a: &mut usize, b: &mut usize) -> ::windows_core::Result<usize> {
+        let this = self;
+        unsafe {
+            let mut result__ = ::std::mem::zeroed();
+            (::windows_core::Interface::vtable(this).USize)(
+                ::windows_core::Interface::as_raw(this),
+                a,
+                b,
+                &mut result__,
+            )
+            .from_abi(result__)
+        }
+    }
 }
 ::windows_core::imp::interface_hierarchy!(
     IParams,
@@ -75,5 +231,77 @@ pub struct IParams_Vtbl {
         a: *mut bool,
         b: *mut bool,
         result__: *mut bool,
+    ) -> ::windows_core::HRESULT,
+    pub I8: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut i8,
+        b: *mut i8,
+        result__: *mut i8,
+    ) -> ::windows_core::HRESULT,
+    pub U8: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut u8,
+        b: *mut u8,
+        result__: *mut u8,
+    ) -> ::windows_core::HRESULT,
+    pub I16: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut i16,
+        b: *mut i16,
+        result__: *mut i16,
+    ) -> ::windows_core::HRESULT,
+    pub U16: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut u16,
+        b: *mut u16,
+        result__: *mut u16,
+    ) -> ::windows_core::HRESULT,
+    pub I32: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut i32,
+        b: *mut i32,
+        result__: *mut i32,
+    ) -> ::windows_core::HRESULT,
+    pub U32: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut u32,
+        b: *mut u32,
+        result__: *mut u32,
+    ) -> ::windows_core::HRESULT,
+    pub I64: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut i64,
+        b: *mut i64,
+        result__: *mut i64,
+    ) -> ::windows_core::HRESULT,
+    pub U64: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut u64,
+        b: *mut u64,
+        result__: *mut u64,
+    ) -> ::windows_core::HRESULT,
+    pub F32: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut f32,
+        b: *mut f32,
+        result__: *mut f32,
+    ) -> ::windows_core::HRESULT,
+    pub F64: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut f64,
+        b: *mut f64,
+        result__: *mut f64,
+    ) -> ::windows_core::HRESULT,
+    pub ISize: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut isize,
+        b: *mut isize,
+        result__: *mut isize,
+    ) -> ::windows_core::HRESULT,
+    pub USize: unsafe extern "system" fn(
+        this: *mut ::core::ffi::c_void,
+        a: *mut usize,
+        b: *mut usize,
+        result__: *mut usize,
     ) -> ::windows_core::HRESULT,
 }
--- crates/tests/riddle/tests/params.rs
+++ crates/tests/riddle/tests/params.rs
@@ -12,11 +12,50 @@ fn primitives() {
         .expect("Type missing");
 
     assert_eq!(reader.type_def_kind(def), TypeKind::Interface);
+    let generics = &reader.type_def_generics(def);
 
     assert!(reader.type_def_fields(def).next().is_none());
     let methods: Vec<MethodDef> = reader.type_def_methods(def).collect();
-    assert_eq!(methods.len(), 2);
+    assert_eq!(methods.len(), 14);
 
     assert_eq!(reader.method_def_name(methods[0]), "Nothing");
+    let sig = reader.method_def_signature(methods[0], generics);
+    assert_eq!(sig.return_type, Type::Void);
+    assert!(sig.params.is_empty());
+
     assert_eq!(reader.method_def_name(methods[1]), "Bool");
+    assert_eq!(reader.method_def_name(methods[2]), "I8");
+    assert_eq!(reader.method_def_name(methods[3]), "U8");
+    assert_eq!(reader.method_def_name(methods[4]), "I16");
+    assert_eq!(reader.method_def_name(methods[5]), "U16");
+    assert_eq!(reader.method_def_name(methods[6]), "I32");
+    assert_eq!(reader.method_def_name(methods[7]), "U32");
+    assert_eq!(reader.method_def_name(methods[8]), "I64");
+    assert_eq!(reader.method_def_name(methods[9]), "U64");
+    assert_eq!(reader.method_def_name(methods[10]), "F32");
+    assert_eq!(reader.method_def_name(methods[11]), "F64");
+    assert_eq!(reader.method_def_name(methods[12]), "ISize");
+    assert_eq!(reader.method_def_name(methods[13]), "USize");
+
+    method(reader, generics, methods[1], Type::Bool);
+    method(reader, generics, methods[2], Type::I8);
+    method(reader, generics, methods[3], Type::U8);
+    method(reader, generics, methods[4], Type::I16);
+    method(reader, generics, methods[5], Type::U16);
+    method(reader, generics, methods[6], Type::I32);
+    method(reader, generics, methods[7], Type::U32);
+    method(reader, generics, methods[8], Type::I64);
+    method(reader, generics, methods[9], Type::U64);
+    method(reader, generics, methods[10], Type::F32);
+    method(reader, generics, methods[11], Type::F64);
+    method(reader, generics, methods[12], Type::ISize);
+    method(reader, generics, methods[13], Type::USize);
+}
+
+fn method(reader: &Reader, generics: &[Type], method: MethodDef, expected: Type) {
+    let sig = reader.method_def_signature(method, generics);
+    assert_eq!(sig.return_type, expected);
+    assert_eq!(sig.params.len(), 2);
+    assert_eq!(sig.params[0].ty, expected);
+    assert_eq!(sig.params[1].ty, expected);
 }
