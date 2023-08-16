81a2cd01f3c80b5284098c76990eb4b99bd22374
Kenny Kerr <kenny@kennykerr.ca>
2023-08-16 15:13:39 -0500

    0.51.1

---
+++ crates/tests/targets/tests/files.rs
@@ -0,0 +1,13 @@
+// This test just makes it easier to ensure that the lib file names match the crate versions
+// so we don't accidentally forget to bump the version numbers embedded in the lib file names.
+
+#[test]
+fn test() {
+    std::include_bytes!("../../../targets/aarch64_gnullvm/lib/libwindows.0.48.3.a");
+    std::include_bytes!("../../../targets/aarch64_msvc/lib/windows.0.48.3.lib");
+    std::include_bytes!("../../../targets/i686_gnu/lib/libwindows.0.48.3.a");
+    std::include_bytes!("../../../targets/i686_msvc/lib/windows.0.48.3.lib");
+    std::include_bytes!("../../../targets/x86_64_gnu/lib/libwindows.0.48.3.a");
+    std::include_bytes!("../../../targets/x86_64_gnullvm/lib/libwindows.0.48.3.a");
+    std::include_bytes!("../../../targets/x86_64_msvc/lib/windows.0.48.3.lib");
+}
