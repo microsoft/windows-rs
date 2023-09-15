86b4806960926c0d24dacec61d73dd4c6679d743
Kenny Kerr <kenny@kennykerr.ca>
2023-09-15 11:45:24 -0500

    all four cases

--- crates/tests/implement/tests/query.rs
+++ crates/tests/implement/tests/query.rs
@@ -1,38 +1,4 @@
-use windows::{core::*, Foundation::*, Win32::Foundation::*};
-
-#[test]
-fn delegate() {
-    unsafe {
-        let interface: EventHandler<i32> = EventHandler::<i32>::new(move |_, _| todo!());
-
-        // Successful query
-        {
-            let mut unknown: Option<IUnknown> = None;
-            let hr = interface.query(&IUnknown::IID, &mut unknown as *mut _ as *mut _);
-            assert_eq!(hr, S_OK);
-            assert_eq!(interface.cast::<IUnknown>().unwrap(), unknown.unwrap());
-        }
-        // Unsuccessful query
-        {
-            let mut closable: Option<IClosable> = None;
-            let hr = interface.query(&IClosable::IID, &mut closable as *mut _ as *mut _);
-            assert_eq!(hr, E_NOINTERFACE);
-            assert_eq!(closable, None);
-        }
-        // iid param is null
-        {
-            let mut unknown: Option<IUnknown> = None;
-            let hr = interface.query(std::ptr::null(), &mut unknown as *mut _ as *mut _);
-            assert_eq!(hr, E_POINTER);
-            assert_eq!(unknown, None);
-        }
-        // interface param is null
-        {
-            let hr = interface.query(&IUnknown::IID, std::ptr::null_mut());
-            assert_eq!(hr, E_POINTER);
-        }
-    }
-}
+use windows::{core::*, Foundation::*, Win32::Foundation::*, Win32::System::WinRT::*};
 
 #[implement(IStringable)]
 struct Stringable;
@@ -44,10 +10,26 @@ impl IStringable_Impl for Stringable {
 }
 
 #[test]
-fn implement() {
+fn test() {
     unsafe {
-        let interface: IStringable = Stringable.into();
+        // This covers the four distinct implementations of QueryInterface.
 
+        let delegate: EventHandler<i32> = EventHandler::<i32>::new(move |_, _| todo!());
+        test_query(&delegate);
+
+        let interface: IStringable = Stringable.into();
+        test_query(&interface);
+
+        let source: IWeakReferenceSource = interface.cast().unwrap();
+        test_query(&source);
+
+        let weak = source.GetWeakReference().unwrap();
+        test_query(&weak);
+    }
+}
+
+fn test_query<I: ComInterface>(interface: &I) {
+    unsafe {
         // Successful query
         {
             let mut unknown: Option<IUnknown> = None;
@@ -55,6 +37,7 @@ fn implement() {
             assert_eq!(hr, S_OK);
             assert_eq!(interface.cast::<IUnknown>().unwrap(), unknown.unwrap());
         }
+
         // Unsuccessful query
         {
             let mut closable: Option<IClosable> = None;
@@ -62,6 +45,7 @@ fn implement() {
             assert_eq!(hr, E_NOINTERFACE);
             assert_eq!(closable, None);
         }
+
         // iid param is null
         {
             let mut unknown: Option<IUnknown> = None;
@@ -69,6 +53,7 @@ fn implement() {
             assert_eq!(hr, E_POINTER);
             assert_eq!(unknown, None);
         }
+
         // interface param is null
         {
             let hr = interface.query(&IUnknown::IID, std::ptr::null_mut());
