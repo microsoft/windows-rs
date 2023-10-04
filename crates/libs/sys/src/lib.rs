c113099ea9401aed971fd237bf3fb1fea5bc7e01
Kenny Kerr <kenny@kennykerr.ca>
2023-10-03 20:34:35 -0500

    docs

--- crates/libs/sys/src/lib.rs
+++ crates/libs/sys/src/lib.rs
@@ -5,7 +5,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 #![no_std]
 #![doc(html_no_source)]
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, clippy::all)]
-#![cfg_attr(not(feature = "doc"), doc(hidden))]
+#![cfg_attr(not(feature = "docs"), doc(hidden))]
 
 extern crate self as windows_sys;
 mod Windows;
