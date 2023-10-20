d2d40f58ac3f9137b69d5d46b402a62e5999ad9e
Kenny Kerr <kenny@kennykerr.ca>
2023-10-20 11:19:14 -0500

    trim_tick

--- crates/libs/metadata/src/tables.rs
+++ crates/libs/metadata/src/tables.rs
@@ -298,7 +298,7 @@ impl TypeDef {
     }
 
     pub fn name(&self) -> &'static str {
-        self.str(1)
+        trim_tick(self.str(1))
     }
 
     pub fn namespace(&self) -> &'static str {
@@ -395,7 +395,7 @@ impl TypeDef {
 
 impl TypeRef {
     pub fn name(&self) -> &'static str {
-        self.str(1)
+        trim_tick(self.str(1))
     }
 
     pub fn namespace(&self) -> &'static str {
