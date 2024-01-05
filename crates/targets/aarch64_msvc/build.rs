7219668f80a459b47097bc524af304073c69ec4b
Dennis Duda <git@seri.tools>
2024-01-05 01:38:16 +0100

    Generalized build target checks in `windows-targets` sub-crates (#2774)

--- crates/targets/aarch64_msvc/build.rs
+++ crates/targets/aarch64_msvc/build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
-    if target != "aarch64-pc-windows-msvc" && target != "aarch64-uwp-windows-msvc" {
+    let family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
+    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
+    if family != "windows" || arch != "aarch64" || env != "msvc" {
         return;
     }
 
--- crates/targets/x86_64_gnullvm/build.rs
+++ crates/targets/x86_64_gnullvm/build.rs
@@ -1,6 +1,9 @@
 fn main() {
+    let family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
+    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
     let target = std::env::var("TARGET").unwrap();
-    if target != "x86_64-pc-windows-gnullvm" {
+    if family != "windows" || arch != "x86_64" || env != "gnu" || !target.ends_with("-gnullvm") {
         return;
     }
 
