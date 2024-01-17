6c9faed093aa0411f3226fb4f6910874435db9e3
Kenny Kerr <kenny@kennykerr.ca>
2024-01-17 10:11:24 -0600

    windows-sys has no docs

--- crates/libs/sys/src/lib.rs
+++ crates/libs/sys/src/lib.rs
@@ -4,7 +4,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 
 #![no_std]
 #![doc(html_no_source)]
-#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, clippy::all)]
+#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, missing_docs, clippy::all)]
 #![cfg_attr(not(feature = "docs"), doc(hidden))]
 
 #[allow(unused_extern_crates)]
