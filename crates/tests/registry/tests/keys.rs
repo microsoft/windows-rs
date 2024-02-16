b2d5b19c9fdb93465fae75950eae9cda397837a3
Kenny Kerr <kenny@kennykerr.ca>
2024-02-16 08:43:11 -0600

    test open is read-only

--- crates/tests/registry/tests/keys.rs
+++ crates/tests/registry/tests/keys.rs
@@ -18,5 +18,10 @@ fn keys() -> Result<()> {
     assert_eq!(err.code(), HRESULT(0x80070002u32 as i32)); // HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND)
     assert_eq!(err.message(), "The system cannot find the file specified.");
 
+    let key = key.open("one")?;
+    let err = key.set_u32("name", 123).unwrap_err();
+    assert_eq!(err.code(), HRESULT(0x80070005u32 as i32)); // E_ACCESSDENIED
+    assert_eq!(err.message(), "Access is denied.");
+
     Ok(())
 }
