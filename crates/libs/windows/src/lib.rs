1975d2c5940495b9df52bf8f669b35cc03f7f307
Kenny Kerr <kenny@kennykerr.ca>
2024-02-21 10:03:15 -0600

    links

--- crates/libs/windows/src/lib.rs
+++ crates/libs/windows/src/lib.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(docsrs, doc = "")]
 /*!
 Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs>
+
+[Feature search](https://microsoft.github.io/windows-rs/features/0.52.0)
 */
 
 #![doc(html_no_source)]
