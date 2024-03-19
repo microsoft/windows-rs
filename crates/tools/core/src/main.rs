3bb748647a31f4af2de88cde526f64c91fee5502
Kenny Kerr <kenny@kennykerr.ca>
2024-03-19 13:46:36 -0500

    windows-core bindings

--- crates/tools/core/src/main.rs
+++
@@ -1,7 +0,0 @@
-use windows_bindgen::*;
-
-fn main() -> Result<()> {
-    bindgen(["--etc", "crates/tools/core/bindings.txt"])?;
-    bindgen(["--etc", "crates/tools/core/com_bindings.txt"])?;
-    Ok(())
-}
