5a9292852c3351957e0c6af2332e70054f4db314
Kenny Kerr <kenny@kennykerr.ca>
2024-04-02 11:51:22 -0500

    Add missing tests for readme code snippets (#2963)

--- crates/tests/readme/src/lib.rs
+++ crates/tests/readme/src/lib.rs
@@ -1,5 +1,8 @@
 #![doc = include_str!("../../../../crates/libs/core/readme.md")]
 #![doc = include_str!("../../../../crates/libs/metadata/readme.md")]
+#![doc = include_str!("../../../../crates/libs/registry/readme.md")]
+#![doc = include_str!("../../../../crates/libs/result/readme.md")]
 #![doc = include_str!("../../../../crates/libs/sys/readme.md")]
 #![doc = include_str!("../../../../crates/libs/targets/readme.md")]
+#![doc = include_str!("../../../../crates/libs/version/readme.md")]
 #![doc = include_str!("../../../../crates/libs/windows/readme.md")]
