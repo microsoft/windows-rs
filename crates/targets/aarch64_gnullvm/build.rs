80112ddacba770ea708e9ed322567d3c35417154
Kenny Kerr <kenny@kennykerr.ca>
2024-04-04 13:54:56 -0500

    remove redundant build ellission

--- crates/targets/aarch64_gnullvm/build.rs
+++ crates/targets/aarch64_gnullvm/build.rs
@@ -1,12 +1,4 @@
 fn main() {
-    let family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
-    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-    let target = std::env::var("TARGET").unwrap();
-    if family != "windows" || arch != "aarch64" || env != "gnu" || !target.ends_with("-gnullvm") {
-        return;
-    }
-
     let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
     println!("cargo:rustc-link-search=native={}", std::path::Path::new(&dir).join("lib").display());
