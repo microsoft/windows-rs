0f9d8f89470502242cb3e4341cd51722861f591a
jeremyd2019 <github@jdrake.com>
2024-04-04 10:03:18 -0700

    Exclude -gnullvm target from i686_gnu build.rs
    This was missed in #2774, even though the similar check was added to x86_64_gnu, presumably because there was no handling for i686-pc-windows-gnullvm at that time.

--- crates/targets/i686_gnu/build.rs
+++ crates/targets/i686_gnu/build.rs
@@ -2,7 +2,8 @@ fn main() {
     let family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
     let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-    if family != "windows" || arch != "x86" || env != "gnu" {
+    let target = std::env::var("TARGET").unwrap();
+    if family != "windows" || arch != "x86" || env != "gnu" || !target.ends_with("-gnu") {
         return;
     }
 
