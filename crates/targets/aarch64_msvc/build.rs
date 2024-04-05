879c0cf56d97e696ee02a99f71552648bef843e1
Kenny Kerr <kenny@kennykerr.ca>
2024-04-04 19:50:36 -0500

    Remove redundant target checks (#2973)

--- crates/targets/aarch64_msvc/build.rs
+++ crates/targets/aarch64_msvc/build.rs
@@ -1,11 +1,4 @@
 fn main() {
-    let family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
-    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-    if family != "windows" || arch != "aarch64" || env != "msvc" {
-        return;
-    }
-
     let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
     println!("cargo:rustc-link-search=native={}", std::path::Path::new(&dir).join("lib").display());
