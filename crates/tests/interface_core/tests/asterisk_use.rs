bedd359eac9a58b39faa06d7da1a4f48c0ca7303
Kenny Kerr <kenny@kennykerr.ca>
2024-04-08 13:07:35 -0500

    Add explicit path support to the `interface` macro (#2976)

--- crates/tests/interface_core/tests/asterisk_use.rs
+++ crates/tests/interface_core/tests/asterisk_use.rs
@@ -1,5 +1,8 @@
 #![allow(non_snake_case)]
 
+// This tests uses `windows_core` via an asterisk to test that the interface/implement macros support this mode
+// as opposed to no_use.rs which tests the opposite.
+
 use windows_core::*;
 
 #[interface("72cd87fa-9c99-42e0-8986-84a76f08fc5a")]
---
+++ crates/tests/interface_core/tests/no_use.rs
@@ -0,0 +1,26 @@
+#![allow(non_snake_case)]
+
+// This tests uses `windows_core` explicitly to test that the interface/implement macros support this mode
+// as opposed to asterisk_use.rs which tests the opposite.
+
+#[windows_core::interface("72cd87fa-9c99-42e0-8986-84a76f08fc5a")]
+unsafe trait ITest: windows_core::IUnknown {
+    unsafe fn Test(&self) -> windows_core::HRESULT;
+}
+
+#[windows_core::implement(ITest)]
+struct Test;
+
+impl ITest_Impl for Test {
+    unsafe fn Test(&self) -> windows_core::HRESULT {
+        windows_core::HRESULT(123)
+    }
+}
+
+#[test]
+fn test() {
+    unsafe {
+        let test: ITest = Test.into();
+        assert_eq!(test.Test(), windows_core::HRESULT(123));
+    }
+}
