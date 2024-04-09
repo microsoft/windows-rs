4049d4b10e06ad4c7d68db186f96fd5d98bedf66
Kenny Kerr <kenny@kennykerr.ca>
2024-04-09 09:54:28 -0500

    unroll

--- crates/tools/yml/src/main.rs
+++ crates/tools/yml/src/main.rs
@@ -54,25 +54,31 @@ jobs:
       - name: Update toolchain
         run: rustup update --no-self-update ${{ matrix.version }} && rustup default ${{ matrix.version }}
       - name: Fix environment
-        uses: ./.github/actions/fix-environment
-      - name: Test
-        run: >"
+        uses: ./.github/actions/fix-environment"
         .to_string();
 
-    let crates = lib::crates("crates");
-    let (first, last) = crates.split_at(crates.len() / 2);
+    // This unrolling is required since "cargo test --all" consumes too much memory for the GitHub hosted runners
+    // and the occasional "cargo clean" is required to avoid running out of disk space in the same runners.
 
-    for (name, _) in first {
-        write!(&mut yml, "\n          cargo test ${{{{ matrix.no-run }}}} --target ${{{{ matrix.target }}}} -p {name} &&").unwrap();
+    for (count, (name, _)) in lib::crates("crates").iter().enumerate() {
+        if count % 50 == 0 {
+            write!(
+                &mut yml,
+                r"
+      - name: Clean
+        run:  cargo clean"
+            )
+            .unwrap();
+        }
+
+        write!(
+            &mut yml,
+            r"
+      - name: Test
+        run:  cargo test ${{{{ matrix.no-run }}}} --target ${{{{ matrix.target }}}} -p {name}"
+        )
+        .unwrap();
     }
-
-    write!(&mut yml, "\n          cargo clean &&").unwrap();
-
-    for (name, _) in last {
-        write!(&mut yml, "\n          cargo test ${{{{ matrix.no-run }}}} --target ${{{{ matrix.target }}}} -p {name} &&").unwrap();
-    }
-
-    yml.truncate(yml.len() - 3);
 
     write!(
         &mut yml,
