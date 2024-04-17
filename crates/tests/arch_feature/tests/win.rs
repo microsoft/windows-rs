85a77ef285a7771914d520cf5194c3ea2974f5fa
Kenny Kerr <kenny@kennykerr.ca>
2024-04-17 12:44:43 -0500

    Workflow improvements 3 (#3000)

--- crates/tests/arch_feature/tests/win.rs
+++ crates/tests/arch_feature/tests/win.rs
@@ -1,11 +1,11 @@
-use windows::{
-    Win32::System::Diagnostics::Debug::CONTEXT,
-    Win32::System::Environment::VBS_BASIC_ENCLAVE_BASIC_CALL_CREATE_THREAD,
-};
-
 #[test]
 #[cfg(target_arch = "x86_64")]
 fn test() {
+    use windows::{
+        Win32::System::Diagnostics::Debug::CONTEXT,
+        Win32::System::Environment::VBS_BASIC_ENCLAVE_BASIC_CALL_CREATE_THREAD,
+    };
+
     assert_eq!(1232, core::mem::size_of::<CONTEXT>());
 
     use windows::Win32::System::Environment::VBS_BASIC_ENCLAVE_THREAD_DESCRIPTOR64;
@@ -21,6 +21,11 @@ fn test() {
 #[test]
 #[cfg(target_arch = "x86")]
 fn test() {
+    use windows::{
+        Win32::System::Diagnostics::Debug::CONTEXT,
+        Win32::System::Environment::VBS_BASIC_ENCLAVE_BASIC_CALL_CREATE_THREAD,
+    };
+
     assert_eq!(716, core::mem::size_of::<CONTEXT>());
 
     use windows::Win32::System::Environment::VBS_BASIC_ENCLAVE_THREAD_DESCRIPTOR32;
