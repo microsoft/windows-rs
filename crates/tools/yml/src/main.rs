ae56a77c6eb782c43b36ad5c81f08549112e62d5
Kenny Kerr <kenny@kennykerr.ca>
2024-04-17 16:02:59 -0500

    Workflow improvements 4 (#3001)

--- crates/tools/yml/src/main.rs
+++ crates/tools/yml/src/main.rs
@@ -3,7 +3,6 @@ use std::fmt::Write;
 fn main() {
     test_yml();
     clippy_yml();
-    arm64_yml();
 }
 
 fn test_yml() {
@@ -28,19 +27,35 @@ jobs:
       matrix:
         include:
           - version: stable
+            host: x86_64-pc-windows-msvc
             target: x86_64-pc-windows-msvc
+            etc:
           - version: nightly
+            host: x86_64-pc-windows-msvc
             target: i686-pc-windows-msvc
+            etc:
           - version: nightly
+            host: x86_64-pc-windows-gnu
             target: x86_64-pc-windows-gnu
+            etc:
           - version: stable
+            host: x86_64-pc-windows-gnu
             target: i686-pc-windows-gnu
+            etc:
+          - version: stable
+            host: x86_64-pc-windows-msvc
+            target: aarch64-pc-windows-msvc
+            etc: --no-run
+          - version: nightly
+            host: x86_64-pc-windows-msvc
+            target: aarch64-pc-windows-msvc
+            etc: --no-run
 
     steps:
       - name: Checkout
         uses: actions/checkout@v4
       - name: Update toolchain
-        run: rustup update --no-self-update ${{ matrix.version }} && rustup default ${{ matrix.version }}-${{ matrix.target }}
+        run: rustup update --no-self-update ${{ matrix.version }} && rustup default ${{ matrix.version }}-${{ matrix.host }}
       - name: Add toolchain target
         run: rustup target add ${{ matrix.target }}
       - name: Install fmt
@@ -67,7 +82,7 @@ jobs:
             &mut yml,
             r"
       - name: Test {name}
-        run:  cargo test -p {name}"
+        run:  cargo test -p {name} --target ${{{{ matrix.target }}}} ${{{{ matrix.etc }}}}"
         )
         .unwrap();
     }
@@ -137,66 +152,4 @@ jobs:
     }
 
     std::fs::write(".github/workflows/clippy.yml", yml.as_bytes()).unwrap();
-}
-
-// Ideally this would just be another matrix dimension in test_yml but Cargo limitations are blocking this for the time being.
-// See https://github.com/rust-lang/cargo/issues/9661
-fn arm64_yml() {
-    let mut yml = r"name: arm64
-
-on:
-  pull_request:
-  push:
-    paths-ignore:
-      - '.github/ISSUE_TEMPLATE/**'
-    branches:
-      - master
-
-env:
-  RUSTFLAGS: -Dwarnings
-
-jobs:
-  check:
-    runs-on: windows-2019
-
-    strategy:
-      matrix:
-        include:
-          - version: stable
-            target: aarch64-pc-windows-msvc
-          - version: nightly
-            target: aarch64-pc-windows-msvc
-
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v4
-      - name: Update toolchain
-        run: rustup update --no-self-update ${{ matrix.version }} && rustup default ${{ matrix.version }}-x86_64-pc-windows-msvc
-      - name: Add toolchain target
-        run: rustup target add ${{ matrix.target }}
-      - name: Fix environment
-        uses: ./.github/actions/fix-environment"
-      .to_string();
-
-    for (count, (name, _)) in lib::crates("crates").iter().enumerate() {
-        if count % 50 == 0 {
-            write!(
-                &mut yml,
-                r"
-      - name: Clean
-        run:  cargo clean"
-            )
-            .unwrap();
-        }
-
-        write!(
-            &mut yml,
-            r"
-      - name: Test {name}
-        run:  cargo test -p {name} --no-run --target ${{{{ matrix.target }}}}"
-        )
-        .unwrap();
-    }
-
-    std::fs::write(".github/workflows/arm64.yml", yml.as_bytes()).unwrap();
 }
