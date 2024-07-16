03a3c01ae725f24312b3fcb1fccec6a4c4dcec4d
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-07-16 13:39:20 +0800

    From trait for u128

--- crates/libs/sys/src/core/mod.rs
+++ crates/libs/sys/src/core/mod.rs
@@ -30,4 +30,20 @@ impl GUID {
     pub const fn from_u128(uuid: u128) -> Self {
         Self { data1: (uuid >> 96) as u32, data2: (uuid >> 80 & 0xffff) as u16, data3: (uuid >> 64 & 0xffff) as u16, data4: (uuid as u64).to_be_bytes() }
     }
+
+    pub const fn to_u128(&self) -> u128 {
+        (self.data1 as u128) << 96 | (self.data2 as u128) << 80 | (self.data3 as u128) << 64 | u64::from_be_bytes(self.data4) as u128
+    }
+}
+
+impl From<u128> for GUID {
+    fn from(uuid: u128) -> Self {
+        GUID::from_u128(uuid)
+    }
+}
+
+impl From<GUID> for u128 {
+    fn from(guid: GUID) -> Self {
+        guid.to_u128()
+    }
 }
