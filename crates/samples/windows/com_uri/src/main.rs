1f5b4244f8f32e458cc51f2389b53bba992dd0e7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2024-07-16 16:54:27 -0700

    Update main.rs with CoInitializeEx

--- crates/samples/windows/com_uri/src/main.rs
+++ crates/samples/windows/com_uri/src/main.rs
@@ -3,6 +3,8 @@ use windows::Win32::System::Com::*;
 
 fn main() -> windows::core::Result<()> {
     unsafe {
+        CoInitializeEx(None, COINIT_MULTITHREADED).ok()?;
+        
         let uri = CreateUri(w!("http://kennykerr.ca"), URI_CREATE_FLAGS::default(), 0)?;
 
         let domain = uri.GetDomain()?;
