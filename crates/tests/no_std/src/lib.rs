bc6395fe6e1b5e392f05404412a94716a2c10ae8
Kenny Kerr <kenny@kennykerr.ca>
2024-07-29 14:16:09 -0500

    test_no_std

--- crates/tests/no_std/src/lib.rs
+++ crates/tests/no_std/src/lib.rs
@@ -1,21 +1,21 @@
 //! Test for `#![no_std]` crates.
 //!
 //! Compiling this crate verifies that the Windows crates can be compiled with their "std"
-//! feature disabled.
+//! feature disabled. The tricky part is that `cargo test` depends on `std` so testing
+//! `no_std` requires using `cargo check` instead. That's what the `no_std.yml` is for.
 
 #![no_std]
 
-#[cfg(test)]
-mod tests {
-    use windows::core::{implement, ComObject};
+#[windows::core::implement]
+struct App;
 
-    #[implement]
-    struct App {}
-
-    // Compilation is sufficient to test.
-    #[test]
-    fn basic() {
-        let object = ComObject::new(App {});
-        drop(object);
-    }
+#[cfg(not(test))]
+fn _test() {
+    let _ = windows::core::HRESULT(0);
+    let _ = windows::core::ComObject::new(App);
 }
+
+// #[cfg_attr(not(test), panic_handler)]
+// fn _panic(_: &core::panic::PanicInfo<'_>) -> ! {
+//     loop {}
+// }
