35da686f045330c752637e08d0377da21b177082
Kenny Kerr <kenny@kennykerr.ca>
2024-08-16 09:11:26 -0500

    revert cfg doc

--- crates/libs/sys/src/lib.rs
+++ crates/libs/sys/src/lib.rs
@@ -1,13 +1,9 @@
-#![cfg_attr(
-    all(
-        feature = "Win32_Security",
-        feature = "Win32_System_Threading",
-        feature = "Win32_UI_WindowsAndMessaging",
-    ),
-    doc = include_str!("../readme.md")
-)]
-// fallback if not all features are enabled
-#![cfg_attr(not(all(feature = "Win32_Security", feature = "Win32_System_Threading", feature = "Win32_UI_WindowsAndMessaging",)), doc = "Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs>\n\n[Feature search](https://microsoft.github.io/windows-rs/features/#/0.58.0)")]
+/*!
+Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs>
+
+[Feature search](https://microsoft.github.io/windows-rs/features/#/0.59.0)
+*/
+
 #![no_std]
 #![doc(html_no_source)]
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, missing_docs, clippy::all)]
