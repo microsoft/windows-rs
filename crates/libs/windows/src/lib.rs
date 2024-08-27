f06da256c75a943deaa1da0543872665aaab7823
Kenny Kerr <kenny@kennykerr.ca>
2024-08-27 13:35:31 -0500

    revert all

--- crates/libs/windows/src/lib.rs
+++ crates/libs/windows/src/lib.rs
@@ -8,7 +8,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 
 #![cfg(windows)]
 #![doc(html_no_source)]
-#![allow(non_snake_case, clashing_extern_declarations, non_upper_case_globals, non_camel_case_types, missing_docs, clippy::missing_transmute_annotations)]
+#![allow(non_snake_case, clashing_extern_declarations, non_upper_case_globals, non_camel_case_types, missing_docs, clippy::all)]
 #![cfg_attr(not(feature = "docs"), doc(hidden))]
 #![cfg_attr(all(not(feature = "std")), no_std)]
 
