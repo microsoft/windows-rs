ed973c99ba35e6350aadfed5f12ce9a53a3f3f21
Kenny Kerr <kenny@kennykerr.ca>
2024-09-05 14:23:34 -0500

    simpler

--- crates/tests/winrt/reference_float/src/bindings.rs
+++ crates/tests/winrt/reference_float/src/bindings.rs
@@ -8,33 +8,15 @@
 #[repr(C)]
 #[derive(Clone, Debug, PartialEq)]
 pub struct RefWithFloat {
-    pub ReferenceFloat: Option<windows::Foundation::IReference<f32>>,
-    pub ReferenceWithFloat: Option<windows::Foundation::IReference<WithFloat>>,
+    pub Value: Option<windows::Foundation::IReference<f32>>,
 }
 impl windows_core::TypeKind for RefWithFloat {
     type TypeKind = windows_core::CloneType;
 }
 impl windows_core::RuntimeType for RefWithFloat {
-    const SIGNATURE :windows_core::imp::ConstBuffer = windows_core::imp::ConstBuffer::from_slice ( b"struct(test_reference_float.RefWithFloat;pinterface({61c17706-2d65-11e0-9ae8-d48564015472};f4);pinterface({61c17706-2d65-11e0-9ae8-d48564015472};struct(test_reference_float.WithFloat;f4)))" ) ;
+    const SIGNATURE :windows_core::imp::ConstBuffer = windows_core::imp::ConstBuffer::from_slice ( b"struct(test_reference_float.RefWithFloat;pinterface({61c17706-2d65-11e0-9ae8-d48564015472};f4))" ) ;
 }
 impl Default for RefWithFloat {
-    fn default() -> Self {
-        unsafe { core::mem::zeroed() }
-    }
-}
-#[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct WithFloat {
-    pub Value: f32,
-}
-impl windows_core::TypeKind for WithFloat {
-    type TypeKind = windows_core::CopyType;
-}
-impl windows_core::RuntimeType for WithFloat {
-    const SIGNATURE: windows_core::imp::ConstBuffer =
-        windows_core::imp::ConstBuffer::from_slice(b"struct(test_reference_float.WithFloat;f4)");
-}
-impl Default for WithFloat {
     fn default() -> Self {
         unsafe { core::mem::zeroed() }
     }
--- crates/tests/winrt/reference_float/src/lib.rs
+++ crates/tests/winrt/reference_float/src/lib.rs
@@ -5,10 +5,7 @@ use windows::{core::*, Foundation::*};
 #[test]
 fn test() -> Result<()> {
     let mut container = RefWithFloat::default();
-    container.ReferenceFloat = Some(PropertyValue::CreateSingle(1.23)?.cast()?);
-
-    // TODO: https://github.com/microsoft/windows-rs/issues/292 is needed to provide a stock implementation
-    // of `IReference` for arbitrary types in order to test `container.ReferenceWithFloat`.
+    container.Value = Some(PropertyValue::CreateSingle(1.23)?.cast()?);
 
     Ok(())
 }
