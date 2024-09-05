43ab9ca369b94fcefa4885ed487e808dd18c9570
Kenny Kerr <kenny@kennykerr.ca>
2024-09-05 15:37:20 -0500

    client

---
+++ crates/tests/winrt/composable_client/build.rs
@@ -0,0 +1,38 @@
+fn main() {
+    if !cfg!(target_env = "msvc") {
+        return;
+    }
+
+    println!("cargo:rerun-if-changed=src/interop.cpp");
+
+    windows_bindgen::bindgen([
+        "--in",
+        "../composable/metadata.winmd",
+        "--out",
+        "src/bindings.rs",
+        "--filter",
+        "test_composable",
+        "--config",
+        "no-bindgen-comment",
+    ])
+    .unwrap();
+
+    let include = std::env::var("OUT_DIR").unwrap();
+
+    cppwinrt::cppwinrt([
+        "-in",
+        "../composable/metadata.winmd",
+        "../../../libs/bindgen/default",
+        "-out",
+        &include,
+    ])
+    .unwrap();
+
+    cc::Build::new()
+        .cpp(true)
+        .std("c++20")
+        .flag("/EHsc")
+        .file("src/interop.cpp")
+        .include(include)
+        .compile("interop");
+}
---
+++ crates/tests/winrt/composable_client/src/bindings.rs
@@ -0,0 +1,297 @@
+#![allow(
+    non_snake_case,
+    non_upper_case_globals,
+    non_camel_case_types,
+    dead_code,
+    clippy::all
+)]
+windows_core::imp::define_interface!(
+    ICompositor,
+    ICompositor_Vtbl,
+    0xac7b49b8_e092_52ad_8456_48696a5a258e
+);
+impl windows_core::RuntimeType for ICompositor {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+#[repr(C)]
+pub struct ICompositor_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+    pub CreateSpriteVisual: unsafe extern "system" fn(
+        *mut core::ffi::c_void,
+        i32,
+        *mut *mut core::ffi::c_void,
+    ) -> windows_core::HRESULT,
+    pub CreateContainerVisual: unsafe extern "system" fn(
+        *mut core::ffi::c_void,
+        i32,
+        *mut *mut core::ffi::c_void,
+    ) -> windows_core::HRESULT,
+}
+windows_core::imp::define_interface!(
+    IContainerVisual,
+    IContainerVisual_Vtbl,
+    0xb8accc46_3ff7_5a24_8247_f5a52e1f5a8d
+);
+impl windows_core::RuntimeType for IContainerVisual {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+#[repr(C)]
+pub struct IContainerVisual_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+    pub Children:
+        unsafe extern "system" fn(*mut core::ffi::c_void, *mut i32) -> windows_core::HRESULT,
+}
+windows_core::imp::define_interface!(
+    IContainerVisualFactory,
+    IContainerVisualFactory_Vtbl,
+    0x558b6180_1a65_5f01_8be2_2cc0b2034c0e
+);
+impl windows_core::RuntimeType for IContainerVisualFactory {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+#[repr(C)]
+pub struct IContainerVisualFactory_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+}
+windows_core::imp::define_interface!(
+    ISpriteVisual,
+    ISpriteVisual_Vtbl,
+    0x25f23ebe_4cd3_5349_b16d_d88c4d852ea1
+);
+impl windows_core::RuntimeType for ISpriteVisual {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+#[repr(C)]
+pub struct ISpriteVisual_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+    pub Brush: unsafe extern "system" fn(*mut core::ffi::c_void, *mut i32) -> windows_core::HRESULT,
+}
+windows_core::imp::define_interface!(
+    IVisual,
+    IVisual_Vtbl,
+    0xce89606a_5b03_5861_af26_9dced3aab7e6
+);
+impl windows_core::RuntimeType for IVisual {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+#[repr(C)]
+pub struct IVisual_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+    pub Compositor: unsafe extern "system" fn(
+        *mut core::ffi::c_void,
+        *mut *mut core::ffi::c_void,
+    ) -> windows_core::HRESULT,
+}
+windows_core::imp::define_interface!(
+    IVisualFactory,
+    IVisualFactory_Vtbl,
+    0x1974545d_259f_553c_8ea0_e505f897df81
+);
+impl windows_core::RuntimeType for IVisualFactory {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+#[repr(C)]
+pub struct IVisualFactory_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+}
+#[repr(transparent)]
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct Compositor(windows_core::IUnknown);
+windows_core::imp::interface_hierarchy!(
+    Compositor,
+    windows_core::IUnknown,
+    windows_core::IInspectable
+);
+impl Compositor {
+    pub fn new() -> windows_core::Result<Self> {
+        Self::IActivationFactory(|f| f.ActivateInstance::<Self>())
+    }
+    fn IActivationFactory<
+        R,
+        F: FnOnce(&windows_core::imp::IGenericFactory) -> windows_core::Result<R>,
+    >(
+        callback: F,
+    ) -> windows_core::Result<R> {
+        static SHARED: windows_core::imp::FactoryCache<
+            Compositor,
+            windows_core::imp::IGenericFactory,
+        > = windows_core::imp::FactoryCache::new();
+        SHARED.call(callback)
+    }
+    pub fn CreateSpriteVisual(&self, brush: i32) -> windows_core::Result<SpriteVisual> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).CreateSpriteVisual)(
+                windows_core::Interface::as_raw(this),
+                brush,
+                &mut result__,
+            )
+            .and_then(|| windows_core::Type::from_abi(result__))
+        }
+    }
+    pub fn CreateContainerVisual(&self, children: i32) -> windows_core::Result<ContainerVisual> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).CreateContainerVisual)(
+                windows_core::Interface::as_raw(this),
+                children,
+                &mut result__,
+            )
+            .and_then(|| windows_core::Type::from_abi(result__))
+        }
+    }
+}
+impl windows_core::RuntimeType for Compositor {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_class::<Self, ICompositor>();
+}
+unsafe impl windows_core::Interface for Compositor {
+    type Vtable = ICompositor_Vtbl;
+    const IID: windows_core::GUID = <ICompositor as windows_core::Interface>::IID;
+}
+impl windows_core::RuntimeName for Compositor {
+    const NAME: &'static str = "test_composable.Compositor";
+}
+unsafe impl Send for Compositor {}
+unsafe impl Sync for Compositor {}
+#[repr(transparent)]
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct ContainerVisual(windows_core::IUnknown);
+windows_core::imp::interface_hierarchy!(
+    ContainerVisual,
+    windows_core::IUnknown,
+    windows_core::IInspectable
+);
+windows_core::imp::required_hierarchy!(ContainerVisual, Visual);
+impl ContainerVisual {
+    pub fn Children(&self) -> windows_core::Result<i32> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Children)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .map(|| result__)
+        }
+    }
+    pub fn Compositor(&self) -> windows_core::Result<Compositor> {
+        let this = &windows_core::Interface::cast::<IVisual>(self)?;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Compositor)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .and_then(|| windows_core::Type::from_abi(result__))
+        }
+    }
+}
+impl windows_core::RuntimeType for ContainerVisual {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_class::<Self, IContainerVisual>();
+}
+unsafe impl windows_core::Interface for ContainerVisual {
+    type Vtable = IContainerVisual_Vtbl;
+    const IID: windows_core::GUID = <IContainerVisual as windows_core::Interface>::IID;
+}
+impl windows_core::RuntimeName for ContainerVisual {
+    const NAME: &'static str = "test_composable.ContainerVisual";
+}
+unsafe impl Send for ContainerVisual {}
+unsafe impl Sync for ContainerVisual {}
+#[repr(transparent)]
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct SpriteVisual(windows_core::IUnknown);
+windows_core::imp::interface_hierarchy!(
+    SpriteVisual,
+    windows_core::IUnknown,
+    windows_core::IInspectable
+);
+windows_core::imp::required_hierarchy!(SpriteVisual, ContainerVisual, Visual);
+impl SpriteVisual {
+    pub fn Children(&self) -> windows_core::Result<i32> {
+        let this = &windows_core::Interface::cast::<IContainerVisual>(self)?;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Children)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .map(|| result__)
+        }
+    }
+    pub fn Brush(&self) -> windows_core::Result<i32> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Brush)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .map(|| result__)
+        }
+    }
+    pub fn Compositor(&self) -> windows_core::Result<Compositor> {
+        let this = &windows_core::Interface::cast::<IVisual>(self)?;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Compositor)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .and_then(|| windows_core::Type::from_abi(result__))
+        }
+    }
+}
+impl windows_core::RuntimeType for SpriteVisual {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_class::<Self, ISpriteVisual>();
+}
+unsafe impl windows_core::Interface for SpriteVisual {
+    type Vtable = ISpriteVisual_Vtbl;
+    const IID: windows_core::GUID = <ISpriteVisual as windows_core::Interface>::IID;
+}
+impl windows_core::RuntimeName for SpriteVisual {
+    const NAME: &'static str = "test_composable.SpriteVisual";
+}
+unsafe impl Send for SpriteVisual {}
+unsafe impl Sync for SpriteVisual {}
+#[repr(transparent)]
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct Visual(windows_core::IUnknown);
+windows_core::imp::interface_hierarchy!(Visual, windows_core::IUnknown, windows_core::IInspectable);
+impl Visual {
+    pub fn Compositor(&self) -> windows_core::Result<Compositor> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Compositor)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .and_then(|| windows_core::Type::from_abi(result__))
+        }
+    }
+}
+impl windows_core::RuntimeType for Visual {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_class::<Self, IVisual>();
+}
+unsafe impl windows_core::Interface for Visual {
+    type Vtable = IVisual_Vtbl;
+    const IID: windows_core::GUID = <IVisual as windows_core::Interface>::IID;
+}
+impl windows_core::RuntimeName for Visual {
+    const NAME: &'static str = "test_composable.Visual";
+}
+unsafe impl Send for Visual {}
+unsafe impl Sync for Visual {}
