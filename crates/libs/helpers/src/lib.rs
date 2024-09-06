2405bff8a5d7a9cf0adb34758720b59242441142
Kenny Kerr <kenny@kennykerr.ca>
2024-09-06 11:53:05 -0500

    Group more of the WinRT tests together (#3263)

--- crates/libs/helpers/src/lib.rs
+++ crates/libs/helpers/src/lib.rs
@@ -98,3 +98,21 @@ fn find<P: AsRef<Path>>(path: P, regex: &Regex) -> Vec<(String, String)> {
 
     names
 }
+
+pub fn set_thread_ui_language() {
+    // Enables testing without pulling in a dependency on the `windows` crate.
+    windows_targets::link!("kernel32.dll" "system" fn SetThreadPreferredUILanguages(flags : u32, language : *const u16, _ : *mut u32) -> i32);
+    pub const MUI_LANGUAGE_NAME: u32 = 8u32;
+
+    let language: Vec<_> = "en-US".encode_utf16().chain(std::iter::once(0)).collect();
+    unsafe {
+        assert_eq!(
+            1,
+            SetThreadPreferredUILanguages(
+                MUI_LANGUAGE_NAME,
+                language.as_ptr(),
+                std::ptr::null_mut()
+            )
+        );
+    }
+}
