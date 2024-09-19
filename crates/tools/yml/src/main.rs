7e4ce4803e412856ba283eb29ac9e4f0cc861655
Kenny Kerr <kenny@kennykerr.ca>
2024-09-19 08:41:15 -0500

    simpler

--- crates/tools/yml/src/main.rs
+++ crates/tools/yml/src/main.rs
@@ -1,14 +1,14 @@
 use std::fmt::Write;
 
 fn main() {
-    test_lib_yml();
-    test_raw_yml();
+    test_yml("test", false);
+    test_yml("raw-dylib", true);
     clippy_yml();
     no_default_features_yml();
 }
 
-fn test_lib_yml() {
-    let mut yml = r"name: test
+fn test_yml(name: &str, raw_dylib: bool) {
+    let mut yml = format!(r"name: {name}
 
 on:
   pull_request:
@@ -18,7 +18,16 @@ on:
       - 'web/**'
     branches:
       - master
+");
 
+      if raw_dylib {
+        write!(&mut yml, r"
+env:
+  RUSTFLAGS: --cfg windows_raw_dylib
+").unwrap();
+      }
+
+      write!(&mut yml, r"
 jobs:
   check:
     runs-on: windows-2022
@@ -55,14 +64,14 @@ jobs:
       - name: Checkout
         uses: actions/checkout@v4
       - name: Update toolchain
-        run: rustup update --no-self-update ${{ matrix.version }} && rustup default ${{ matrix.version }}-${{ matrix.host }}
+        run: rustup update --no-self-update ${{{{ matrix.version }}}} && rustup default ${{{{ matrix.version }}}}-${{{{ matrix.host }}}}
       - name: Add toolchain target
-        run: rustup target add ${{ matrix.target }}
+        run: rustup target add ${{{{ matrix.target }}}}
       - name: Install fmt
         run: rustup component add rustfmt
       - name: Fix environment
         uses: ./.github/actions/fix-environment"
-        .to_string();
+    ).unwrap();
 
     // This unrolling is required since "cargo test --all" consumes too much memory for the GitHub hosted runners
     // and the occasional "cargo clean" is required to avoid running out of disk space in the same runners.
@@ -99,106 +108,7 @@ jobs:
     )
     .unwrap();
 
-    std::fs::write(".github/workflows/test.yml", yml.as_bytes()).unwrap();
-}
-
-
-fn test_raw_yml() {
-  let mut yml = r"name: raw-dylib
-
-on:
-pull_request:
-push:
-  paths-ignore:
-    - '.github/ISSUE_TEMPLATE/**'
-    - 'web/**'
-  branches:
-    - master
-
-env:
-  RUSTFLAGS: --cfg windows_raw_dylib
-
-jobs:
-check:
-  runs-on: windows-2022
-
-  strategy:
-    matrix:
-      include:
-        - version: stable
-          host: x86_64-pc-windows-msvc
-          target: x86_64-pc-windows-msvc
-          etc:
-        - version: nightly
-          host: x86_64-pc-windows-msvc
-          target: i686-pc-windows-msvc
-          etc:
-        - version: nightly
-          host: x86_64-pc-windows-gnu
-          target: x86_64-pc-windows-gnu
-          etc:
-        - version: stable
-          host: x86_64-pc-windows-gnu
-          target: i686-pc-windows-gnu
-          etc:
-        - version: stable
-          host: x86_64-pc-windows-msvc
-          target: aarch64-pc-windows-msvc
-          etc: --no-run
-        - version: nightly
-          host: x86_64-pc-windows-msvc
-          target: aarch64-pc-windows-msvc
-          etc: --no-run
-
-  steps:
-    - name: Checkout
-      uses: actions/checkout@v4
-    - name: Update toolchain
-      run: rustup update --no-self-update ${{ matrix.version }} && rustup default ${{ matrix.version }}-${{ matrix.host }}
-    - name: Add toolchain target
-      run: rustup target add ${{ matrix.target }}
-    - name: Install fmt
-      run: rustup component add rustfmt
-    - name: Fix environment
-      uses: ./.github/actions/fix-environment"
-      .to_string();
-
-  // This unrolling is required since "cargo test --all" consumes too much memory for the GitHub hosted runners
-  // and the occasional "cargo clean" is required to avoid running out of disk space in the same runners.
-
-  for (count, (name, _)) in helpers::crates("crates").iter().enumerate() {
-      if count % 50 == 0 {
-          write!(
-              &mut yml,
-              r"
-    - name: Clean
-      run:  cargo clean"
-          )
-          .unwrap();
-      }
-
-      write!(
-          &mut yml,
-          r"
-    - name: Test {name}
-      run:  cargo test -p {name} --target ${{{{ matrix.target }}}} ${{{{ matrix.etc }}}}"
-      )
-      .unwrap();
-  }
-
-  write!(
-      &mut yml,
-      r"
-    - name: Check diff
-      shell: bash
-      run: |
-        git add -N .
-        git diff --exit-code || (echo 'Tests changed code in the repo.'; exit 1)
-"
-  )
-  .unwrap();
-
-  std::fs::write(".github/workflows/raw_dylib.yml", yml.as_bytes()).unwrap();
+    std::fs::write(format!(".github/workflows/{name}.yml"), yml.as_bytes()).unwrap();
 }
 
 fn clippy_yml() {
