172e4b847168a9b3933addcf9c979b5be1cd8b10
Kenny Kerr <kenny@kennykerr.ca>
2024-09-23 10:34:29 -0500

    allow improper_ctypes

--- crates/libs/windows/src/lib.rs
+++ crates/libs/windows/src/lib.rs
@@ -8,7 +8,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 
 #![cfg(windows)]
 #![doc(html_no_source)]
-#![allow(non_snake_case, clashing_extern_declarations, non_upper_case_globals, non_camel_case_types, missing_docs, dead_code, clippy::all)]
+#![allow(non_snake_case, clashing_extern_declarations, non_upper_case_globals, non_camel_case_types, missing_docs, dead_code, improper_ctypes, clippy::all)]
 #![cfg_attr(not(feature = "docs"), doc(hidden))]
 #![cfg_attr(all(not(feature = "std")), no_std)]
 
