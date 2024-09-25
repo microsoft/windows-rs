4cb554675bf28bfc4db2054d7f7694f44185176e
Chris Denton <chris@chrisdenton.dev>
2024-09-25 23:25:03 +0000

    Remove improper_ctypes workaround

--- crates/libs/sys/src/lib.rs
+++ crates/libs/sys/src/lib.rs
@@ -8,8 +8,6 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 #![doc(html_no_source)]
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, missing_docs, clippy::all)]
 #![cfg_attr(not(feature = "docs"), doc(hidden))]
-// TODO: workaround for https://github.com/rust-lang/rust/issues/130757
-#![allow(improper_ctypes)]
 
 #[allow(unused_extern_crates)]
 extern crate self as windows_sys;
