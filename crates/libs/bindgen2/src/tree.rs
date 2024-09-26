3e176de769395e68f5e0e91045282591f99c7fc3
Kenny Kerr <kenny@kennykerr.ca>
2024-09-26 13:21:25 -0500

    tree

--- crates/libs/bindgen2/src/tree.rs
+++ crates/libs/bindgen2/src/tree.rs
@@ -10,16 +10,22 @@ pub struct Tree {
 impl Tree {
     pub fn new(reader: &winmd::Reader, filter: &Filter, include_dependencies: bool) -> Self {
         let mut tree = Tree::with_namespace("");
+        let mut dependencies = HashMap::new();
+        let dependencies = &include_dependencies.then(|| &mut dependencies);
 
         for namespace in reader.keys() {
             if filter.includes_namespace(namespace) {
-                tree.insert_namespace(reader, filter, namespace, 0);
+                tree.insert_namespace(reader, filter, dependencies, namespace, 0);
             }
         }
 
-        if include_dependencies {
-
-        }
+        // if let Some(dependencies) = dependencies {
+        //     for (namespace, names) in dependencies.iter() {
+        //         for name in names {
+        //             tree.insert_dependency(namespace, name);
+        //         }
+        //     }
+        // }
 
         tree
     }
@@ -36,27 +42,48 @@ impl Tree {
         &mut self,
         reader: &winmd::Reader,
         filter: &Filter,
+        dependencies: &Option<&mut HashMap<&'static str, HashSet<&'static str>>>,
         namespace: &'static str,
         pos: usize,
-    ) -> &mut Self {
-        let tree = if let Some(next) = namespace[pos..].find('.') {
+    ) {
+        if let Some(next) = namespace[pos..].find('.') {
             let next = pos + next;
             self.nested
                 .entry(&namespace[pos..next])
                 .or_insert_with(|| Self::with_namespace(&namespace[..next]))
-                .insert_namespace(reader, filter, namespace, next + 1)
+                .insert_namespace(reader, filter, dependencies, namespace, next + 1);
         } else {
-            self.nested
+            let tree = self
+                .nested
                 .entry(&namespace[pos..])
-                .or_insert_with(|| Self::with_namespace(namespace))
-        };
+                .or_insert_with(|| Self::with_namespace(namespace));
 
-        for name in reader[namespace].keys() {
-            if filter.includes_type_name(namespace, name) {
-                tree.items.insert(name);
+            for name in reader[namespace].keys() {
+                if filter.includes_type_name(namespace, name) {
+                    tree.items.insert(name);
+
+                    // if let Some(dependencies) = dependencies {
+                    //     // TODO: ask reader for dependencies of item
+                    // }
+                }
             }
         }
-
-        tree
     }
+
+    // fn insert_dependency(&mut self, namespace: &'static str, pos: usize, name: &'static str) {
+    //     let tree = if let Some(next) = namespace[pos..].find('.') {
+    //         let next = pos + next;
+    //         self.nested
+    //             .entry(&namespace[pos..next])
+    //             .or_insert_with(|| Self::with_namespace(&namespace[..next]))
+    //             .insert_dependency(, namespace, next + 1, name)
+    //     } else {
+    //         self.nested
+    //             .entry(&namespace[pos..])
+    //             .or_insert_with(|| Self::with_namespace(namespace))
+    //     };
+
+    //     tree.items.insert(name);
+    //     tree
+    // }
 }
