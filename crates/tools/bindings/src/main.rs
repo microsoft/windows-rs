7f2d538c2a8b93be44e9d2cfde0271cc346fa319
Kenny Kerr <kenny@kennykerr.ca>
2024-10-15 06:24:15 -0500

    tool_bindings

--- crates/tools/bindings/src/main.rs
+++ crates/tools/bindings/src/main.rs
@@ -1,15 +1,15 @@
 use windows_bindgen2::*;
 
 fn main() {
-    //run("crates/tools/bindings/src/core_com.txt")?;
+    run("crates/tools/bindings/src/core_com.txt");
     run("crates/tools/bindings/src/core.txt");
-    //run("crates/tools/bindings/src/metadata.txt")?;
-    //run("crates/tools/bindings/src/registry.txt")?;
-    //run("crates/tools/bindings/src/result.txt")?;
-    //run("crates/tools/bindings/src/strings.txt")?;
-    //run("crates/tools/bindings/src/sys.txt")?;
-    //run("crates/tools/bindings/src/version.txt")?;
-    //run("crates/tools/bindings/src/windows.txt")?;
+    run("crates/tools/bindings/src/metadata.txt");
+    run("crates/tools/bindings/src/registry.txt");
+    run("crates/tools/bindings/src/result.txt");
+    run("crates/tools/bindings/src/strings.txt");
+    run("crates/tools/bindings/src/sys.txt");
+    run("crates/tools/bindings/src/version.txt");
+    run("crates/tools/bindings/src/windows.txt");
 }
 
 fn run(path: &str) {
