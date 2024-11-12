3216e560d07ff5595d0723c1f171dc51d13a1c08
Kenny Kerr <kenny@kennykerr.ca>
2024-11-12 09:06:24 -0600

    sample

--- crates/samples/components/json_validator_winrt/src/bindings.rs
+++ crates/samples/components/json_validator_winrt/src/bindings.rs
@@ -7,6 +7,7 @@
     dead_code,
     clippy::all
 )]
+
 windows_core::imp::define_interface!(
     IJsonValidator,
     IJsonValidator_Vtbl,
@@ -21,9 +22,44 @@ pub struct IJsonValidator_Vtbl {
     pub base__: windows_core::IInspectable_Vtbl,
     pub Validate: unsafe extern "system" fn(
         *mut core::ffi::c_void,
-        core::mem::MaybeUninit<windows_core::HSTRING>,
-        *mut core::mem::MaybeUninit<windows_core::HSTRING>,
+        *mut core::ffi::c_void,
+        *mut *mut core::ffi::c_void,
     ) -> windows_core::HRESULT,
+}
+impl windows_core::RuntimeName for IJsonValidator {
+    const NAME: &'static str = "Sample.IJsonValidator";
+}
+pub trait IJsonValidator_Impl: Sized + windows_core::IUnknownImpl {
+    fn Validate(
+        &self,
+        value: &windows_core::HSTRING,
+    ) -> windows_core::Result<windows_core::HSTRING>;
+}
+impl IJsonValidator_Vtbl {
+    pub const fn new<Identity: IJsonValidator_Impl, const OFFSET: isize>() -> Self {
+        unsafe extern "system" fn Validate<Identity: IJsonValidator_Impl, const OFFSET: isize>(
+            this: *mut core::ffi::c_void,
+            value: *mut core::ffi::c_void,
+            result__: *mut *mut core::ffi::c_void,
+        ) -> windows_core::HRESULT {
+            let this: &Identity = &*((this as *const *const ()).offset(OFFSET) as *const Identity);
+            match IJsonValidator_Impl::Validate(this, core::mem::transmute(&value)) {
+                Ok(ok__) => {
+                    result__.write(core::mem::transmute_copy(&ok__));
+                    core::mem::forget(ok__);
+                    windows_core::HRESULT(0)
+                }
+                Err(err) => err.into(),
+            }
+        }
+        Self {
+            base__: windows_core::IInspectable_Vtbl::new::<Identity, IJsonValidator, OFFSET>(),
+            Validate: Validate::<Identity, OFFSET>,
+        }
+    }
+    pub fn matches(iid: &windows_core::GUID) -> bool {
+        iid == &<IJsonValidator as windows_core::Interface>::IID
+    }
 }
 windows_core::imp::define_interface!(
     IJsonValidatorFactory,
@@ -39,12 +75,49 @@ pub struct IJsonValidatorFactory_Vtbl {
     pub base__: windows_core::IInspectable_Vtbl,
     pub CreateInstance: unsafe extern "system" fn(
         *mut core::ffi::c_void,
-        core::mem::MaybeUninit<windows_core::HSTRING>,
+        *mut core::ffi::c_void,
         *mut *mut core::ffi::c_void,
     ) -> windows_core::HRESULT,
 }
+impl windows_core::RuntimeName for IJsonValidatorFactory {
+    const NAME: &'static str = "Sample.IJsonValidatorFactory";
+}
+pub trait IJsonValidatorFactory_Impl: Sized + windows_core::IUnknownImpl {
+    fn CreateInstance(&self, schema: &windows_core::HSTRING)
+        -> windows_core::Result<JsonValidator>;
+}
+impl IJsonValidatorFactory_Vtbl {
+    pub const fn new<Identity: IJsonValidatorFactory_Impl, const OFFSET: isize>() -> Self {
+        unsafe extern "system" fn CreateInstance<
+            Identity: IJsonValidatorFactory_Impl,
+            const OFFSET: isize,
+        >(
+            this: *mut core::ffi::c_void,
+            schema: *mut core::ffi::c_void,
+            result__: *mut *mut core::ffi::c_void,
+        ) -> windows_core::HRESULT {
+            let this: &Identity = &*((this as *const *const ()).offset(OFFSET) as *const Identity);
+            match IJsonValidatorFactory_Impl::CreateInstance(this, core::mem::transmute(&schema)) {
+                Ok(ok__) => {
+                    result__.write(core::mem::transmute_copy(&ok__));
+                    core::mem::forget(ok__);
+                    windows_core::HRESULT(0)
+                }
+                Err(err) => err.into(),
+            }
+        }
+        Self {
+            base__: windows_core::IInspectable_Vtbl::new::<Identity, IJsonValidatorFactory, OFFSET>(
+            ),
+            CreateInstance: CreateInstance::<Identity, OFFSET>,
+        }
+    }
+    pub fn matches(iid: &windows_core::GUID) -> bool {
+        iid == &<IJsonValidatorFactory as windows_core::Interface>::IID
+    }
+}
 #[repr(transparent)]
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct JsonValidator(windows_core::IUnknown);
 windows_core::imp::interface_hierarchy!(
     JsonValidator,
@@ -64,7 +137,7 @@ impl JsonValidator {
                 core::mem::transmute_copy(value),
                 &mut result__,
             )
-            .and_then(|| windows_core::Type::from_abi(result__))
+            .map(|| core::mem::transmute(result__))
         }
     }
     pub fn CreateInstance(schema: &windows_core::HSTRING) -> windows_core::Result<JsonValidator> {
@@ -91,7 +164,7 @@ impl windows_core::RuntimeType for JsonValidator {
         windows_core::imp::ConstBuffer::for_class::<Self, IJsonValidator>();
 }
 unsafe impl windows_core::Interface for JsonValidator {
-    type Vtable = IJsonValidator_Vtbl;
+    type Vtable = <IJsonValidator as windows_core::Interface>::Vtable;
     const IID: windows_core::GUID = <IJsonValidator as windows_core::Interface>::IID;
 }
 impl windows_core::RuntimeName for JsonValidator {
@@ -99,76 +172,3 @@ impl windows_core::RuntimeName for JsonValidator {
 }
 unsafe impl Send for JsonValidator {}
 unsafe impl Sync for JsonValidator {}
-pub trait IJsonValidator_Impl: Sized + windows_core::IUnknownImpl {
-    fn Validate(
-        &self,
-        value: &windows_core::HSTRING,
-    ) -> windows_core::Result<windows_core::HSTRING>;
-}
-impl windows_core::RuntimeName for IJsonValidator {
-    const NAME: &'static str = "Sample.IJsonValidator";
-}
-impl IJsonValidator_Vtbl {
-    pub const fn new<Identity: IJsonValidator_Impl, const OFFSET: isize>() -> IJsonValidator_Vtbl {
-        unsafe extern "system" fn Validate<Identity: IJsonValidator_Impl, const OFFSET: isize>(
-            this: *mut core::ffi::c_void,
-            value: core::mem::MaybeUninit<windows_core::HSTRING>,
-            result__: *mut core::mem::MaybeUninit<windows_core::HSTRING>,
-        ) -> windows_core::HRESULT {
-            let this: &Identity = &*((this as *const *const ()).offset(OFFSET) as *const Identity);
-            match IJsonValidator_Impl::Validate(this, core::mem::transmute(&value)) {
-                Ok(ok__) => {
-                    result__.write(core::mem::transmute_copy(&ok__));
-                    core::mem::forget(ok__);
-                    windows_core::HRESULT(0)
-                }
-                Err(err) => err.into(),
-            }
-        }
-        Self {
-            base__: windows_core::IInspectable_Vtbl::new::<Identity, IJsonValidator, OFFSET>(),
-            Validate: Validate::<Identity, OFFSET>,
-        }
-    }
-    pub fn matches(iid: &windows_core::GUID) -> bool {
-        iid == &<IJsonValidator as windows_core::Interface>::IID
-    }
-}
-pub trait IJsonValidatorFactory_Impl: Sized + windows_core::IUnknownImpl {
-    fn CreateInstance(&self, schema: &windows_core::HSTRING)
-        -> windows_core::Result<JsonValidator>;
-}
-impl windows_core::RuntimeName for IJsonValidatorFactory {
-    const NAME: &'static str = "Sample.IJsonValidatorFactory";
-}
-impl IJsonValidatorFactory_Vtbl {
-    pub const fn new<Identity: IJsonValidatorFactory_Impl, const OFFSET: isize>(
-    ) -> IJsonValidatorFactory_Vtbl {
-        unsafe extern "system" fn CreateInstance<
-            Identity: IJsonValidatorFactory_Impl,
-            const OFFSET: isize,
-        >(
-            this: *mut core::ffi::c_void,
-            schema: core::mem::MaybeUninit<windows_core::HSTRING>,
-            result__: *mut *mut core::ffi::c_void,
-        ) -> windows_core::HRESULT {
-            let this: &Identity = &*((this as *const *const ()).offset(OFFSET) as *const Identity);
-            match IJsonValidatorFactory_Impl::CreateInstance(this, core::mem::transmute(&schema)) {
-                Ok(ok__) => {
-                    result__.write(core::mem::transmute_copy(&ok__));
-                    core::mem::forget(ok__);
-                    windows_core::HRESULT(0)
-                }
-                Err(err) => err.into(),
-            }
-        }
-        Self {
-            base__: windows_core::IInspectable_Vtbl::new::<Identity, IJsonValidatorFactory, OFFSET>(
-            ),
-            CreateInstance: CreateInstance::<Identity, OFFSET>,
-        }
-    }
-    pub fn matches(iid: &windows_core::GUID) -> bool {
-        iid == &<IJsonValidatorFactory as windows_core::Interface>::IID
-    }
-}
