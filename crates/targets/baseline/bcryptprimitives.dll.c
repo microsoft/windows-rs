4d54fb96f554af5e8a124195a33061796be5fb63
Rafael Rivera <rafael@withinwindows.com>
2024-11-13 17:20:07 -0800

    Update libs

--- crates/targets/baseline/bcryptprimitives.dll.c
+++ crates/targets/baseline/bcryptprimitives.dll.c
@@ -1,2 +1,9 @@
+void __stdcall GetAsymmetricEncryptionInterface(int p0, int p1, int p2, int p3) {}
+void __stdcall GetCipherInterface(int p0, int p1, int p2, int p3) {}
+void __stdcall GetHashInterface(int p0, int p1, int p2, int p3) {}
+void __stdcall GetKeyDerivationInterface(int p0, int p1, int p2, int p3) {}
+void __stdcall GetRngInterface(int p0, int p1, int p2) {}
+void __stdcall GetSecretAgreementInterface(int p0, int p1, int p2, int p3) {}
+void __stdcall GetSignatureInterface(int p0, int p1, int p2, int p3) {}
 void __stdcall ProcessPrng(int p0, int p1) {}
 void __stdcall SystemPrng(int p0, int p1) {}
