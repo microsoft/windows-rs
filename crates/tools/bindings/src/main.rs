c2ff3bf11f92660d232945a954fc1c6b2534e429
Kenny Kerr <kenny@kennykerr.ca>
2024-11-27 09:40:59 -0600

    tools

--- crates/tools/bindings/src/main.rs
+++ crates/tools/bindings/src/main.rs
@@ -1,20 +1,17 @@
 use windows_bindgen::*;
 
-fn main() -> Result<()> {
-    run("crates/tools/bindings/src/core_com.txt")?;
-    run("crates/tools/bindings/src/core.txt")?;
-    run("crates/tools/bindings/src/metadata.txt")?;
-    run("crates/tools/bindings/src/registry.txt")?;
-    run("crates/tools/bindings/src/result.txt")?;
-    run("crates/tools/bindings/src/strings.txt")?;
-    run("crates/tools/bindings/src/sys.txt")?;
-    run("crates/tools/bindings/src/version.txt")?;
-    run("crates/tools/bindings/src/windows.txt")?;
-    Ok(())
-}
+fn main() {
+    let time = std::time::Instant::now();
 
-fn run(path: &str) -> Result<()> {
-    println!("{path}");
-    bindgen(["--etc", path])?;
-    Ok(())
+    bindgen(["--etc", "crates/tools/bindings/src/core_com.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/core.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/metadata.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/registry.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/result.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/strings.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/version.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/sys.txt"]);
+    bindgen(["--etc", "crates/tools/bindings/src/windows.txt"]);
+
+    println!("Finished in {:.2}s", time.elapsed().as_secs_f32());
 }
