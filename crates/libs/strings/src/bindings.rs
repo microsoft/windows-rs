d1fec91a628d16675a6bf2c6709d7e72590539f9
Kenny Kerr <kenny@kennykerr.ca>
2024-11-30 07:34:39 -0600

    libs

--- crates/libs/strings/src/bindings.rs
+++ crates/libs/strings/src/bindings.rs
@@ -5,14 +5,15 @@
     dead_code,
     clippy::all
 )]
+
 windows_targets::link!("kernel32.dll" "system" fn GetProcessHeap() -> HANDLE);
 windows_targets::link!("kernel32.dll" "system" fn HeapAlloc(hheap : HANDLE, dwflags : HEAP_FLAGS, dwbytes : usize) -> *mut core::ffi::c_void);
 windows_targets::link!("kernel32.dll" "system" fn HeapFree(hheap : HANDLE, dwflags : HEAP_FLAGS, lpmem : *const core::ffi::c_void) -> BOOL);
 windows_targets::link!("oleaut32.dll" "system" fn SysAllocStringLen(strin : PCWSTR, ui : u32) -> BSTR);
 windows_targets::link!("oleaut32.dll" "system" fn SysFreeString(bstrstring : BSTR));
 windows_targets::link!("oleaut32.dll" "system" fn SysStringLen(pbstr : BSTR) -> u32);
-pub type BOOL = i32;
-pub type BSTR = *const u16;
-pub type HANDLE = *mut core::ffi::c_void;
 pub type HEAP_FLAGS = u32;
+pub type BOOL = i32;
+pub type HANDLE = *mut core::ffi::c_void;
 pub type PCWSTR = *const u16;
+pub type BSTR = *const u16;
