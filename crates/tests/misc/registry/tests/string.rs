c9ff9710e201802fc32f8eb39732b8f9f476b616
kerosina <164688001+kerosina@users.noreply.github.com>
2025-01-06 21:19:38 +0000

    Separate tests for calling `set_string`/`set_expand_string` from the normal tests

--- crates/tests/misc/registry/tests/string.rs
+++ crates/tests/misc/registry/tests/string.rs
@@ -8,11 +8,17 @@ fn string() -> Result<()> {
     _ = CURRENT_USER.remove_tree(test_key);
     let key = CURRENT_USER.create(test_key)?;
 
-    key.set_string("string", Cow::Owned("lolz".to_string()))?;
+    key.set_string("string", "value")?;
     assert_eq!(key.get_type("string")?, Type::String);
     assert_eq!(key.get_string("string")?, "value");
     assert_eq!(key.get_hstring("string")?, "value");
     assert_eq!(key.get_multi_string("string")?, ["value".to_string()]);
+
+    key.set_string("string_different_types", Cow::Owned("value".to_string()))?;
+    assert_eq!(key.get_type("string_different_types")?, Type::String);
+    assert_eq!(key.get_string("string_different_types")?, "value");
+    assert_eq!(key.get_hstring("string_different_types")?, "value");
+    assert_eq!(key.get_multi_string("string_different_types")?, ["value".to_string()]);
 
     key.set_hstring("hstring", h!("value"))?;
     assert_eq!(key.get_type("hstring")?, Type::String);
@@ -20,11 +26,17 @@ fn string() -> Result<()> {
     assert_eq!(key.get_hstring("hstring")?, "value");
     assert_eq!(key.get_multi_string("hstring")?, ["value".to_string()]);
 
-    key.set_expand_string("expand_string".to_string(), "value")?;
+    key.set_expand_string("expand_string", "value")?;
     assert_eq!(key.get_type("expand_string")?, Type::ExpandString);
     assert_eq!(key.get_string("expand_string")?, "value");
     assert_eq!(key.get_hstring("expand_string")?, "value");
     assert_eq!(key.get_multi_string("expand_string")?, ["value"]);
+
+    key.set_expand_string("expand_string_different_types".to_string(), "value")?;
+    assert_eq!(key.get_type("expand_string_different_types")?, Type::ExpandString);
+    assert_eq!(key.get_string("expand_string_different_types")?, "value");
+    assert_eq!(key.get_hstring("expand_string_different_types")?, "value");
+    assert_eq!(key.get_multi_string("expand_string_different_types")?, ["value"]);
 
     key.set_expand_hstring("expand_hstring", h!("value"))?;
     assert_eq!(key.get_type("expand_hstring")?, Type::ExpandString);
