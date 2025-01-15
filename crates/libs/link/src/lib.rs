98f2d3772cb87f5c1c31dcf186842e12cd52d5d4
Kenny Kerr <kenny@kennykerr.ca>
2025-01-15 15:17:04 -0600

    link

---
+++ crates/libs/link/src/lib.rs
@@ -0,0 +1,28 @@
+#![doc = include_str!("../readme.md")]
+#![no_std]
+
+/// Defines an external function to import.
+#[cfg(target_arch = "x86")]
+#[macro_export]
+macro_rules! link {
+    ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
+        #[link(name = $library, kind = "raw-dylib", modifiers = "+verbatim", import_name_type = "undecorated")]
+        extern $abi {
+            $(#[link_name=$link_name])?
+            pub fn $($function)*;
+        }
+    )
+}
+
+/// Defines an external function to import.
+#[cfg(not(target_arch = "x86"))]
+#[macro_export]
+macro_rules! link {
+    ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
+        #[link(name = $library, kind = "raw-dylib", modifiers = "+verbatim")]
+        extern "C" {
+            $(#[link_name=$link_name])?
+            pub fn $($function)*;
+        }
+    )
+}
