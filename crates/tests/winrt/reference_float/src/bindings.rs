812861830643eab9d76c50361a7ece50731ea979
Kenny Kerr <kenny@kennykerr.ca>
2025-01-16 17:32:08 -0600

    code gen

--- crates/tests/winrt/reference_float/src/bindings.rs
+++ crates/tests/winrt/reference_float/src/bindings.rs
@@ -172,7 +172,7 @@ impl IPropertyValue {
                 windows_core::Interface::as_raw(this),
                 &mut result__,
             )
-            .map(|| core::mem::transmute(result__))
+            .map(|| result__)
         }
     }
     pub fn GetUInt8Array(&self, value: &mut windows_core::Array<u8>) -> windows_core::Result<()> {
@@ -1279,7 +1279,7 @@ impl<T: windows_core::RuntimeType + 'static> IReference<T> {
                 windows_core::Interface::as_raw(this),
                 &mut result__,
             )
-            .map(|| core::mem::transmute(result__))
+            .map(|| result__)
         }
     }
     pub fn GetUInt8Array(&self, value: &mut windows_core::Array<u8>) -> windows_core::Result<()> {
