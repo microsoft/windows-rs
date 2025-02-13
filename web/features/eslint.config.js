0f0116c648e4d85444b9314e2168b50e9376b307
Rafael Rivera <rafael@withinwindows.com>
2025-02-12 20:59:58 -0800

    update deps

--- web/features/eslint.config.js
+++ web/features/eslint.config.js
@@ -1,48 +1,61 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  overrides: [
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
     {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+        ignores: [
+            '**/webpack.config.js',
+            '**/webpack.*.js',
+            '**/eslint.*.js',
+            '**/react-app-env.d.ts',
+            'dist/**',
+            'node_modules/**',
+        ]
     },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  rules: {
-    indent: [
-      'error',
-      4,
-      {
-        SwitchCase: 1,
-      },
-    ],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    js.configs.recommended,
+    {
+        files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+        },
+        plugins: {
+            '@typescript-eslint': ts,
+            react,
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            ...ts.configs['recommended'].rules,
+            ...react.configs.recommended.rules,
+            'indent': ['error', 4, { 'SwitchCase': 1 }],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'react/jsx-uses-react': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'no-undef': 'off',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        }
     },
-  },
-};
+    {
+        files: ['.eslintrc.{js,cjs}'],
+        languageOptions: {
+            sourceType: 'script',
+        }
+    },
+];
