cd002f56b46bca000d08c3013c003e2e4ed2fef4
robmikh <rob.mikh@outlook.com>
2025-03-04 06:22:46 -0800

    Add `scale` and `skew` functions to `Matrix3x2` (#3527)

--- crates/tests/misc/matrix3x2/tests/matrix3x2.rs
+++ crates/tests/misc/matrix3x2/tests/matrix3x2.rs
@@ -1,6 +1,11 @@
-use windows_numerics::Matrix3x2;
+use windows_numerics::{Matrix3x2, Vector2};
 
 #[test]
-fn test() {
-    _ = Matrix3x2::rotation(0.0, 1.0, 2.0);
+fn rotation_test() {
+    _ = Matrix3x2::rotation_around(0.0, Vector2::new(1.0, 2.0));
+}
+
+#[test]
+fn skew_test() {
+    _ = Matrix3x2::skew_around(2.0, 1.0, Vector2::new(1.0, 2.0));
 }
