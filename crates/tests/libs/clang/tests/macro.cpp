0f0ebb9a97058d7962d01bf7ad0a0b07d0dbd700
Kenny Kerr <kenny@kennykerr.ca>
2025-04-02 08:57:25 -0500

    macro

---
+++ crates/tests/libs/clang/tests/macro.cpp
@@ -0,0 +1,5 @@
+#define HRESULT int
+
+struct Type {
+    HRESULT field;
+};
---
+++ crates/tests/libs/clang/tests/macro.rs
@@ -0,0 +1,27 @@
+#![cfg(all(target_arch = "x86_64", target_env = "msvc"))]
+
+use windows_clang::*;
+
+#[test]
+fn find() {
+    let index = Index::new();
+    let tu = index.tu(c"tests/macro.cpp").unwrap();
+    let cursor = tu.cursor();
+
+    let ty = cursor.find(|next| next.name() == "Type").unwrap();
+    assert_eq!(ty.name(), "Type");
+    assert_eq!(ty.kind(), CursorKind::StructDecl);
+    assert_eq!(ty.ty().kind(), TypeKind::Record);
+
+    assert!(cursor.find(|next| next.name() == "TypeNotFound").is_none());
+
+    let field = ty.ty().find(|next| next.name() == "field").unwrap();
+    assert_eq!(field.name(), "field");
+    assert_eq!(field.kind(), CursorKind::FieldDecl);
+    assert_eq!(field.ty().kind(), TypeKind::Int);
+
+    assert!(ty
+        .ty()
+        .find(|next| next.name() == "FieldNotFound")
+        .is_none());
+}
