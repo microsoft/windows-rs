d73ed7e7a935b115613ddc1802bce27e4e65fcfa
Chris Denton <chris@chrisdenton.dev>
2025-06-12 17:31:28 +0000

    Revert ABI changes to the `link` macros (#3628)

--- crates/libs/targets/src/lib.rs
+++ crates/libs/targets/src/lib.rs
@@ -28,7 +28,7 @@ macro_rules! link {
 }
 
 /// Defines an external function to import.
-#[cfg(all(windows, not(windows_raw_dylib), target_arch = "x86"))]
+#[cfg(all(windows, not(windows_raw_dylib)))]
 #[macro_export]
 macro_rules! link {
     ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
@@ -41,24 +41,11 @@ macro_rules! link {
 }
 
 /// Defines an external function to import.
-#[cfg(all(windows, not(windows_raw_dylib), not(target_arch = "x86")))]
-#[macro_export]
-macro_rules! link {
-    ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-        #[link(name = "windows.0.53.0")]
-        extern "C" {
-            $(#[link_name=$link_name])?
-            pub fn $($function)*;
-        }
-    )
-}
-
-/// Defines an external function to import.
 #[cfg(all(not(windows), not(windows_raw_dylib)))]
 #[macro_export]
 macro_rules! link {
     ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-        extern "C" {
+        extern $abi {
             pub fn $($function)*;
         }
     )
