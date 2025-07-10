40c38c8bae91f2ca5660053130966e56a45fdfcf
Kenny Kerr <kenny@kennykerr.ca>
2025-07-10 14:58:06 -0500

    WriteLine

--- crates/samples/csharp/client/client.cs
+++ crates/samples/csharp/client/client.cs
@@ -3,10 +3,10 @@
 [DllImport("csharp_component.dll", PreserveSig = false)]
 static extern uint Add(uint left, uint right);
 
-Console.WriteLine("Add: {0:D}", Add(4, 5));
+Console.WriteLine("Add: {0}", Add(4, 5));
 
 [DllImport("csharp_component.dll", PreserveSig = false)]
 [return: MarshalAs(UnmanagedType.BStr)]
 static extern string Concat([MarshalAs(UnmanagedType.LPWStr)] string left, [MarshalAs(UnmanagedType.LPWStr)] string right);
 
-Console.WriteLine("Concat: {0:D}", Concat("hello ", "world"));
+Console.WriteLine("Concat: {0}", Concat("hello ", "world"));
