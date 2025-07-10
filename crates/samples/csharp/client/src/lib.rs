4b0df4a7c372e4ba433d2735fba4bc0268474650
Kenny Kerr <kenny@kennykerr.ca>
2025-07-10 10:59:47 -0500

    x86 test

--- crates/samples/csharp/client/src/lib.rs
+++ crates/samples/csharp/client/src/lib.rs
@@ -2,6 +2,10 @@
 fn test() {
     let mut command = std::process::Command::new("dotnet.exe");
     command.arg("run");
+
+    #[cfg(target_arch = "x86")]
+    command.args("-r win-x86 /p:PlatformTarget=x86".split_whitespace());
+
     let output = command.output().expect("failed to run dotnet");
 
     if !output.status.success() {
