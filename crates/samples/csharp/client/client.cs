027e3590c76d108b02aa2f3431f280bbd21a69f0
Kenny Kerr <kenny@kennykerr.ca>
2025-07-11 07:45:14 -0500

    Add error handling and string test for C# interop (#3658)

--- crates/samples/csharp/client/client.cs
+++ crates/samples/csharp/client/client.cs
@@ -3,4 +3,10 @@
 [DllImport("csharp_component.dll", PreserveSig = false)]
 static extern uint Add(uint left, uint right);
 
-Console.WriteLine("result = {0:D}", Add(4, 5));
+Console.WriteLine("Add: {0}", Add(4, 5));
+
+[DllImport("csharp_component.dll", PreserveSig = false)]
+[return: MarshalAs(UnmanagedType.BStr)]
+static extern string Concat([MarshalAs(UnmanagedType.LPWStr)] string left, [MarshalAs(UnmanagedType.LPWStr)] string right);
+
+Console.WriteLine("Concat: {0}", Concat("hello ", "world"));
