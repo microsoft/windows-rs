66f608bba9e40e2e0e74a32d44b537ef60cc5c13
Kenny Kerr <kenny@kennykerr.ca>
2025-07-21 11:18:49 -0500

    forward link macro

--- crates/libs/targets/src/lib.rs
+++ crates/libs/targets/src/lib.rs
@@ -1,31 +1,8 @@
 #![doc = include_str!("../readme.md")]
 #![no_std]
 
-/// Defines an external function to import.
-#[cfg(all(windows_raw_dylib, target_arch = "x86"))]
-#[macro_export]
-macro_rules! link {
-    ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-        #[link(name = $library, kind = "raw-dylib", modifiers = "+verbatim", import_name_type = "undecorated")]
-        extern $abi {
-            $(#[link_name=$link_name])?
-            pub fn $($function)*;
-        }
-    )
-}
-
-/// Defines an external function to import.
-#[cfg(all(windows_raw_dylib, not(target_arch = "x86")))]
-#[macro_export]
-macro_rules! link {
-    ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-        #[link(name = $library, kind = "raw-dylib", modifiers = "+verbatim")]
-        extern "C" {
-            $(#[link_name=$link_name])?
-            pub fn $($function)*;
-        }
-    )
-}
+#[cfg(windows_raw_dylib)]
+pub use windows_link::link;
 
 /// Defines an external function to import.
 #[cfg(all(windows, not(windows_raw_dylib)))]
