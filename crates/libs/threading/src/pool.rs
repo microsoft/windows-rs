017d6d9c00319b8b6211e69ce4916a929920e2d1
Chris Denton <chris@chrisdenton.dev>
2025-08-02 22:13:18 +0000

    windows-threading: Implement scope for Pool

--- crates/libs/threading/src/pool.rs
+++ crates/libs/threading/src/pool.rs
@@ -1,4 +1,5 @@
 use super::*;
+use core::{marker::PhantomData, ops::Deref};
 
 /// A `Pool` object represents a private thread pool with its own thread limits.
 ///
@@ -25,6 +26,15 @@ impl Pool {
         Self(Box::new(e))
     }
 
+    /// Convenience function for creating a new pool and calling [`scope`][Self::scope].
+    pub fn with_scope<'env, F>(f: F)
+    where
+        F: for<'scope> FnOnce(&'scope Scope<'scope, 'env>),
+    {
+        let pool = Pool::new();
+        pool.scope(f);
+    }
+
     /// Sets the thread limits for the `Pool` object.
     pub fn set_thread_limits(&self, min: u32, max: u32) {
         unsafe {
@@ -33,14 +43,40 @@ impl Pool {
         }
     }
 
-    /// Submits the closure to run on the `Pool`.
+    /// Submit the closure to the thread pool.
     ///
-    /// The closure cannot outlive the `Pool` on which it runs.
-    pub fn submit<'a, F: FnOnce() + Send + 'a>(&'a self, f: F) {
-        // This is safe because the lifetime of the closure is bounded by the `Pool`.
+    /// * The closure must have `'static` lifetime as the thread may outlive the lifetime in which `submit` is called.
+    /// * The closure must be `Send` as it will be sent to another thread for execution.
+    pub fn submit<F: FnOnce() + Send + 'static>(&self, f: F) {
+        // This is safe because the closure has a `'static` lifetime.
         unsafe {
             try_submit(&*self.0, f);
         }
+    }
+
+    /// Create a scope for submitting closures.
+    ///
+    /// Within this scope local variables can be sent to the pool thread for execution.
+    /// This is possible because `scope` will wait for all submitted closures to finish before returning,
+    /// Note however that it will also wait for closures that were submitted from other threads.
+    pub fn scope<'env, F>(&self, f: F)
+    where
+        F: for<'scope> FnOnce(&'scope Scope<'scope, 'env>),
+    {
+        struct DropGuard<'a>(&'a Pool);
+        impl Drop for DropGuard<'_> {
+            fn drop(&mut self) {
+                self.0.join();
+            }
+        }
+        // Ensure that we always join the pool before returning.
+        let _guard = DropGuard(self);
+        let scope = Scope {
+            pool: self,
+            env: PhantomData,
+            scope: PhantomData,
+        };
+        f(&scope);
     }
 
     /// Waits for all submissions to finish.
@@ -72,5 +108,32 @@ impl Drop for Pool {
             CloseThreadpoolCleanupGroup(self.0.CleanupGroup);
             CloseThreadpool(self.0.Pool);
         }
+    }
+}
+
+/// A scope to submit closures in.
+///
+/// See [`scope`][Pool::scope] for details.
+pub struct Scope<'scope, 'env: 'scope> {
+    pool: &'scope Pool,
+    scope: PhantomData<&'scope mut &'scope ()>,
+    env: PhantomData<&'env mut &'env ()>,
+}
+
+impl<'scope, 'env> Scope<'scope, 'env> {
+    /// Submits the closure to run on the `Pool`.
+    ///
+    /// The closure cannot outlive the `Scope` it's run in.
+    pub fn submit<F: FnOnce() + Send + 'scope>(&'scope self, f: F) {
+        unsafe {
+            try_submit(&*self.pool.0, f);
+        }
+    }
+}
+
+impl Deref for Scope<'_, '_> {
+    type Target = Pool;
+    fn deref(&self) -> &Self::Target {
+        self.pool
     }
 }
