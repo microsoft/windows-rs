c90ee157ec722fac30b0ac7edb331c6e73e2fc60
Kenny Kerr <kenny@kennykerr.ca>
2025-08-08 17:03:48 -0500

    test

--- crates/tests/libs/future/tests/when.rs
+++ crates/tests/libs/future/tests/when.rs
@@ -1,1 +1,66 @@
-// TODO: todo: need specific tests for "when"
+use windows_future::*;
+use windows_result::*;
+use windows_threading::*;
+
+#[test]
+fn ok() -> Result<()> {
+    let thread = thread_id();
+
+    let a = IAsyncAction::ready(Ok(()));
+
+    a.when(move |r| {
+        assert!(r.is_ok());
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncAction::spawn(|| Ok(()));
+
+    a.when(move |r| {
+        assert!(r.is_ok());
+        assert_ne!(thread, thread_id());
+    })?;
+
+    let a = IAsyncActionWithProgress::<i32>::ready(Ok(()));
+
+    a.when(move |r| {
+        assert!(r.is_ok());
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncActionWithProgress::<i32>::spawn(|| Ok(()));
+
+    a.when(move |r| {
+        assert!(r.is_ok());
+        assert_ne!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperation::<i32>::ready(Ok(123));
+
+    a.when(move |r| {
+        assert_eq!(r, Ok(123));
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperation::<i32>::spawn(|| Ok(456));
+
+    a.when(move |r| {
+        assert_eq!(r, Ok(456));
+        assert_ne!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperationWithProgress::<i32, i32>::ready(Ok(123));
+
+    a.when(move |r| {
+        assert_eq!(r, Ok(123));
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperationWithProgress::<i32, i32>::spawn(|| Ok(456));
+
+    a.when(move |r| {
+        assert_eq!(r, Ok(456));
+        assert_ne!(thread, thread_id());
+    })?;
+
+    Ok(())
+}
