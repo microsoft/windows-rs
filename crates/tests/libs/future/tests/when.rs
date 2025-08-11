6ddd2e0d35821c9515db4722c560b260cb1c28d7
Kenny Kerr <kenny@kennykerr.ca>
2025-08-11 09:00:33 -0500

    test_err

--- crates/tests/libs/future/tests/when.rs
+++ crates/tests/libs/future/tests/when.rs
@@ -1,3 +1,4 @@
+use windows::Win32::Foundation::E_FAIL;
 use windows_future::*;
 use windows_result::*;
 use windows_threading::*;
@@ -59,6 +60,95 @@ fn ok() -> Result<()> {
 
     a.when(move |r| {
         assert_eq!(r, Ok(456));
+        assert_ne!(thread, thread_id());
+    })?;
+
+    Ok(())
+}
+
+#[test]
+fn err() -> Result<()> {
+    let thread = thread_id();
+
+    let a = IAsyncAction::ready(Err(Error::new(E_FAIL, "IAsyncAction-ready")));
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncAction-ready");
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncAction::spawn(|| Err(Error::new(E_FAIL, "IAsyncAction-spawn")));
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncAction-spawn");
+        assert_ne!(thread, thread_id());
+    })?;
+
+    let a = IAsyncActionWithProgress::<i32>::ready(Err(Error::new(
+        E_FAIL,
+        "IAsyncActionWithProgress-ready",
+    )));
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncActionWithProgress-ready");
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncActionWithProgress::<i32>::spawn(|| {
+        Err(Error::new(E_FAIL, "IAsyncActionWithProgress-spawn"))
+    });
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncActionWithProgress-spawn");
+        assert_ne!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperation::<i32>::ready(Err(Error::new(E_FAIL, "IAsyncOperation-ready")));
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncOperation-ready");
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperation::<i32>::spawn(|| Err(Error::new(E_FAIL, "IAsyncOperation-spawn")));
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncOperation-spawn");
+        assert_ne!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperationWithProgress::<i32, i32>::ready(Err(Error::new(
+        E_FAIL,
+        "IAsyncOperationWithProgress-ready",
+    )));
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncOperationWithProgress-ready");
+        assert_eq!(thread, thread_id());
+    })?;
+
+    let a = IAsyncOperationWithProgress::<i32, i32>::spawn(|| {
+        Err(Error::new(E_FAIL, "IAsyncOperationWithProgress-spawn"))
+    });
+
+    a.when(move |r| {
+        let err = r.unwrap_err();
+        assert_eq!(err.code(), E_FAIL);
+        assert_eq!(err.message(), "IAsyncOperationWithProgress-spawn");
         assert_ne!(thread, thread_id());
     })?;
 
