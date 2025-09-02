7ffbf24a1e0e27d865638ac139ab3e90a4dc3f04
Rafael Rivera <rafael@withinwindows.com>
2025-09-02 11:16:47 -0700

    Avoid running test suite for web changes (#3739)

--- crates/tools/yml/src/clippy.rs
+++ crates/tools/yml/src/clippy.rs
@@ -7,10 +7,12 @@ on:
   pull_request:
     paths-ignore:
       - '.github/ISSUE_TEMPLATE/**'
+      - '.github/workflows/web.yml'
       - 'web/**'
   push:
     paths-ignore:
       - '.github/ISSUE_TEMPLATE/**'
+      - '.github/workflows/web.yml'
       - 'web/**'
     branches:
       - master
