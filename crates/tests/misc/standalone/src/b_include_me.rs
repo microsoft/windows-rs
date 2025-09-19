83675feed1068f1ebddadfcbe796bfdd1d86e4d9
Kenny Kerr <kenny@kennykerr.ca>
2025-09-19 10:52:35 -0500

    test_standalone

--- crates/tests/misc/standalone/src/b_include_me.rs
+++ crates/tests/misc/standalone/src/b_include_me.rs
@@ -1,5 +1,5 @@
 #[inline]
 pub unsafe fn GetVersion() -> u32 {
-    windows_link::link!("kernel32.dll" "system" fn GetVersion() -> u32);
+    windows_core::link!("kernel32.dll" "system" fn GetVersion() -> u32);
     unsafe { GetVersion() }
 }
