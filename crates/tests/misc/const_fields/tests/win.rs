0e054214f6473d811ea27b2738e2341ff79cf374
Kenny Kerr <kenny@kennykerr.ca>
2025-10-14 10:08:14 -0500

    variable is assigned to, but never used

--- crates/tests/misc/const_fields/tests/win.rs
+++ crates/tests/misc/const_fields/tests/win.rs
@@ -4,8 +4,8 @@ use windows::{Win32::Storage::CloudFilters::*, Win32::System::CorrelationVector:
 #[expect(clippy::field_reassign_with_default)] // testing unusual field assignment
 fn can_assign_const() {
     let v: CORRELATION_VECTOR = Default::default();
-    let mut i: CF_OPERATION_INFO = Default::default();
+    let mut _i: CF_OPERATION_INFO = Default::default();
 
     // Note that the rhs expression does not need to be mutable.
-    i.CorrelationVector = &v;
+    _i.CorrelationVector = &v;
 }
