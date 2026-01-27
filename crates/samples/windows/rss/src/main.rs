6835792270df071f8b5df63ea64907fb3fc0d2a6
Kenny Kerr <kenny@kennykerr.ca>
2026-01-27 10:07:58 -0600

    sample await

--- crates/samples/windows/rss/src/main.rs
+++ crates/samples/windows/rss/src/main.rs
@@ -1,6 +1,6 @@
 use windows::{core::*, Foundation::Uri, Web::Syndication::SyndicationClient};
 
-fn main() -> Result<()> {
+async fn main_async() -> Result<()> {
     let uri = Uri::CreateUri(h!("https://blogs.windows.com/feed"))?;
     let client = SyndicationClient::new()?;
 
@@ -9,11 +9,15 @@ fn main() -> Result<()> {
         h!("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)"),
     )?;
 
-    let feed = client.RetrieveFeedAsync(&uri)?.join()?;
+    let feed = client.RetrieveFeedAsync(&uri)?.await?;
 
     for item in feed.Items()? {
         println!("{:?}", item.Title()?.Text()?);
     }
 
     Ok(())
+}
+
+fn main() -> Result<()> {
+    futures::executor::block_on(main_async())
 }
