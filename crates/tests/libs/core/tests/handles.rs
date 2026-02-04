a1db204a8565652cfa7229f2fa063abe6a6eaff8
Charles Milette <charles.milette@gmail.com>
2026-02-04 11:12:01 -0500

    Add `Owned::into_raw` to relinquish ownership of an handle (#3869)
    Co-authored-by: Kenny Kerr <kenny@kennykerr.ca>

---
+++ crates/tests/libs/core/tests/handles.rs
@@ -0,0 +1,47 @@
+use std::cell::Cell;
+use std::rc::Rc;
+
+use windows::core::Free;
+use windows::core::Owned;
+
+#[derive(Clone)]
+struct FreeCounter {
+    pub count: Rc<Cell<u32>>,
+}
+
+impl Free for FreeCounter {
+    unsafe fn free(&mut self) {
+        self.count.update(|c| c + 1);
+    }
+}
+
+#[test]
+fn into_raw() {
+    let free_count: Rc<Cell<u32>> = Rc::new(Cell::new(0));
+    let counter = FreeCounter {
+        count: free_count.clone(),
+    };
+
+    {
+        let owned = unsafe { Owned::new(counter) };
+        let _counter = Owned::into_raw(owned);
+        assert!(free_count.get() == 0);
+    }
+
+    assert!(free_count.get() == 0);
+}
+
+#[test]
+fn drop() {
+    let free_count: Rc<Cell<u32>> = Rc::new(Cell::new(0));
+    let counter = FreeCounter {
+        count: free_count.clone(),
+    };
+
+    {
+        let _owned = unsafe { Owned::new(counter) };
+        assert!(free_count.get() == 0);
+    }
+
+    assert!(free_count.get() == 1);
+}
