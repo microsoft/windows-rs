b789304a6b8954cdfac233b1349ed16993431e38
Kenny Kerr <kenny@kennykerr.ca>
2026-03-09 13:56:03 -0500

    test attribute fields

--- crates/libs/metadata/src/reader/tables/attribute.rs
+++ crates/libs/metadata/src/reader/tables/attribute.rs
@@ -64,6 +64,8 @@ fn read_value(blob: &mut Blob, ty: &Type) -> Value {
         Type::U32 => Value::U32(blob.read_u32()),
         Type::I64 => Value::I64(blob.read_i64()),
         Type::U64 => Value::U64(blob.read_u64()),
+        Type::F32 => Value::F32(blob.read_f32()),
+        Type::F64 => Value::F64(blob.read_f64()),
         Type::String => Value::Utf8(blob.read_utf8()),
         Type::Name(tn) => {
             if tn.namespace == "System" && tn.name == "Type" {
