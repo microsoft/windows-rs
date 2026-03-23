8effa64c7f9fb644f650d9de8485e6a59e307504
Kenny Kerr <kenny@kennykerr.ca>
2026-03-23 13:07:44 -0500

    test name

--- crates/tests/libs/metadata/tests/assembly_name.rs
+++ crates/tests/libs/metadata/tests/assembly_name.rs
@@ -2,13 +2,13 @@ use windows_metadata::*;
 
 #[test]
 fn name() {
-    let writer = writer::File::new("test");
+    let writer = writer::File::new("TestName");
 
     let bytes = writer.into_stream();
-    std::fs::write("tests/assembly.winmd", bytes).unwrap();
+    std::fs::write("tests/assembly_name.winmd", bytes).unwrap();
 
-    let reader = reader::File::read("tests/assembly.winmd").unwrap();
-    assert_eq!(reader.assembly_name(), Some("test"));
+    let reader = reader::File::read("tests/assembly_name.winmd").unwrap();
+    assert_eq!(reader.assembly_name(), Some("TestName"));
 
     let reader = reader::File::read("../../../libs/bindgen/default/Windows.Win32.winmd").unwrap();
     assert_eq!(reader.assembly_name(), Some("Windows.Win32.winmd"));
