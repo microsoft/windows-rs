c72597c141d9e8a2b99202a3275ef0169b1020a8
Kenny Kerr <kenny@kennykerr.ca>
2026-03-26 12:45:08 -0500

    port

--- crates/tests/winrt/reference/build.rs
+++ crates/tests/winrt/reference/build.rs
@@ -1,22 +1,10 @@
 fn main() {
-    let mut command = std::process::Command::new("midlrt.exe");
-    command.args([
-        "/winrt",
-        "/nomidl",
-        "/h",
-        "nul",
-        "/metadata_dir",
-        "../../../libs/bindgen/default",
-        "/reference",
-        "../../../libs/bindgen/default/Windows.winmd",
-        "/winmd",
-        "metadata.winmd",
-        "src/metadata.idl",
-    ]);
-
-    if !command.status().unwrap().success() {
-        panic!("Failed to run midlrt");
-    }
+    windows_rdl::reader()
+        .output("metadata.winmd")
+        .input("src/metadata.rdl")
+        .reference("../../../libs/bindgen/default")
+        .write()
+        .unwrap();
 
     windows_bindgen::bindgen([
         "--in",
