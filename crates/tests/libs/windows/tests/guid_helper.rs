fb079d0edeb59189584de5cce94403d93b3ca5c9
Kenny Kerr <kenny@kennykerr.ca>
2026-04-08 09:35:34 -0500

    test_windows

---
+++ crates/tests/libs/windows/tests/guid_helper.rs
@@ -0,0 +1,16 @@
+use windows::{core::*, Foundation::*};
+
+// Simple test for the OS-provided implementation of ELEMENT_TYPE_CMOD_REQD parameters.
+#[test]
+fn test() -> Result<()> {
+    let empty = GuidHelper::Empty()?;
+    let a = GuidHelper::CreateNewGuid()?;
+    let b = GuidHelper::CreateNewGuid()?;
+
+    assert!(!GuidHelper::Equals(a, b)?);
+    assert!(!GuidHelper::Equals(a, empty)?);
+    assert!(GuidHelper::Equals(GUID::default(), empty)?);
+    assert!(GuidHelper::Equals(a, a)?);
+
+    Ok(())
+}
