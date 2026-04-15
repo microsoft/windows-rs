5218729616248d43634f418ebe6ee5031b194a72
Kenny Kerr <kenny@kennykerr.ca>
2026-04-15 13:51:28 -0500

    test

---
+++ crates/tests/libs/clang/tests/roundtrip.rs
@@ -0,0 +1,38 @@
+use windows_rdl::*;
+
+#[test]
+fn roundtrip() {
+    let paths = std::fs::read_dir("roundtrip")
+        .unwrap()
+        .map(|e| e.unwrap().path())
+        .filter(|p| p.extension().and_then(|e| e.to_str()) == Some("h"));
+
+    for h in paths {
+        let rdl = h.with_extension("rdl").to_str().unwrap().to_string();
+        let winmd = h.with_extension("winmd").to_str().unwrap().to_string();
+        let h = h.to_str().unwrap().to_string();
+        let reference = "../../../libs/bindgen/default";
+
+        clang()
+            .input(&h)
+            .input(reference)
+            .output(&rdl)
+            .write()
+            .unwrap();
+
+        reader()
+            .input(&rdl)
+            .input(reference)
+            .output(&winmd)
+            .write()
+            .unwrap();
+
+        writer()
+            .input(&winmd)
+            .input(reference)
+            .output(&rdl)
+            .filter("Test")
+            .write()
+            .unwrap();
+    }
+}
