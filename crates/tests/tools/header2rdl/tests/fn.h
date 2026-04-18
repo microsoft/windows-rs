9e4cd8364d563587660a9ed195097226ee40773f
Kenny Kerr <kenny@kennykerr.ca>
2026-04-17 21:39:20 -0500

    Remove old clang experiment

--- crates/tests/tools/header2rdl/tests/fn.h
+++
@@ -1,8 +0,0 @@
-typedef long HRESULT;
-typedef unsigned int DWORD;
-typedef void* HANDLE;
-
-int Add(int a, int b);
-HRESULT CreateHandle(DWORD flags, HANDLE* out);
-void CloseHandle(HANDLE handle);
-DWORD GetCount(const void* data, DWORD length);
