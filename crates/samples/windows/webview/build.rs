731cc32d048586058f0bfc4d1898d96421e6c688
Kenny Kerr <kenny@kennykerr.ca>
2026-04-22 10:29:46 -0500

    sample_webview

---
+++ crates/samples/windows/webview/build.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let h = "src/WebView2.h";
+    let library = "WebView2Loader.dll";
+    let namespace = "WebView2";
+    let rdl = "src/WebView2.rdl";
+    let reference = "../../../libs/bindgen/default";
+    let winmd = "WebView2.winmd";
+
+    println!("cargo:rerun-if-changed={h}");
+
+    windows_rdl::clang()
+        .args([
+            "-x",
+            "c++",
+            "--target=x86_64-pc-windows-msvc",
+            "-fms-extensions",
+        ])
+        .input(h)
+        .input(reference)
+        .output(rdl)
+        .namespace(namespace)
+        .library(library)
+        .write()
+        .unwrap();
+
+    windows_rdl::reader()
+        .input(rdl)
+        .input(reference)
+        .output(winmd)
+        .write()
+        .unwrap();
+
+    windows_bindgen::bindgen([
+        "--in",
+        winmd,
+        reference,
+        "--out",
+        "src/bindings.rs",
+        "--filter",
+        namespace,
+        "--flat",
+        "--reference",
+        "windows",
+    ])
+    .unwrap();
+}
---
+++ crates/samples/windows/webview/src/main.rs
@@ -0,0 +1,3 @@
+mod bindings;
+
+fn main() {}
