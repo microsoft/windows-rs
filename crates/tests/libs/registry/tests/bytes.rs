de06d2bf3701a853cd6034fe491d82858fc887de
Zane Purvis <zanedp@users.noreply.github.com>
2026-05-12 12:48:46 -0400

    Update bytes tests to include get_bytes()

--- crates/tests/libs/registry/tests/bytes.rs
+++ crates/tests/libs/registry/tests/bytes.rs
@@ -27,5 +27,18 @@ fn bytes() -> Result<()> {
         (Type::Other(1234), 4)
     );
 
+    // Make sure there's some zeros in the middle in case someone adds some
+    // null-checks that shouldn't be happening for this Type.
+    let test_bytes = &[
+        0x12, 0x34, 0x56, 0x78, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x9A, 0xBC,
+    ];
+    key.set_bytes("binary", Type::Bytes, test_bytes)?;
+    assert_eq!(key.get_type("binary")?, Type::Bytes);
+    assert_eq!(key.get_bytes("binary")?, test_bytes.to_vec());
+
+    // make sure you can't get a non-bytes value as bytes
+    key.set_string("string_value", "not bytes")?;
+    assert!(key.get_bytes("string_value").is_err());
+
     Ok(())
 }
