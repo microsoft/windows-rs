0abc44e6eeb755353903c5d2065d7afc09f4a218
Kenny Kerr <kenny@kennykerr.ca>
2026-06-04 11:19:03 -0500

    non-windows

--- crates/libs/time/src/lib.rs
+++ crates/libs/time/src/lib.rs
@@ -1,11 +1,13 @@
 #![doc = include_str!("../readme.md")]
 #![cfg_attr(all(not(feature = "std")), no_std)]
 
+#[allow(dead_code)]
 #[expect(missing_docs, non_snake_case, clippy::upper_case_acronyms)]
 mod bindings;
 mod datetime;
 mod timespan;
 
+#[cfg(windows)]
 use bindings::*;
 pub use bindings::{DateTime, TimeSpan};
 pub use timespan::TimeRangeError;
