d776c59330bd6cae685540c86a60823cc615bef5
Kenny Kerr <kenny@kennykerr.ca>
2026-06-06 23:36:38 -0500

    fix solitaire

--- crates/libs/reactor/src/core/widgets/border.rs
+++ crates/libs/reactor/src/core/widgets/border.rs
@@ -73,7 +73,14 @@ impl Default for Border {
 impl Widget for Border {
     widget_header!(ControlKind::Border);
     fn bindings(&self) -> PropBindings {
-        crate::core::generated_bindings::border_bindings(self)
+        let mut out = crate::core::generated_bindings::border_bindings(self);
+        if let Some(BrushBinding::Direct(br)) = &self.border_brush {
+            out.push(Binding::Prop(
+                Prop::BorderBrush,
+                PropValue::Brush(br.clone()),
+            ));
+        }
+        out
     }
     fn children(&self) -> Children<'_> {
         Children::PositionalSingle(&self.child)
