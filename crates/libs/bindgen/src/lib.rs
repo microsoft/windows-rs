52ed9e9a1d52076914cd507d4b086d9640dda5d9
Kenny Kerr <kenny@kennykerr.ca>
2026-06-11 16:31:15 -0500

    windows-bindgen

--- crates/libs/bindgen/src/lib.rs
+++ crates/libs/bindgen/src/lib.rs
@@ -1,9 +1,4 @@
 #![doc = include_str!("../readme.md")]
-#![expect(
-    non_upper_case_globals,
-    clippy::enum_variant_names,
-    clippy::upper_case_acronyms
-)]
 #![warn(unused_qualifications)]
 
 mod config;
