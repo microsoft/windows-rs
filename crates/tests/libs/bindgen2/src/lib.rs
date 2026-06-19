1aa830dd436dc54f5ae44c72633d3b7230873977
Kenny Kerr <kenny@kennykerr.ca>
2026-06-19 16:09:41 -0500

    expect

--- crates/tests/libs/bindgen2/src/lib.rs
+++ crates/tests/libs/bindgen2/src/lib.rs
@@ -2,7 +2,9 @@
     dead_code,
     non_snake_case,
     non_camel_case_types,
-    non_upper_case_globals
+    non_upper_case_globals,
+    clippy::upper_case_acronyms,
+    clippy::missing_transmute_annotations
 )]
 
 include!("../../../../../target/test_bindgen2/compile_fixtures.rs");
