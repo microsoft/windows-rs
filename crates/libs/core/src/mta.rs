8aa13e3059672eddb6101b3f60b3b5d67e043317
Kenny Kerr <kenny@kennykerr.ca>
2026-08-02 15:55:13 -0500

    simpler

--- crates/libs/core/src/mta.rs
+++ crates/libs/core/src/mta.rs
@@ -3,11 +3,6 @@ use core::ptr::null_mut;
 
 /// Initializes COM for apartment-agnostic code and keeps the multithreaded apartment alive for the
 /// rest of the process.
-///
-/// This does not change the apartment of a thread that is already initialized, but places an
-/// uninitialized calling thread into the multithreaded apartment.
-///
-/// This function must not be called during process shutdown or inside `DllMain`.
 pub fn init_mta() -> Result<()> {
     unsafe {
         let mut cookie = null_mut();
