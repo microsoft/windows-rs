1112d6e6dccecae8879e561ff1b24cca158b07fa
Kenny Kerr <kenny@kennykerr.ca>
2026-08-11 00:15:47 -0500

    `windows-reactor` run effects after native commit (#4826)

--- crates/tests/libs/reactor/tests/host_effects.rs
+++ crates/tests/libs/reactor/tests/host_effects.rs
@@ -7,7 +7,7 @@ use windows_reactor::RenderHost;
 use windows_reactor::{Dispatcher, DispatcherQueuePriority};
 use windows_reactor::{Element, TextBlock};
 use windows_reactor::{RenderCx, SetState};
-use windows_reactor::{component, text_block};
+use windows_reactor::{component, composition_host, swap_chain_panel, text_block, vstack};
 
 type QueuedJob = (DispatcherQueuePriority, Box<dyn FnOnce()>);
 
@@ -92,6 +92,204 @@ fn effect_runs_after_reconcile_ops_are_emitted() {
     assert!(
         set_prop_count >= 1,
         "at least one SetProp for the TextBlock"
+    );
+}
+
+struct NestedCommitEffect {
+    log: Rc<RefCell<Vec<&'static str>>>,
+}
+
+impl Component for NestedCommitEffect {
+    fn render(&self, _props: &(), cx: &mut RenderCx) -> Element {
+        self.log.borrow_mut().push("nested render");
+        let log = Rc::clone(&self.log);
+        cx.use_effect((), move || log.borrow_mut().push("nested effect"));
+        text_block("nested").into()
+    }
+}
+
+struct NestedCommitParent {
+    log: Rc<RefCell<Vec<&'static str>>>,
+}
+
+impl Component for NestedCommitParent {
+    fn render(&self, _props: &(), cx: &mut RenderCx) -> Element {
+        self.log.borrow_mut().push("parent render");
+        let log = Rc::clone(&self.log);
+        cx.use_effect((), move || log.borrow_mut().push("parent effect"));
+        component(
+            NestedCommitEffect {
+                log: Rc::clone(&self.log),
+            },
+            (),
+        )
+    }
+}
+
+struct CommitOrderRoot {
+    log: Rc<RefCell<Vec<&'static str>>>,
+}
+
+impl Component for CommitOrderRoot {
+    fn render(&self, _props: &(), cx: &mut RenderCx) -> Element {
+        self.log.borrow_mut().push("root render");
+        let root_effect_log = Rc::clone(&self.log);
+        cx.use_effect((), move || root_effect_log.borrow_mut().push("root effect"));
+        let mounted_log = Rc::clone(&self.log);
+        vstack((
+            component(
+                NestedCommitParent {
+                    log: Rc::clone(&self.log),
+                },
+                (),
+            ),
+            swap_chain_panel()
+                .on_mounted(move |_| mounted_log.borrow_mut().push("later native mounted")),
+        ))
+        .into()
+    }
+}
+
+#[test]
+fn nested_effects_run_after_the_native_tree_commits() {
+    let dispatcher = TestDispatcher::default();
+    let log = Rc::new(RefCell::new(Vec::new()));
+    let host = RenderHost::new(
+        RecordingBackend::new(),
+        Box::new(CommitOrderRoot {
+            log: Rc::clone(&log),
+        }),
+        dispatcher.clone(),
+    );
+    host.kick();
+
+    dispatcher.drain();
+
+    assert_eq!(
+        &*log.borrow(),
+        &[
+            "root render",
+            "parent render",
+            "nested render",
+            "later native mounted",
+            "nested effect",
+            "parent effect",
+            "root effect",
+        ]
+    );
+}
+
+struct PanickingNestedEffect;
+
+impl Component for PanickingNestedEffect {
+    fn render(&self, _props: &(), cx: &mut RenderCx) -> Element {
+        cx.use_effect((), || panic!("nested effect failed"));
+        text_block("committed").into()
+    }
+}
+
+struct PanickingEffectRoot;
+
+impl Component for PanickingEffectRoot {
+    fn render(&self, _props: &(), _cx: &mut RenderCx) -> Element {
+        component(PanickingNestedEffect, ())
+    }
+}
+
+#[test]
+fn nested_effect_panics_leave_the_host_tree_committed() {
+    let dispatcher = TestDispatcher::default();
+    let host = RenderHost::new(
+        RecordingBackend::new(),
+        Box::new(PanickingEffectRoot),
+        dispatcher.clone(),
+    );
+    host.kick();
+
+    dispatcher.drain();
+
+    assert!(host.root_id().is_some());
+    assert_eq!(
+        host.with_reconciler(|reconciler| reconciler.backend.live_control_count()),
+        1
+    );
+}
+
+#[derive(Clone)]
+struct EffectCommitProps {
+    updated: bool,
+    log: Rc<RefCell<Vec<&'static str>>>,
+}
+
+impl PartialEq for EffectCommitProps {
+    fn eq(&self, other: &Self) -> bool {
+        self.updated == other.updated && Rc::ptr_eq(&self.log, &other.log)
+    }
+}
+
+struct EffectCommitOrder;
+
+impl Component<EffectCommitProps> for EffectCommitOrder {
+    fn render(&self, props: &EffectCommitProps, cx: &mut RenderCx) -> Element {
+        let updated = props.updated;
+        let setup_log = Rc::clone(&props.log);
+        let cleanup_log = Rc::clone(&props.log);
+        cx.use_effect_with_cleanup((updated,), move || {
+            setup_log
+                .borrow_mut()
+                .push(if updated { "effect new" } else { "effect old" });
+            Some(move || {
+                cleanup_log.borrow_mut().push(if updated {
+                    "cleanup new"
+                } else {
+                    "cleanup old"
+                });
+            })
+        });
+
+        if updated {
+            let mounted_log = Rc::clone(&props.log);
+            composition_host()
+                .on_mounted(move |_| mounted_log.borrow_mut().push("new mounted"))
+                .into()
+        } else {
+            let mounted_log = Rc::clone(&props.log);
+            let unmounted_log = Rc::clone(&props.log);
+            swap_chain_panel()
+                .on_mounted(move |_| mounted_log.borrow_mut().push("old mounted"))
+                .on_unmounted(move |_| unmounted_log.borrow_mut().push("old unmounted"))
+                .into()
+        }
+    }
+}
+
+#[test]
+fn changed_effects_clean_up_and_restart_after_the_native_update() {
+    let log = Rc::new(RefCell::new(Vec::new()));
+    let old = component(
+        EffectCommitOrder,
+        EffectCommitProps {
+            updated: false,
+            log: Rc::clone(&log),
+        },
+    );
+    let new = component(
+        EffectCommitOrder,
+        EffectCommitProps {
+            updated: true,
+            log: Rc::clone(&log),
+        },
+    );
+    let mut reconciler = windows_reactor::Reconciler::new(RecordingBackend::new());
+    reconciler.reconcile(None, &old, None, Rc::new(|| {}));
+    assert_eq!(&*log.borrow(), &["old mounted", "effect old"]);
+
+    log.borrow_mut().clear();
+    reconciler.reconcile(Some(&old), &new, None, Rc::new(|| {}));
+
+    assert_eq!(
+        &*log.borrow(),
+        &["old unmounted", "new mounted", "cleanup old", "effect new"]
     );
 }
 
