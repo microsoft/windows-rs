c5afb96ba9f5a04c689b02af213ec978dd4cc1c2
Kenny Kerr <kenny@kennykerr.ca>
2026-09-18 17:45:53 -0500

    sample

--- crates/samples/reactor/app-bar-icon/src/main.rs
+++ crates/samples/reactor/app-bar-icon/src/main.rs
@@ -3,10 +3,18 @@ use windows_reactor::*;
 fn main() {
     App::run(
         CommandBar::new()
-            .primary_commands([KeyedView::new(
-                "like",
-                AppBarButton::new().label("Like").icon(Symbol::Like),
-            )])
+            .primary_commands([
+                KeyedView::new(
+                    "symbol",
+                    AppBarButton::new().label("Symbol").icon(Symbol::Like),
+                ),
+                KeyedView::new(
+                    "search",
+                    AppBarButton::new()
+                        .label("Search")
+                        .icon(FontIcon::new().glyph("\u{E721}")),
+                ),
+            ])
             .into(),
     )
     .unwrap();
